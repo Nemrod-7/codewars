5f776c7b9c2ac3ec73ceafe5d01b7e2bf88857e5
Nemrod-7 <74612985+Nemrod-7@users.noreply.github.com>
2021-07-21 09:59:04 +0200

    Update 2 kyu - Gerrymander Solver.cpp

--- 2 Kyu/2 kyu - Gerrymander Solver.cpp
+++ 2 Kyu/2 kyu - Gerrymander Solver.cpp
@@ -1,150 +1,195 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <random>
 
 using namespace std;
 
 struct Point {int x, y;};
 
-const vector<Point> compass {{-1,0},{1,0},{0,-1},{0,1}};
+const vector<Point> compass {{1,0},{0,1},{-1,0},{0,-1}};
+const vector<Point> corner {{1,1},{-1,1},{-1,-1},{1,-1}};
 
 class Vertex {
     public :
         int x, y;
-        int dist, state;
+        int dist;
         char data;
 
     int distance (Vertex &a) { return abs (x - a.x) + abs (y - a.y); }
-    Vertex nearest (vector<Vertex*> &points) {
-        Vertex V;
-        int min = 999, dist;
-
-        for (auto &p : points) {
-
-            dist = distance (*p);
-            if ((x != p->x) || (y != p->y)) {
-                if (dist < min) {
-                    min = dist;
-                    V = *p;
-                }
-            }
-        }
-        return V;
-    }
 };
 class Graph {
     public :
         vector<vector<Vertex>> Vmap;
-        vector<Vertex> edge;
-        vector<Vertex*> edge2;
-        int clust[5];
+        vector<Vertex*> obj;
+        vector<int> clust;
 
         Graph (const string &src) {
+          
             Vmap.resize (5, vector<Vertex> (5));
+            clust.resize(5);
+          
             for (int y = 0; y < 5; ++y) {
-
-                Vertex V ;
-                clust[y] = 0;
-                for (int x = 0; x < 5; ++x) {
-                    Vertex V = {x, y, y, 0, src[y * 6 + x]};
+                for (int x = 0; x < 5; ++x) {                 
+                    Vertex V = {x, y, y, src[y * 6 + x]};
+                  
                     if (src[y * 6 + x] == 'O') {
-
-                        edge2.push_back(&Vmap[y][x]);
+                        obj.push_back(&Vmap[y][x]);
                         clust[y]++;
                     }
+                  
                     Vmap[y][x] = V;
                 }
             }
         }
 
-        Vertex *get (Point p) { return &Vmap[p.y][p.x]; }
-};
-class Display {
-    public :
-        static void regions (const Graph &G) {
-            for (int y = 0; y < 5; ++y) {
-                for (int x = 0; x < 5; ++x) {
-                    cout << G.Vmap[y][x].dist;
-                }
-                cout << endl;
-            }
-        }
-        static void pt (const Point p) {
-            cout << '[' << p.x << ',' << p.y << ']' << endl;
-        }
-        static void weights (Graph &G) {
-            for (int y = 0; y < 5; y++) {
-                for (int x = 0; x < 5; x++) {
-                    Vertex V = G.Vmap[y][x];
-
-                    if (V.data == 'O') {
-                        Vertex next = V.nearest (G.edge2);
-                        cout << V.distance (next) << ' ';
-                    } else {
-                        cout << "# ";
-                    }
-                }
-                cout << endl;
-            }
-        }
-
+        Vertex *get (Point p) { return is_inside(p) ? &Vmap[p.y][p.x] : nullptr; }
+        int getd (Point p) { return get(p) ? get(p)->dist : -1; }
+        bool is_inside (Point p) { return (p.x >= 0 && p.y >= 0 && p.x < 5 && p.y < 5); }
 };
 
-bool is_inside (Point p) { return (p.x >= 0 && p.y >= 0 && p.x < 5 && p.y < 5); }
+Vertex *nearest (vector<Vertex*> &points, Vertex *p) {
+  Vertex *out;
+  int min = 999, dist;
 
-bool inters (Graph G, Point p) {
+  for (auto &V : points) {
+      dist = V->distance (*p);
 
-    int dist[5] = {0};
+      if (V != p && dist < min) {
+          min = dist;
+          out = V;
+      }
+  }
+  return out;
+}
 
-    for (auto &dir : compass) {
-        Point nxt = {p.x + dir.x, p.y + dir.y};
-        if (is_inside (nxt)) {
+bool inters (Graph &G, Vertex *V) {
+    int id = V->dist;
 
-            Vertex *V = G.get(nxt); // Vertex V = G.Vmap[nxt.y][nxt.x];
-            dist[V->dist]++;
+    for (int i = 0; i < 4; i++) {
+        int j = (i + 1) % 4, k = (i + 2) % 4, l = (i + 3) % 4;
 
-            if (dist[V->dist] > 1) return false;
-        }
+        Point N = {V->x + compass[i].x, V->y + compass[i].y};
+        Point E = {V->x + compass[j].x, V->y + compass[j].y};
+        Point S = {V->x + compass[k].x, V->y + compass[k].y};
+        Point W = {V->x + compass[l].x, V->y + compass[l].y};
+        Point NE = {V->x + corner[i].x, V->y + corner[i].y};
+
+        if (G.getd(N) == id && G.getd(S) == id && G.getd(E) != id && G.getd(W) != id) return false;
+        if (G.getd(N) == id && G.getd(E) == id && G.getd(NE) != id) return false;
     }
     return true;
 }
-Point nxt_intersection (Graph G, Point p) {
-    int iy = p.y, ix = p.x;
 
-    for (p.y; p.y < 5; p.y++) {
-        p.x = (iy == p.y ? ix : 0);
+vector<Vertex *> get_clust (Graph &G, int id) {
+    vector<Vertex *> cluster;
+    Point p;
 
-        for (; p.x < 5; p.x++)
-            if (inters (G, p))  return p;
+    for (p.y = 0; p.y < 5; ++p.y) {
+        for (p.x = 0; p.x < 5; ++p.x) {
+            Vertex *V = G.get(p);
+          
+            if (V->dist == id)
+                cluster.push_back(V);
+        }
     }
+    return cluster;
 }
 
-int main () {
+void scan_cluster (Graph &G, Vertex *p) {
+    int id = p->dist;
+    Vertex *nxtv, *heur;
+    vector<Vertex *> actual = get_clust (G, id), voter;
 
-    string region;
+    for (auto &dir : compass) {
+        Point nxt = {p->x + dir.x, p->y + dir.y};
+        nxtv = G.get(nxt);
+      
+        if (nxtv && inters (G, nxtv) && (nxtv->dist != p->dist)) {
+            voter.push_back(nxtv);
+        }
+    }
 
-      region =
-      "OXOOX\n"
-			"XXOXO\n"
-			"XOXXX\n"
-			"XXOXX\n"
-			"OXXOO";
+    if (voter.size() == 0) return;
 
-    Graph G (region);
-    Point p = {0, 0};
+    bool seq2 = false;
+    int toswap = voter.front()->dist;
+    voter.front()->dist = id;
 
-    //Vertex next = V.nearest (G.edge);
+    for (auto &V : actual)
+        if (inters (G, V)) {
+            for (auto &dir : compass) {
+                Point nxt = {V->x + dir.x, V->y + dir.y};
+                if (G.is_inside (nxt)) {
+                    nxtv = G.get (nxt);
+                    if (inters (G, nxtv) && (nxtv->dist == toswap) && (nxtv != voter.front())) {
 
-    Display::weights(G);
+                          heur = V;
+                          seq2 = true;
+                    }
+                }
+            }
+        }
 
-    /*
-    region =
-    "ABCDE\n"
-    "FGEHI\n"
-    "JKLMN\n"
-    "OPQRS\n"
-    "TUVWX";
-    */
+    if (seq2)
+        heur->dist = toswap;
+    else
+        voter.front()->dist = toswap;
 
 }
-////////////////////////////////////////////////////////////////////////////////
+
+int update_cnt (Graph &G) {
+    
+    Point p;
+    int cnt = 0;
+    fill(G.clust.begin(), G.clust.end(), 0);
+  
+    for (p.y = 0; p.y < 5; ++p.y)
+        for (p.x = 0; p.x < 5; ++p.x) 
+            if (G.get(p)->data == 'O')
+                G.clust[G.get(p)->dist]++;
+
+    for (auto &it : G.clust)
+        if (it > 2) cnt++;
+
+    return cnt;
+}
+bool check (Graph &G) {
+
+    Point p;
+    int minv = 99;
+
+    for (p.y = 0; p.y < 5; p.y++) {
+      for (p.x = 0; p.x < 5; p.x++) {
+        Vertex *V = G.get(p), *next = nearest (G.obj, V);
+
+        if (V->data == 'O')
+            minv = min (minv, V->distance (*next));
+      }
+    }
+    return minv < 2 ? true : false;
+}
+string gerrymander (string src) {
+
+    Graph G (src);
+    if (check (G) == false) return "";
+
+    random_device rd; 
+    mt19937 gen (rd());
+    uniform_int_distribution<> rng (0, 4);
+
+    string os;
+
+    while (update_cnt(G) != 3)
+        scan_cluster (G, G.get ({rng(gen),rng(gen)}));
+    
+    Point p;
+    for (p.y = 0; p.y < 5; ++p.y) {
+        for (p.x = 0; p.x < 5; ++p.x) 
+            os += G.get(p)->dist + '1';
+        
+        if (p.y < 4)
+            os += '\n';
+    }
+
+    return os;
+}
