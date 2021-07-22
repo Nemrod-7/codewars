181425ef7796c1fb3c7d94ee7d8c2a33f4bde5de
Nemrod-7 <ocopey@gmail.com>
2021-07-22 07:57:26 +0200

    update

--- 2 Kyu/2 kyu - Blaine is pain.cpp
+++ 2 Kyu/2 kyu - Blaine is pain.cpp
@@ -4,33 +4,43 @@
 #include <algorithm>
 #include <map>
 
+#include <chrono>
+#include <thread>
+
 using namespace std;
 
-pair<int,int> operator+ (pair<int,int> &a, const pair<int,int> &b) {
+pair<int,int> operator+ (const pair<int,int> &a, const pair<int,int> &b) {
     return {a.first + b.first, a.second + b.second};
+}
+pair<int,int> operator- (const pair<int,int> &a, const pair<int,int> &b) {
+    return {a.first - b.first, a.second - b.second};
 }
 void operator+= (pair<int,int> &a, const pair<int,int> &b) {
     a.first += b.first, a.second += b.second;
 }
+void operator-= (pair<int,int> &a, const pair<int,int> &b) {
+    a.first -= b.first, a.second -= b.second;
+}
 
-                                    //  0      1      2     3       4     5     6      7
-                                    //  W     NW      N     NE      E     SE    S     SW
-const vector<pair<int,int>> compass {{0,-1},{-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1}};
+                                      //  0      1      2     3       4     5     6      7
+                                      //  W     NW      N     NE      E     SE    S     SW
+//const vector<pair<int,int>> compass {{0,-1},{-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1}};
 
-using direction = pair<pair<int,int>,pair<int,int>>;
-map<char, direction> base;
+const pair<int, int> vert {0,1}, horiz { 1,0};
+map<char, pair<int,int>> base {{'-', {1,0}}, {'|', {0,1}}, {'\\', {1,1}}, {'/', {-1,1}}};
 
-/*
+const vector< pair<int,int>> compass {{1,0},{0,1},{-1,0},{0,-1}};
+const vector< pair<int,int>> corner {{1,1},{-1,1},{-1,-1},{1,-1}};
 
-'|' == compass[2] || compass[6]
-'-' == compass[0] || compass[4]
-
-*/
-
-enum {clockwise, counterclockwise};
 struct train {
-    //pair<int,int> p;
-    int unit, way;
+    pair<int,int> curr, prev;
+    char id;
+    int unit;
+
+    void move (const pair<int,int> &p) {
+        prev = curr;
+        curr = p;
+    }
 };
 
 class graph {
@@ -42,51 +52,118 @@ class graph {
             width = 0, height = count (begin(src), end(src), '\n');
             size_t x = 0, y = 0;
 
-            for (size_t i = 0, x = 0; i < src.size(); i++, x++) {
+            for (int i = 0; i < src.size(); i++, x++) {
                 pair<size_t, size_t> p {x, y};
                 if (src[i] == '\n') {
                     width = max (x, width);
-                    y++, x = 0;
-                } else
+                    y++, x = -1;
+                } else {
+                   if (src[i] != ' ')
+                    //cout << x << ' ';
                     vmap[p] = src[i];
+                }
             }
         }
+
+        char operator[] (const pair<int,int> &p) { return vmap[p] ? vmap[p] : ' ';}
 };
 
 class Display {
     public :
-        static void graph (graph &G) {
-            for (int y = 0; y < G.height; ++y) {
-                for (int x = 0; x < G.width; ++x) {
-                    cout << G.vmap[{x,y}];
+        static void track (graph &G) {
+          pair<int,int> p;
+          for (p.second = 0; p.second < G.height; p.second++) {
+              for (p.first = 0; p.first < G.width; p.first++) {
+                    cout << G[p];
                 }
                 cout << "\n";
             }
         }
+        static void rail (graph &G, train &a) {
+            cout << "\033c";
+            pair<int,int> p;
+            for (p.second = 0; p.second < G.height; ++p.second) {
+                for (p.first = 0; p.first < G.width; ++p.first) {
+                    if (a.curr == p)
+                        cout << a.id;
+                    else
+                        cout << G[p];
+                }
+                cout << "\n";
+            }
+            this_thread::sleep_for(500ms);
+        }
+        static void point (const pair<int,int> &p) {
+            cout << "[" << p.first << ',' << p.second << "]";
+        }
 };
 
-train mk_train (const string &src) { return {src.size() - 1, isupper (src[0]) ? clockwise : counterclockwise};}
-
-int train_crash(const string &track, const string &a_train, int a_train_pos, const string &b_train, int b_train_pos, int limit) {
-
-    graph railroad (track);
-    train A = mk_train (a_train), B = mk_train (b_train);
-
-    pair<int,int> from, next ;
-                          //      E       W
-    vector<pair<int,int>> horiz {{0,1}, {0,-1}};
-    char tile;
-    switch (tile) {
-
-        case '\\' : break;
-        case '|'  : break;
-        case '-'  : next = horiz[A.way]; break;
-        case '/'  : break;
-        case '+'  : break;
-        case 'X'  : break;
-
-        default: break;
+pair<int,int> get_origin (graph &track) {
+    pair<int,int> p;
+    for (p.second = 0; p.second < track.height; ++p.second) {
+        for (p.first = 0; p.first < track.width; ++p.first) {
+            if (track[p] != ' ') return p;
+        }
     }
+    return {0,0};
+}
+train mk_train (graph &track, const string &src) {
+    train out;
+    out.curr = get_origin (track);
+    out.unit = src.size() - 1;
+    if (isupper(src[0])) {
+        out.id = src[0];
+    }
+
+    return out;
+}
+
+train update (graph &track, train &x) {
+    char curr = track[x.curr], last = track[x.prev];
+    pair<int,int> next;
+
+    switch (curr) {
+        case '+' :
+            if (last != '-' )curr = '|';
+            //break;
+        case '/' :
+            if (track[x.curr + base[curr]] == curr) {x.move (x.curr + base[curr]); return x;}
+
+            for (auto &p : compass) {
+                if (track[x.curr + p] != ' ')
+                    next = x.curr + p;
+            }
+            x.move (next);
+            break;
+        case '|' :
+            if (track[x.curr + vert]) {x.move (x.curr + vert); return x;}
+            if (track[x.curr - vert]) {x.move (x.curr - vert); return x;}
+        case '\\' :
+            if (track[x.curr + base['\\']] == curr) {x.move (x.curr + base['\\']); return x;}
+            if (track[x.curr + vert]) {x.move (x.curr + vert); return x;}
+            if (track[x.curr - vert]) {x.move (x.curr - vert); return x;}
+            if (track[x.curr - horiz]) {x.move (x.curr - horiz); return x;}
+            if (track[x.curr + horiz]) {x.move (x.curr + horiz); return x;}
+
+
+        default : break;
+    }
+
+
+    return x;
+}
+int train_crash(const string &src, const string &a_train, int a_train_pos, const string &b_train, int b_train_pos, int limit) {
+
+    graph track (src);
+    pair<int,int> origin = get_origin (track);
+    train A = mk_train (track, a_train);
+
+    int index = 6;
+
+    while (index-->0) {
+        update (track, A);
+    }
+    Display::rail (track, A);
 
     return 42;
 }
