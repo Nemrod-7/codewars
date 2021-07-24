767bb9d7ed20c89e9385780fa2259fcfad0c2ff6
Nemrod-7 <ocopey@gmail.com>
2021-07-24 13:19:50 +0200

    update

--- 2 Kyu/2 kyu - Blaine is pain.cpp
+++ 2 Kyu/2 kyu - Blaine is pain.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <map>
 
+#include <list>
 #include <chrono>
 #include <thread>
 
@@ -35,18 +36,19 @@ const vector<pair<int,int>> direct {{0,-1},{1,-1},{1,0},{1,1},{0,1},{-1,1},{-1,0
 vector<map<char,bool>> base {{{'\\',1},{'/',1},{'x',1},{'|',1},{'+',1}},{{'/',1}},
                              {{'/',1},{'/',1},{'x',1},{'-',1},{'+',1}},{{'\\',1}}};
 
-struct train {
-    pair<int,int> curr, prev;
-    char id;
-    int unit;
+class train {
+    public :
+        pair<int,int> curr, prev;
+        char id;
+        int unit;
+        list<pair<int,int>> wagon;
 
-    void move2 (const pair<int,int> p) {
-        pair<int,int> tmp = prev;
-        prev = curr;
-
+        void move2 (const pair<int,int> p) {
+            wagon.push_front (curr);
+            wagon.pop_back();
+            prev = curr;
             curr += p;
-
-    }
+        }
 };
 
 class graph {
@@ -65,8 +67,7 @@ class graph {
                     y++, x = -1;
                 } else {
                    if (src[i] != ' ')
-                    //cout << x << ' ';
-                    vmap[p] = src[i];
+                        vmap[p] = src[i];
                 }
             }
         }
@@ -87,13 +88,13 @@ class Display {
         }
         static void rail (graph &G, train &a) {
             cout << "\033c";
-
             pair<int,int> p;
             for (p.second = 0; p.second < G.height; ++p.second) {
                 for (p.first = 0; p.first < G.width; ++p.first) {
-
                     if (a.curr == p)
                         cout << a.id;
+                    else if (find (begin(a.wagon), end(a.wagon), p) != end(a.wagon))
+                        cout << 'a';  //tolower (a.id);
                     else
                         cout << G[p];
                 }
@@ -115,130 +116,100 @@ pair<int,int> get_origin (graph &track) {
     }
     return {0,0};
 }
-train mk_train (graph &track, const string &src) {
-    train out;
-    out.curr = get_origin (track);
-    out.unit = src.size() - 1;
-    if (isupper(src[0])) {
-        out.id = src[0];
-    }
+bool is_valid (graph &track, train &a, int index) {
+    pair<int,int> next = a.curr + direct[index];
+    char curr = track[a.curr], last = track[a.prev];
 
-    return out;
-}
-
-train update (graph &track, train &x) {
-    char curr = track[x.curr], last = track[x.prev];
-    pair<int,int> dir;
+    if (base[index % 4][track[next]] == false) return false;
+    if (next == a.prev) return false;
 
     if (curr == '+') {
         if (last != '-' ) curr = '|';
         else curr = '-';
     }
-    if (curr == 'S') {
-        curr = last;
-    }
+
 
     switch (curr) {
+        case 's' : break;
+        case '|' : if (index % 4 == 0) return true; break;
+        case '-' : if (index % 4 == 2) return true; break;
+        default  : return true; break;
+    }
+    return false;
+    //base[i % 4][track[next]]
+}
 
-        case '/' :
+train mk_train (graph &track, const string &src, int moves) {
+    train out;
+    int size = src.size() - 1;
+    out.curr = get_origin (track);
+    out.unit = size;
 
-            for (int i = 0; i < direct.size(); i++) {
-                pair<int,int> next = x.curr + direct[i];
+    for (int i = 5; i < direct.size(); i++) {
+          pair<int, int> nxt = out.curr + direct[i];
+          if (base[i % 4][track[nxt]]) {
+              out.prev = nxt;
+          }
+    }
+    pair<int,int> dir, p;
+    while (moves-->0) {
+        for (int i = 0; i < direct.size(); i++) {
+            if (is_valid (track, out, i))
+                dir = direct[i];
 
-                if (base[i % 4][track[next]] && next != x.prev) {
-                    dir = direct[i];
-                }
-            }
-            x.move2 (dir);
-            break;
-        case '-' :
-
-            for (int i = 0; i < direct.size(); i++) {
-                pair<int,int> next = x.curr + direct[i];
-
-
-                if (i % 4 == 2) {
-                    if (track[next] != ' ' && next != x.prev) {
-                        dir = direct[i];
-                    }
-                }
-            }
-            x.move2 (dir);
-            break;
-        case '|' :
-            for (int i = 0; i < direct.size(); i++) {
-                pair<int,int> next = x.curr + direct[i];
-
-                if (i % 4 == 0) {
-                    if (track[next] != ' ' && next != x.prev) {
-                        dir = direct[i];
-                    }
-                }
-            }
-
-            x.move2 (dir);
-            break;
-        case '\\' :
-            for (int i = 0; i < direct.size(); i++) {
-                pair<int,int> next = x.curr + direct[i];
-
-                if (track[next] != ' ' && next != x.prev) {
-                    dir = direct[i];
-                }
-            }
-
-            x.move2 (dir);
-            break;
-
-        default : break;
+        }
+        out.move2 (dir); //dir = direct[i];
     }
 
-    return x;
-}
-bool is_free (graph &G, const train &x, const pair<int,int> &p) {
+    p = out.curr;
+    if (isupper (src.front())) {
+        out.id = src.front();
 
-    return G[p] != ' ' && p != x.prev;
+        while (size-->0)
+            for (int i = 0; i < 4; i++) {
+                  pair<int, int> nxt = p + direct[i];
+                  if (base[i % 4][track[nxt]]) {
+                      p += direct[i];
+                      out.wagon.push_back (p);
+                  }
+            }
+
+    } else {
+        out.id = src.back();
+
+        while (size-->0)
+            for (int i = 5; i < direct.size(); i++) {
+                  pair<int, int> nxt = p + direct[i];
+                  //cout << track[nxt];
+                  if (base[i % 4][track[nxt]]) {
+                      p += direct[i];
+                      out.wagon.push_back (p);
+                  }
+            }
+    }
+    out.prev = out.wagon.front();
+    //Display::rail (track, out);
+    return out;
 }
-int train_crash(const string &src, const string &a_train, int a_train_pos, const string &b_train, int b_train_pos, int limit) {
+
+int train_crash (const string &src, const string &a_train, int a_train_pos, const string &b_train, int b_train_pos, int limit) {
 
     graph track (src);
-    pair<int,int> origin = get_origin (track);
-    train A = mk_train (track, a_train);
-
-    //A.prev = make_pair (31,1);
-
     pair<int,int> dir;
-    int index = 5;
+    train A = mk_train (track, b_train, b_train_pos);
 
-    while (index-->0) {
-        char curr = track[A.curr], last = track[A.prev];
+    int index = 50;
 
-        if (curr == '+') {
-
-            if (last == '-')
-
-            for (int i = 0; i < direct.size(); i++) {
-                pair<int,int> next = A.curr + direct[i];
-
-                if (i % 2 == 0 && next != A.prev) {
-                    cout << track[next];
-                }
-            }
-
-            cout << endl;
-        }
-
+    while (true) {
         for (int i = 0; i < direct.size(); i++) {
-            pair<int,int> next = A.curr + direct[i];
-
-            if (base[i % 4][track[next]] && next != A.prev) {
+            if (is_valid (track, A, i))
                 dir = direct[i];
-            }
         }
-        A.move2 (dir);
-        //update (track, A);
-        //Display::rail (track, A);
+        A.move2 (dir); //dir = direct[i];
+        Display::rail (track, A);
     }
+    //A.prev = make_pair (31,1);
+        //update (track, A);
 
     return 42;
 }
