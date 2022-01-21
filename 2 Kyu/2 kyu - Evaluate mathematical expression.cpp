c3b33ff72d6ee3b8cef0ac596a6beea9f550682d
Nemrod-7 <74612985+Nemrod-7@users.noreply.github.com>
2022-01-21 19:56:00 +0100

    Update 2 kyu - Evaluate mathematical expression.cpp

--- 2 Kyu/2 kyu - Evaluate mathematical expression.cpp
+++ 2 Kyu/2 kyu - Evaluate mathematical expression.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
 #include <algorithm>
+#include <map>
 #include <stack>
+#include <functional>
 
 using namespace std;
 using index = string::iterator;
+using func = function<double(double,double)>;
+
+map<char,int> order {{'+', 1},{'-',1},{'*',2},{'/',2},{'%',2}};
+map<char,func> operate {{'+', plus<double>()},{'-', minus<double>()},
+              {'*', multiplies<double>()}, {'/', divides<double>()}};
+
+/////////////////////////////////Assert/////////////////////////////////////////
+class Assert {
+    public :
+      template<class T> static void That (const T& actual, const T& expression) {
+          cout << fixed;
+          if (actual != expression) {
+              cout << "actual : " << actual;
+              cout << "\nexpect : " << expression;
+
+              cout << endl;
+          }
+    }
+};
+template<class T> const T& Equals (const T& entry) { return entry;}
+void Test();
+////////////////////////////////////////////////////////////////////////////////
 
 template<class T> T getstack (stack<T> &S) {
     if (S.empty()) return 0;
     T val = S.top();
     S.pop();
     return val;
-  }
-
+}
 double getnum (index &it) {
     string num;
 
@@ -33,43 +56,22 @@ string getsub (index &it) {
     }
     return sub;
 }
-int getop (char c) {
-    switch (c) {
-        case '-' : return 1;
-        case '+' : return 1;
-        case '*' : return 2;
-        case '/' : return 2;
-        default  : return 0;
-    }
-}
 
-double operation (double a, double b, char op) {
-
-    double val;
-    switch (op) {
-        case '+' : val = a + b; break;
-        case '-' : val = a - b; break;
-        case '*' : val = a * b; break;
-        case '/' : val = a / b; break;
-    }
-
-    return val;
-}
 double calc (string src) {
-
+    // convert expression to reverse polish notation using shunting-yard algorithm
     src.erase(remove (src.begin(), src.end(), ' '), src.end());
     index it = src.begin();
-    cout << fixed;
+    double num, sign = 1;
+
     stack<double> val;
     stack<char> ops;
 
-    double num, sign = 1;
-
     while (it != src.end()) {
-        char tok = *it;
-        int op = getop (tok);
 
-        if (tok == '-' && getop (*(it - 1))) {
+        char tok = *it;
+        int op = order[tok];
+
+        if (tok == '-' && order[*(it - 1)]) {
             sign = -1;
             op = 0;
         }
@@ -77,22 +79,27 @@ double calc (string src) {
         if (tok == '(') {
             string sub = getsub (it);
             num = calc (sub) * sign;
+            //cout << num << " ";
             val.push (num);
             num = 0, sign = 1;
 
         } else if (isdigit (*it)) {
             num = getnum (it) * sign;
             val.push (num);
-            num = 0, sign = 1;
+            sign = 1;
 
         } else {
             if (op) {
-                while (!ops.empty() && getop (ops.top()) >= op) {
+
+                while (!ops.empty() && order[ops.top()] >= op) {
                     char op = getstack (ops);
                     double b = getstack(val), a = getstack (val);
-                    val.push(operation (a, b, op));
-                    cout << a << " " << op << " " << b << " = " << operation (a, b, op) << endl;
+                    // cout << operate[op] (a, b) << " ";
+                    val.push(operate[op](a, b));
+                    //cout << a << " " << op << " " << b << " = " << operation (a, b, op) << endl;
                 }
+                /*
+                */
                 ops.push(tok);
             }
 
@@ -103,19 +110,23 @@ double calc (string src) {
     while (!ops.empty()) {
         char op = getstack (ops);
         double b = getstack (val), a = getstack (val);
-        val.push(operation (a, b, op));
-    }
+        val.push(operate[op](a,b));
+        //cout << a << " " << op << " " << b << " = " << operation (a, b, op) << endl;
 
+    }
+    //cout << (!val.empty() ? val.top() : 0) << " ";
     return !val.empty() ? val.top() : 0;
 }
 
 int main () {
 
-    /*
-    Assert::That(calc("63 - -20 / 84 - 33 * 87 + -17 * 41 + -10"), 3514.76);
-    
-    Assert::That(calc("(64) * (-97 / 51 * -(55)) - (21 * -((((71 - -33)))) - 44)"), 8922.901960784315);
+    string expression  = "(9.46+8.56+(7.55)/7.56/9.51/7.56/ -5.27-8.63*7.54/7.23* -0.84-5.64/(9.47* -7.40+ -5.94/6.80)/ -7.39*7.45-8.26/ -0.33+ -6.96+7.72-6.12+ -8.59+7.38*8.87+ -0.44+ -3.20+ -2.25- -2.13/(7.99-8.69*9.26-8.33+(7.12/6.71+6.75)+6.49+ -4.73+5.36*9.52/ -0.84/6.71-8.94/ -3.07-(9.52+ -3.57)+8.22- -8.54-5.09+ -1.97/ -3.68/7.13)/7.15*6.67-9.87+7.78/7.96*9.97/(9.50)/(9.02+7.97)+(((7.20+ -8.52+9.04/8.07/ -7.91/8.38/8.54))))";
+
+    //expression = "-7 * -(6 / 3)";
+
     Assert::That(calc("2 + 3 * 4 / 3 - 6 / 3 * 3 + 8"), 8.0);
+    //Assert::That(calc("63 - -20 / 84 - 33 * 87 + -17 * 41 + -10"), -3514.761905);
+    Assert::That(calc("(64) * (-97 / 51 * -(55)) - (21 * -((((71 - -33)))) - 44)"), 8922.901960784315);
     Assert::That(calc("-7 * -6 / 3"), Equals(14.0));
     Assert::That(calc("10- 2- -5"), Equals(13.0));
     Assert::That(calc("(1 + 1) / 3"), 0.6666666666666666);
@@ -141,3 +152,40 @@ int main () {
     //cout << calc (expression);
     //cout << 1 + 1 / (double)3;
 }
+void Test () {
+
+}
+////////////////////////////////////////////////////////////////////////////////
+/*
+
+double operation (double a, double b, char op) {
+
+    double val;
+    switch (op) {
+        case '+' : val = a + b; break;
+        case '-' : val = a - b; break;
+        case '*' : val = a * b; break;
+        case '/' : val = a / b; break;
+    }
+
+    return val;
+}
+
+bool isnumber (index it) {
+  if (isdigit (*it)) return true;
+  if (*it == '-') {
+      if (isdigit (*(it - 1))) return false;
+      if (isdigit (*(it + 1))) return true;
+  }
+  return false;
+}
+int getop (char c) {
+    switch (c) {
+        case '-' : return 1;
+        case '+' : return 1;
+        case '*' : return 2;
+        case '/' : return 2;
+        default  : return 0;
+    }
+  }
+*/
