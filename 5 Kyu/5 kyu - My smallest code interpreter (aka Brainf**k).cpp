82f8e2801df4eb9353f7ccc8b750666324e9cde5
Nemrod-7 <ocopey@gmail.com>
2022-02-02 18:20:01 +0100

    update

--- 5 Kyu/5 kyu - My smallest code interpreter (aka Brainf**k).cpp
+++ 5 Kyu/5 kyu - My smallest code interpreter (aka Brainf**k).cpp
@@ -1,20 +1,7 @@
 #include <iostream>
 #include <string>
-#include <vector>
 
-#define BUFFSIZE 1024
-#define OVERFLOW 255
-
-std::string brainLuck(std::string code, std::string input);
-
-
-int main () {
-
-    std::string tw = "codewars";
-    std::cout << brainLuck(",+[-.,+]",tw);
-
-    return 0;
-}
+const int overflow = 255;
 
 unsigned loop (const std::string code, unsigned pos) {
     int balance = 0, index = pos;
@@ -30,10 +17,10 @@ unsigned loop (const std::string code, unsigned pos) {
 
     return index;
 }
-std::string brainLuck (std::string code, std::string input) {
+std::string brainfuck (std::string code, std::string input) {
 
-    std::string output, tape;
-    auto pos = input.begin(), ptr = tape.begin();
+    std::string output, tape (1024, 0);
+    auto ins = input.begin(), ptr = tape.begin();
     int index = 0;
 
     while (code[index]) {
@@ -43,12 +30,21 @@ std::string brainLuck (std::string code, std::string input) {
             case '+': (*ptr)++; break;
             case '-': (*ptr)--; break;
             case '.': output += *ptr; break;
-            case ',': *ptr = pos != input.end() ? *pos++ : *ptr = OVERFLOW; break;
-            case '[': if (*ptr == NULL) index = loop (code,index); break;
-            case ']': if (*ptr != NULL) index = loop (code,index); break;
+            case ',': *ptr = ins != input.end() ? *ins++ : *ptr = overflow; break;
+            case '[': if (*ptr == 0) index = loop (code,index); break;
+            case ']': if (*ptr != 0) index = loop (code,index); break;
         }
         index++;
     }
 
     return output;
+}
+
+int main () {
+
+  std::string tw = "codewars";
+
+  std::cout << brainfuck(",+[-.,+]",tw);
+
+  return 0;
 }
