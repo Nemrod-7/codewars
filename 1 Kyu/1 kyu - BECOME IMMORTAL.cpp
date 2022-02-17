ef376e5bd668523559eb3d409760f9e67ad31e9c
Nemrod-7 <74612985+Nemrod-7@users.noreply.github.com>
2022-02-17 15:18:10 +0100

    Update 1 kyu - BECOME IMMORTAL.cpp

--- 1 Kyu/1 kyu - BECOME IMMORTAL.cpp
+++ 1 Kyu/1 kyu - BECOME IMMORTAL.cpp
@@ -19,6 +19,7 @@ int64_t Equals (const int64_t &entry) { return entry;}
 void Test ();
 ////////////////////////////////////////////////////////////////////////////////
 int64_t sumr (int64_t m, int64_t l) {
+    if (l >= m) return 0;
     return (m - l) * 0.5 * (m - l - 1);
 }
 int64_t highpow (int64_t x) {
@@ -33,17 +34,26 @@ int64_t highpow2 (int64_t x) {
 int64_t calc (int64_t m0, int64_t n0, int64_t m, int64_t n, int64_t l) {
     if (m == m0) return 0;
     int64_t cnt = 0, lm = highpow2 (m - m0);
+    cnt = sumr (lm + (m0^n0), l);
+    //cout << cnt << " " << (m0^n0) << endl;
+    cout << m0 << " " << n0 << " " << m << " " << n << endl;
+
+    if (l < (m0^n0)) {
+        cnt  -= sumr ((m0^n0), l);
+    }
 
     if ((n - n0) > lm) {
-        cnt = (sumr (lm + (m0^n0), l) - sumr ((m0^n0), l)) * lm;
+        cnt *= lm;
         cnt += calc (lm + m0, n0, m, lm + n0, l);
         cnt += calc (m0, lm + n0, lm + m0, n, l);
         cnt += calc (lm + m0, lm + n0, m, n, l);
 
     } else {
-      cnt = (sumr (lm + (m0^n0), l) - sumr ((m0^n0), l)) * (n - n0);
+        int64_t len = abs (n - n0);
+
+        if (len)
+            cnt *= len;
       //  cnt = (sumr (lm + (m0^n0), l)) * (n - n0);
-        // cout << sumr ((m0^n0), l);
     }
     return cnt;
 }
@@ -51,28 +61,20 @@ int64_t calc (int64_t m0, int64_t n0, int64_t m, int64_t n, int64_t l) {
 int64_t elder_age (int64_t m, int64_t n, int64_t l, int64_t t) {
 
     if (m == 0 || n == 0) return 0;
-    if (m < n) swap (m,n);
-
     int64_t cnt = 0;
 
     cnt = calc (0,0,m,n,l);
     /*
-    int64_t lm = highpow2 (m), ln = highpow2 (n);
-
-    if (lm == m) {
-        cnt = sumr (m, l) * n;
-
-    } else {
-
-        for (int64_t y = 0; y < n; y++) {
-          for (int64_t x = 0; x < m; x++) {
+    for (int64_t y = 0; y < n; y++) {
+        for (int64_t x = 0; x < m; x++) {
             int64_t num =  max (static_cast<int64_t> (0), (x ^ y) - l);
+            cout << setw (2) <<  num << " ";
             cnt += num;
-            cout << setw (2) << num << " ";
-          }
-          cout << endl;
         }
+        cout << endl;
     }
+
+    /*
     */
 
     return cnt % t;
@@ -87,17 +89,17 @@ int main () {
   int64_t m0 = 0, n0 = 0;
   int64_t lm = highpow2 (m - m0), cnt = 0;
 
-  cout << elder_age (7, 7 , 0,10000);  // 2
-
   /*
-   Assert::That(elder_age (8, 5, 1, 100), Equals(5));
-   Assert::That(elder_age (8,8,0,100007), Equals(224));
-
-  Assert::That(elder_age (25,31,0,100007), Equals(11925));
-  Assert::That(elder_age (5,45,3,1000007), Equals(4323));
-  Assert::That(elder_age(31,39,7,2345), Equals(1586));
-  Assert::That(elder_age (545,435,342,1000007), Equals(808451));
-  Assert::That (elder_age (28827050410, 35165045587, 7109602, 13719506), Equals(5456283));
+    Assert::That(elder_age (8, 5, 1, 100), Equals(5));
+    Assert::That(elder_age (8,8,0,100007), Equals(224));
+    Assert::That(elder_age (25,31,0,100007), Equals(11925));
+    Assert::That(elder_age (5,45,3,1000007), Equals(4323));
+    Assert::That(elder_age(31,39,7,2345), Equals(1586));
+    */
+    Assert::That (elder_age (28827050410, 35165045587, 7109602, 13719506), Equals(5456283));
+    /*
+    Assert::That(elder_age (545,435,342,1000007), Equals(808451));
+    /*
   */
 
 
