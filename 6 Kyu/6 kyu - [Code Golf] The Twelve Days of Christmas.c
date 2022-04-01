e370c77d3bcd21680e596ca6fa3e5a492fb999f7
Nemrod-7 <74612985+Nemrod-7@users.noreply.github.com>
2022-04-01 16:53:56 +0200

    Create 6 kyu - [Code Golf] The Twelve Days of Christmas.c

---
+++ 6 Kyu/6 kyu - [Code Golf] The Twelve Days of Christmas.c
@@ -0,0 +1,4 @@
+
+#include <stdio.h>
+#define a p+=sprintf(p,
+char s[2368],*n[]={"First","Second","Third","Four","Fif","Six","Seven","Eigh","Nin","Ten","Eleven","Twelf"},*d[]={"A partridge in a pear tree.\n","Two turtle doves, and","Three French hens,","Four calling birds,","Five gold rings,","Six geese a-lay","Seven swans a-swimm","Eight maids a-milk","Nine ladies danc","Ten lords a-leap","Eleven pipers pip","Twelve drummers drumm"};char*f(void){char*p=s,i=0,j;while(13>(j=++i)){a"On the %s%s day of Christmas\nMy true love sent to me\n",n[i-1],i>3?"th":"");while(j-->0)a"%s%s\n",d[j],j>4?"ing,":"");}*(p-2)='\0';return s;}
