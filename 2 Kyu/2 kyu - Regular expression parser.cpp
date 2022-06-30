31136ec8b15be1b05e4231894e6f3268686fb764
Nemrod-7 <ocopey@gmail.com>
2022-06-30 13:56:08 +0200

    update

--- 2 Kyu/2 kyu - Regular expression parser.cpp
+++ 2 Kyu/2 kyu - Regular expression parser.cpp
@@ -6,28 +6,29 @@
 #include <cstring>
 
 //#include "/home/wintermute/code/templates/Assert.hpp"
+enum {norm, point, star, choice};
 
 struct RegExp {
-    char op;
-    enum id {norm, any, zero, orr, str, add};
 
+    int id;
+    char op;
     RegExp *left, *right;
 
-    RegExp (const char &label = 0) : op (label), left (nullptr), right (nullptr) {}
-    RegExp (const char &label, RegExp *arg1, RegExp *arg2) : op (label), left (arg1), right (arg2) {}
+    RegExp (int type, RegExp *arg1, RegExp *arg2) : id (0), op (op), left (arg1), right (arg2) {}
+    RegExp (int type = 0, const char &op = 0) : id (type), op (op), left (nullptr), right (nullptr) {}
 };
 
-RegExp *normal (char c) {                     // ^ A character that is not in "()*|."
-    //return RegExp (c)
+RegExp *normal (char c) { // ^ A character that is not in "()*|.
+    return new RegExp (norm, c);
 }
 RegExp *any () { // ^ Any character
-
+    return new RegExp (point);
 }
 RegExp *zeroOrMore (RegExp *starred) {     // ^ Zero or more occurances of the same regexp
-
+    return new RegExp (star, starred, nullptr);
 }
 RegExp *orr (RegExp *left, RegExp *right) { // ^ A choice between 2 regexps
-
+    return new RegExp (choice, left, right);
 }
 RegExp *str (RegExp *first) {               // ^ A sequence of regexps, first element
 
@@ -36,7 +37,13 @@ RegExp *add (RegExp *str, RegExp *next) {   // ^ A sequence of regexps, addition
 
 }
 
+char *pretty (RegExp *re) {
+
+    return nullptr;
+}
+
 /*
+
 "ab*"     -> add (str (normal ('a')), zeroOrMore (normal ('b')))
 "(ab)*"   -> zeroOrMore (add (str (normal ('a')), normal ('b')))
 "ab|a"    -> orr (add (str (normal ('a')), normal ('b')), normal ('a'))
@@ -50,30 +57,37 @@ RegExp *add (RegExp *str, RegExp *next) {   // ^ A sequence of regexps, addition
 "(a.*)|(bb)" -> orr (add (str (normal ('a')), zeroOrMore (any ())), add (str (normal ('b')), normal (b)))
 
 */
+char isop (char c) { return (c == '(' || c == ')' || c =='*' || c == '|' || c == '.'); }
+bool check (const char *input) {
 
-std::string getid (std::string::iterator &it) {
-    std::string id;
+    char *expr = strdup (input), *it = expr;
+    const int size = strlen (expr);
+    if (size == 0) return false;
+    int par = 0;
 
-    while (isalnum (*it)) id += *it++;
+    while (*it) {
+        if (*it == '(')  par++;
+        if (*it == ')')  par--;
+        if (par < 0) return false;
+        if ((it - expr) < size) it++;
+    }
 
-    return id;
+    if (par != 0) return false;
+
+    return true;
 }
-std::string getsub (std::string::iterator &it) {
-    int pile = 1;
-    std::string sub;
+int getsub (char *it) {
+    int pile = 0, index = -1;
+    int size = strlen (it);
 
-    while (*it++) {
+    do {
         if (*it == '(') pile++;
         if (*it == ')') pile--;
-        if (pile == 0) return sub;
+        if (pile == 0) return index;
+        index++;
+    } while (*it++ != '\0');
 
-        sub += *it;
-    }
-    return sub;
-}
-
-char isop (char c) {
-    return (c == '(' || c == ')' || c =='*' || c == '|' || c == '.');
+    return size;
 }
 RegExp *getstack (std::stack<RegExp *> &stk) {
     if (stk.empty()) return nullptr;
@@ -81,36 +95,56 @@ RegExp *getstack (std::stack<RegExp *> &stk) {
     stk.pop();
     return ex;
 }
-RegExp *parseRegExp (const char *input) {
 
-    const int size = strlen (input);
+RegExp *parseRegExp (const char *code) {
+
+    if (check (code) == false) return nullptr;
+
+    const int size = strlen (code);
     int index = 0;
 
     std::stack<RegExp *> tree, ops;
 
     while (index < size) {
 
-        char tile = input[index];
+        char tile = code[index];
 
-        if (isalpha (tile)) {
-            tree.push (normal (tile));
+            if (isalpha (tile)) {
+            //tree.push (normal (tile));
+            printf ("[%c]", tile);
         } else if (tile == '|') {
+            /*
             RegExp *right = getstack (tree);
             RegExp *left = getstack (tree);
             tree.push (orr (left, right));
-
+            */
         } else if (tile == '.') {
-            tree.push (any ());
+            //tree.push (any ());
         } else if (tile == '*') {
-
+          /*
             RegExp * act = tree.top();
             tree.pop();
             tree.push (zeroOrMore(act));
-
+          */
         } else if (tile == '(') {
 
-        } else if (tile == ')') {
+            index++;
+            int pile = 1, it = 0;
+            char buffer[1024];
 
+            do {
+
+                if (code[index] == '(') pile++;
+                if (code[index] == ')') pile--;
+                if (pile == 0) break;
+                
+                buffer[it++] = code[index++];
+
+            } while (index < size);
+
+            buffer[it] = '\0';
+
+            printf ("%c", code[index]);
         } else {
 
         }
@@ -118,17 +152,21 @@ RegExp *parseRegExp (const char *input) {
         index++;
     }
 
-
     return !tree.empty() ? tree.top() : nullptr;
 }
 
 int main () {
+  /*
+    parseRegExp ("");
+    parseRegExp ("(");
+    parseRegExp (")(");
+    parseRegExp ("a(");
 
-}
+    parseRegExp ("*");
+    parseRegExp ("a**");
+    */
+    parseRegExp ("ab(cde)fgh");
 
-char *pretty (RegExp *r) {
-
-    return nullptr;
 }
 
 void shouldBe (const char *input, const char *expected) {
