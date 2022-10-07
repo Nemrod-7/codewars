78df126c1480aae602223484eb9ccf94e37d0c3d
Nemrod-7 <ocopey@gmail.com>
2022-10-07 12:30:08 +0200

    update

--- next/rust/target/debug/build/kernel32-sys-5d43251c293b1c10/build_script_build-5d43251c293b1c10.d
+++
@@ -1,5 +0,0 @@
-/home/wintermute/code/codewars/next/rust/target/debug/build/kernel32-sys-5d43251c293b1c10/build_script_build-5d43251c293b1c10: /home/wintermute/.cargo/registry/src/github.com-1ecc6299db9ec823/kernel32-sys-0.2.2/build.rs
-
-/home/wintermute/code/codewars/next/rust/target/debug/build/kernel32-sys-5d43251c293b1c10/build_script_build-5d43251c293b1c10.d: /home/wintermute/.cargo/registry/src/github.com-1ecc6299db9ec823/kernel32-sys-0.2.2/build.rs
-
-/home/wintermute/.cargo/registry/src/github.com-1ecc6299db9ec823/kernel32-sys-0.2.2/build.rs:
