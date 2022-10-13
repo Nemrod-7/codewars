305502794d8313858c7ded814ca93c85ee7728fd
Nemrod-7 <ocopey@gmail.com>
2022-10-13 19:01:48 +0200

    update

--- next/test.cpp
+++ next/test.cpp
@@ -1,101 +1,111 @@
 #include <iostream>
 #include <vector>
-#include <map>
-#include <algorithm>
 
 using namespace std;
 
-class Display {
-    public :
-        static void vect (vector<string> V) {
-            for (auto &it : V) {
-                cout << "["<<it<<"]";
+template<class T> T getval (vector<T> &vec) {
+    if (vec.empty()) return 0;
+    T val = vec.back();
+    vec.pop_back();
+    return val;
+}
+vector<string> tokenize (string src) {
+
+    vector<string> vect;
+
+    for (int i = 0; i < src.size(); i++) {
+        while (src[i] == ' ') i++;
+        string buff;
+
+        while (i < src.size() && src[i] != ' ') buff += src[i++];
+
+        vect.push_back(buff);
+    }
+
+    return vect;
+}
+int calc (vector<string> equ) {
+
+    int it = 0;
+    vector<int> vals;
+    vector<string> op;
+
+    while (it < equ.size()) {
+
+        if (equ[it] == "x") {
+            vals.push_back(0);
+        } else if (equ[it] == "+" || equ[it] == "-") {
+            if (vals.size() > 1 && op.size() > 0) {
+                string oper = getval(op);
+                int b = getval (vals), a = getval (vals);
+                vals.push_back ((oper == "-" ? a - b : a + b));
             }
-            cout << endl;
+            op.push_back(equ[it]);
+        } else {
+            vals.push_back(stoi(equ[it]));
         }
-};
 
-string differentiate2 (const string &poly) {
-
-    if (poly.find('x') == string::npos) return "0";
-
-    string derivative;
-    int dig = 1, exp = 1;
-
-    if (poly[0] == 'x') {
-        sscanf (poly.c_str(), "x^d", &exp);
-    } else if (poly[0] == '-' && poly[1] == 'x') {
-        sscanf (poly.c_str(), "-x^d", &exp);
-    } else {
-        sscanf (poly.c_str(), "dx^d", &dig, &exp);
+        it++;
     }
 
-    dig *= exp, exp -= 1;
+    while (op.size()) {
+        string oper = getval(op);
+        int b = getval (vals), a = getval (vals);
 
-    if (exp) {
-        if (dig == -1) {
-          derivative += "-";
-        } else if (dig < -1 || dig > 1) {
-          derivative += to_string (dig);
-        }
-        derivative += "x";
-    } else {
-        derivative = to_string (dig);
+        vals.push_back ((oper == "-" ? a - b : a + b));
     }
 
-    return derivative;
+    return vals.back();
 }
+int solve (const string &equ) {
 
-/*
+    int res = 0;
+    int equ = equ.find("="), ex = equ.find ("x");
 
-<constant>  ::= [0-9]+ ( "." [0-9]+ )?
-<variable>  ::= "x"
-<func_name> ::= "sin" | "cos" | "tan" | "cot" | "log"
+    vector<string> first = tokenize (equ.substr (0, equ));
+    vector<string> secnd = tokenize (equ.substr (equ + 1));
 
-<expression> ::= <term> ( ( "+" | "-" ) <term> )*
-<term>       ::= <factor> ( ( "*" | "/" ) <factor> )*
-<factor>     ::= <basic> ( "^" <basic> )*
-<func_call>  ::= <func_name> "(" <expression> ")"
-<basic>      ::= <constant> | <variable> | <func_call> | ( "(" <expression> ")" )
+    if (first[0] == "-" && secnd[0] == "-") {
+        first[0] = "+", secnd[0] = "+";
+    }
 
+    int left = calc(first), right = calc (secnd);
 
-const auto f = differentiate("2 * x^3");
-std::get<0>(f)({ 2, 2 }) == (-32, 32)
-  Because:
-  f(x) = 2 * x^3
-  f(2 + 2i) = 2 * (2 + 2i)^3 = -32 + 32i
+    if (ex < equ) {
+        res = right - left;
+    } else {
+        res = left - right;
+    }
 
-
-std::get<1>(f)({ 2, 2 }) == (0, 48)
-  Because:
-  f'(x) = (2 * x^3)' = 6 * x^2
-  f'(2 + 2i) = 6 * (2 + 2i)^2 = 48i
-
-
-std::get<2>(f)({ 2, 2 }) == (24, 24)
-  Because:
-  f''(x) = (6 * x^2)' = 12 * x
-  f''(2 + 2i) = 12 * (2 + 2i)^2 = 24 + 24i
-
-*/
-string differentiate (const string &poly) {
-
-    string derivative;
-
-    return derivative;
-}
-int main () {
-
-  /*
-    "3x^2"  => "6x"
-    "-5x^3" => "-15x^2"
-    "6x^-2" => "-12x^-3"
-    "5x"    => "5"
-    "-x"    => "-1"
-    "42"    => "0"
-
+    /*
+    cout << left << "=" << right << " => " << res << "\n";
     */
-    cout << differentiate ("x");
-
-    //Display::vect(expr);
+    return res;
 }
+
+int main () {
+    /*
+    solve ("x + 1 = 9 - 2"); // 6
+    solve ("- 10 = x");      // -10
+    solve ("x - 2 + 3 = 2"); // 1
+    solve ("- x = - 1");     // 1
+    */
+    int num = -0;
+
+
+    cout << num;
+    //Display::vect (first);
+
+}
+
+
+
+class Display {
+  public :
+  static void vect (vector<string> V) {
+    for (auto &it : V) {
+      cout << "["<<it<<"]";
+    }
+    cout << endl;
+  }
+};
