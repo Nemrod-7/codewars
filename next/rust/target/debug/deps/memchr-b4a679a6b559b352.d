59d7259c108331322a5a74bc9fd645f627a18651
Nemrod-7 <ocopey@gmail.com>
2022-10-28 17:55:36 +0200

    udate

--- next/rust/target/debug/deps/memchr-b4a679a6b559b352.d
+++
@@ -1,7 +0,0 @@
-/home/wintermute/code/codewars/next/rust/target/debug/deps/memchr-b4a679a6b559b352.rmeta: /home/wintermute/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-0.1.11/src/lib.rs
-
-/home/wintermute/code/codewars/next/rust/target/debug/deps/libmemchr-b4a679a6b559b352.rlib: /home/wintermute/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-0.1.11/src/lib.rs
-
-/home/wintermute/code/codewars/next/rust/target/debug/deps/memchr-b4a679a6b559b352.d: /home/wintermute/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-0.1.11/src/lib.rs
-
-/home/wintermute/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-0.1.11/src/lib.rs:
--- next/rust/target/debug/deps/thread_id-a8e2d28ba208a216.d
+++
@@ -1,7 +0,0 @@
-/home/wintermute/code/codewars/next/rust/target/debug/deps/thread_id-a8e2d28ba208a216.rmeta: /home/wintermute/.cargo/registry/src/github.com-1ecc6299db9ec823/thread-id-2.0.0/src/lib.rs
-
-/home/wintermute/code/codewars/next/rust/target/debug/deps/libthread_id-a8e2d28ba208a216.rlib: /home/wintermute/.cargo/registry/src/github.com-1ecc6299db9ec823/thread-id-2.0.0/src/lib.rs
-
-/home/wintermute/code/codewars/next/rust/target/debug/deps/thread_id-a8e2d28ba208a216.d: /home/wintermute/.cargo/registry/src/github.com-1ecc6299db9ec823/thread-id-2.0.0/src/lib.rs
-
-/home/wintermute/.cargo/registry/src/github.com-1ecc6299db9ec823/thread-id-2.0.0/src/lib.rs:
