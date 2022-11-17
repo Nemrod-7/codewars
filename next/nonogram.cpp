98b761300d04a3a87a09ee70b3f260676f00818e
Nemrod-7 <ocopey@gmail.com>
2022-11-17 18:45:45 +0100

    update

--- next/nonogram.cpp
+++ next/nonogram.cpp
@@ -4,43 +4,100 @@
 #include <algorithm>
 #include <bitset>
 #include <cmath>
+#include <chrono>
 
 using namespace std;
+
 // NxN nonogram solver
 class Nonogram {
     private :
-        int width, height;
+        size_t width, height;
+
         vector<vector<bool>> grid;
         vector<vector<int>> cspx, cspy;
+				vector<vector<int>> top1, left1;
 
-        vector<vector<int>> mkcomb (vector<vector<int>> top) {
+        vector<vector<int>> mkcomb (vector<vector<int>> top, int nd) {
 
             int size = top.size(), end = 1;
-            vector<vector<int>> csp (size);
+            vector<vector<int>> csp (top.size());
 
-            for (int i = 0; i < size; i++)
+            for (int i = 0; i < nd; i++)
                 end <<= 1;
-
+						//cout << end << endl;
             for (int num = 0; num < end; num++) {
                 int acc = 0;
                 vector<int> line;
-
-                for (int i = 0; i < size; i++) {
-                    bool bit = static_cast<bool> (num &1 << i);
+								//cout << num << " => ";
+                for (int i = 0; i < nd; i++) {
+                    bool bit = static_cast<bool> (num >> i &1);
+										//cout << bit;
                     acc += bit;
-                    if ((bit == 0 || i == size - 1) && acc != 0) {
+                    if ((bit == 0 || i == nd - 1) && acc != 0) {
                         line.push_back(acc);
                         acc = 0;
                     }
                 }
+								/*
+								cout << " :: ";
+								for (auto it : line) {
+										cout << it << ' ';
+								}
+								cout << endl;
+								*/
 
                 for (int i = 0; i < size; i++) {
                     if (top[i] == line) csp[i].push_back(num);
                 }
             }
+
             return csp;
         }
+				void filter () { // reduce bitmasking
+												 //
+						for (size_t y = 0; y < height; y++) {
 
+								for (size_t x = 0; x < width; x++) {
+
+										if (cspy[y].size() == 1) {
+												int row = cspy[y].front();
+												int bit = row >> x &1;
+												//grid[y][x] = bit; 
+
+												/*
+												for (auto it = cspx[x].begin(); it != cspx[x].end();) {
+														int num = *it;
+
+														if (bity != (num >> y &1)) {
+																cspx[x].erase(it);
+														} else {
+																it++;
+														}
+												}
+												*/
+										}
+
+										if (cspx[x].size() == 1) {
+												int col = cspx[x].front();
+
+												/*
+												for (auto it = cspy[y].begin(); it != cspy[y].end();) {
+														int num = *it;
+
+														if (col != (num >> y &1)) {
+																cspy[y].erase(it);
+														} else {
+																it++;
+														}
+												}
+												*/
+										}
+								}
+								//cout << endl;
+								//cout << cspy[y].size() << "\n";
+						}
+
+				}
         void show (vector<vector<int>> top, vector<vector<int>> left) {
             size_t padx = 0, pady = 0;
 
@@ -52,7 +109,7 @@ class Nonogram {
             }
 
             while (padx-->0) {
-                for (int i = 0; i < pady; i++) {
+                for (size_t i = 0; i < pady; i++) {
                     cout << setw(2) << " " << "|";
                 }
                 for (auto &csp : top) {
@@ -66,14 +123,14 @@ class Nonogram {
                 cout << setw(2) << endl;
             }
 
-            for (int y = 0; y < height; y++) {
-                int space = pady - left[y].size();
+            for (size_t y = 0; y < height; y++) {
+                size_t space = pady - left[y].size();
                 while (space-->0) cout << setw(3) << "|";
 
                 for (auto cspy : left[y])
                     cout << setw(2) << cspy << "|";
 
-                for (int x = 0; x < width; x++) {
+                for (size_t x = 0; x < width; x++) {
                     if (grid[y][x]) {
                         cout << setw(2) << "#";
                     } else {
@@ -86,29 +143,74 @@ class Nonogram {
         }
     public :
         Nonogram (vector<vector<int>> top, vector<vector<int>> left) {
+
             width = top.size(), height = left.size();
             grid.resize(height, vector<bool> (width));
 
-            cspx = mkcomb(top), cspy = mkcomb (left);
+						top1 = top, left1 = left;
 
-            cout << cspy[8].front() << endl;
-            /*
-            for (auto csp : cspy) {
-                cout << csp.size() << endl;
-            }
-            */
-            show (top, left);
+            cspx = mkcomb(top, height);
+						cspy = mkcomb(left, width);
+
+						filter ();
+            //show (top, left);
         }
+
+				bool backtrack (vector<vector<bool>> &grid, const vector<vector<int>> &cspy, const vector<vector<int>> &cspx, int index) {
+
+						if (index == height) {
+
+								for (int x = 0; x < width; x++) {
+										int num = 0;
+
+										for (int y = 0; y < height; y++) {
+												num |= grid[y][x] << y;
+										}
+
+										if (find (cspx[x].begin(), cspx[x].end(), num) == cspx[x].end()) {
+												return false;
+										}
+								}
+								//Display::board(grid);
+								return true;
+						}
+
+						for (auto &num : cspy[index]) {
+								for (int x = 0; x < width; x++) {
+										grid[index][x] = num >> x &1;
+								}
+
+								if (backtrack (grid, cspy, cspx, index + 1) == true) {
+										return true;
+								}
+						}
+
+						return false;
+				}
+
+				vector<vector<bool>> solve () {
+						
+						backtrack (grid, cspy, cspx, 0);
+						//show (top1,left1);
+
+						for (int y = 0; y < height; y++) {
+								for (int x = 0; x < width; x++) {
+										cout << (grid[y][x] ? "#" : " ");
+								}
+								cout << endl;
+						}
+						return grid;
+				}
 
         static pair<vector<vector<int>>,vector<vector<int>>> encoder (vector<vector<int>> grid) {
 
             vector<vector<int>> left, top;
 
-            for (int y = 0; y < grid.size(); y++) {
+            for (size_t y = 0; y < grid.size(); y++) {
                 int numy = 0, numx = 0;
                 vector<int> line, col;
 
-                for (int x = 0; x < grid[0].size(); x++) {
+                for (size_t x = 0; x < grid[0].size(); x++) {
                     numy += grid[y][x], numx += grid[x][y];
 
                     if ((grid[y][x] == 0 || x == grid[0].size() - 1) && numy != 0) {
@@ -135,7 +237,7 @@ vector<bool> to_bset (int x) {
     vector<bool> bset;
 
     for (int i = 0; i < 5; i++)
-        bset.push_back(static_cast<bool>(x&1 << i));
+        bset.push_back(x >> i &1);
 
     return bset;
 }
@@ -143,110 +245,57 @@ int to_int (vector<bool> bset) {
 
     int num = 0;
 
-    for (int i = 0; i < 5; i++)
+    for (size_t i = 0; i < bset.size(); i++)
         num |= bset[i] << i;
 
     return num;
 }
 
-vector<int> mkcomb (int num) {
+int main () {
 
-    vector<int> comb;
-    int acc = 0;
+		auto start = chrono::high_resolution_clock::now();
 
-    for (int i = 0; i < 5; i++) {
-        bool bit = static_cast<bool> (num &1 << i);
-        acc += bit;
-        if ((bit == 0 || i == 4) && acc != 0) {
-            comb.push_back(acc);
-            acc = 0;
-        }
-    }
-
-    return comb;
-}
-
-bool backtrack (vector<vector<bool>> &grid, const vector<vector<int>> &cspy, const vector<vector<int>> &cspx, int index) {
-
-    if (index == 5) {
-
-        for (int x = 0; x < 5; x++) {
-            int num = 0;
-
-            for (int y = 0; y < 5; y++) {
-                num |= grid[y][x] << y;
-            }
-
-            if (find (cspx[x].begin(), cspx[x].end(), num) == cspx[x].end()) {
-                return false;
-            }
-        }
-        //Display::board(grid);
-
-        return true;
-    }
-
-    for (auto &num : cspy[index]) {
-        for (int x = 0; x < 5; x++) {
-            grid[index][x] = static_cast<bool>(num &1 << x);
-        }
-
-        if (backtrack (grid, cspy, cspx, index + 1) == true) {
-            return true;
-        }
-    }
-
-    return false;
-}
-vector<vector<bool>> solver (vector<vector<int>> top, vector<vector<int>> left) {
-
-    vector<vector<int>> cspy(5), cspx(5);
-    vector<vector<bool>> grid (5, vector<bool> (5));
-
-    // making simple bitmasking
-    for (int num = 0; num < 32; num++) {
-        vector<int> comb = mkcomb (num);
-        for (int i = 0; i < 5; i++) {
-            if (top[i] == comb) cspx[i].push_back(num);
-            if (left[i] == comb) cspy[i].push_back(num);
-        }
-        //Display::bitnum (i);
-    }
+		vector<vector<int>> top = {{1,1},{4},{1,1,1},{3},{1}};
+    vector<vector<int>> left = {{1},{2},{3}, {2,1},{4}};
 
 
-    /*
-    // reduce bitmasking
-    for (int y = 0; y < 5; y++) {
-        vector<int> line;
-        for (int x = 0; x < 5; x++) {
-            int col = cspx[x].front();
+    vector<vector<int>> grid = {
+    {0, 0, 0, 1, 0, 0, 0, 1, 1, 0},
+    {0, 0, 1, 1, 1, 0, 1, 1, 1, 1},
+    {0, 0, 1, 1, 1, 1, 1, 1, 1, 1},
+    {0, 0, 0, 1, 1, 1, 1, 1, 1, 0},
+    {0, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+    {0, 1, 0, 0, 0, 0, 1, 1, 0, 0},
+    {1, 0, 1, 0, 0, 0, 1, 1, 0, 0},
+    {1, 1, 1, 0, 0, 1, 1, 0, 0, 0},
+    {1, 1, 1, 0, 0, 1, 1, 1, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
 
-            if (cspx[x].size() == 1) {
-                for (auto &cmy : cspy[y]) {
+		/*
+		*/
+    //auto [top1,left1] = encoder (grid);
+    top = {{3}, {4}, {2, 2, 2}, {2, 4, 2}, {6}, {3}};
+    left = {{4}, {6}, {2, 2}, {2, 2}, {2}, {2}, {2}, {2}, {}, {2}, {2}};
 
-                    if (static_cast<bool>(cmy &1 << x) == static_cast<bool>(col&1 << y))
-                        line.push_back(cmy);
-                }
-            }
-        }
-        cspy[y] = line;
-    }
-    */
-    backtrack (grid, cspy, cspx, 0);
+    Nonogram board (top,left);
+		//board.solve();
 
-    //Display::nonogram (grid ,top,left);
-    return grid;
+
+		auto end = chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    cout << "\nProcess took " << elapsed.count()  << " ms" << endl;
+
 }
 
 pair<vector<vector<int>>,vector<vector<int>>> encoder (vector<vector<int>> grid) {
 
     vector<vector<int>> left, top;
 
-    for (int y = 0; y < grid.size(); y++) {
+    for (size_t y = 0; y < grid.size(); y++) {
         int numy = 0, numx = 0;
         vector<int> line, col;
 
-        for (int x = 0; x < grid[0].size(); x++) {
+        for (size_t x = 0; x < grid[0].size(); x++) {
             numy += grid[y][x], numx += grid[x][y];
 
             if ((grid[y][x] == 0 || x == grid[0].size() - 1) && numy != 0) {
@@ -267,105 +316,3 @@ pair<vector<vector<int>>,vector<vector<int>>> encoder (vector<vector<int>> grid)
     return {top,left};
 }
 
-int main () {
-
-    vector<vector<int>> top = {{1,1},{4},{1,1,1},{3},{1}};
-    vector<vector<int>> left = {{1},{2},{3}, {2,1},{4}};
-
-
-    vector<vector<int>> grid = {
-    {0, 0, 0, 1, 0, 0, 0, 1, 1, 0},
-    {0, 0, 1, 1, 1, 0, 1, 1, 1, 1},
-    {0, 0, 1, 1, 1, 1, 1, 1, 1, 1},
-    {0, 0, 0, 1, 1, 1, 1, 1, 1, 0},
-    {0, 0, 0, 0, 0, 1, 1, 0, 0, 0},
-    {0, 1, 0, 0, 0, 0, 1, 1, 0, 0},
-    {1, 0, 1, 0, 0, 0, 1, 1, 0, 0},
-    {1, 1, 1, 0, 0, 1, 1, 0, 0, 0},
-    {1, 1, 1, 0, 0, 1, 1, 1, 0, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
-
-    auto [top1,left1] = encoder (grid);
-    top = {{3}, {4}, {2, 2, 2}, {2, 4, 2}, {6}, {3}};
-    left = {{4}, {6}, {2, 2}, {2, 2}, {2}, {2}, {2}, {2}, {}, {2}, {2}};
-             /*
-    */
-    Nonogram graph (top,left);
-    //solver (top,left);
-
-    /*
-    for (int num = 0; num < 32; num++) {
-        bitset<5> mask (num);
-
-        for (int i = 0; i< 5; i++) {
-            bool bit = mask[i];
-            cout << bit;
-        }
-        cout << endl;
-        //cout << num << " => " << mask.to_string() << "\n";
-        //int cell = mask;
-    }
-    */
-}
-
-class Display {
-    public :
-        static void board (vector<vector<bool>> grdi) {
-            cout << endl;
-            for (auto line : grdi) {
-                for (auto cell : line) {
-                    cout << cell << ' ';
-                }
-                cout << endl;
-            }
-        }
-        static void readcomb (int num) {
-            for (int i = 0; i < 5; i++) {
-                bool bit = num&1 << i;
-                cout << bit;
-            }
-            cout << endl;
-        }
-        static void nonogram (vector<vector<bool>> grid, vector<vector<int>> top, vector<vector<int>> left) {
-            size_t padx = 0, pady = 0;
-
-            for (auto csp : top) {
-                padx = max (csp.size(), padx);
-            }
-            for (auto csp : left) {
-                pady = max (csp.size(), pady);
-            }
-            while (padx-->0) {
-                for (int i = 0; i < pady; i++) {
-                    cout << " " << "|";
-                }
-                for (auto &csp : top) {
-                    if (csp.size() > padx) {
-                        cout << csp[padx];
-                    } else {
-                        cout << " ";
-                    }
-                    cout << "|";
-                }
-                cout << endl;
-            }
-
-            for (int y = 0; y < 5; y++) {
-                int space = pady - left[y].size();
-                while (space-->0) cout << " |";
-
-                for (auto cspy : left[y])
-                    cout << cspy << "|";
-
-                for (int x = 0; x < 5; x++) {
-                    if (grid[y][x]) {
-                        cout << "#";
-                    } else {
-                        cout << " ";
-                    }
-                    cout << "|";
-                }
-                cout << endl;
-            }
-        }
-};
--- rust/main.rs
+++ rust/main.rs
@@ -1,114 +1,233 @@
-use std::sync::Mutex;
-static MEM:Mutex<Vec<u64>> = Mutex::new(Vec::new());
 
-fn is_prime (num: u64) -> bool {
+fn readcomb (num:u8) {
 
-    match num {
-        1 => return false,
-        2 => return true,
-        3 => return true,
-        _ => {
-            if num % 2 == 0 || num % 3 == 0 { return false }
-            let mut i = 5;
-            while i * i <= num {
-                if num % i == 0 || num % (i + 2) == 0 { return false }
-                i += 6;
-            }
-        },
+    for i in 0..5 {
+        let bit =  num >> i &1;
+        print!("{bit}");
     }
-     true
-}
-fn rev (mut num: u64) -> u64 {
-    
-    let mut ret:u64 = 0;
-    
-    while num != 0 {
-        let prod = ret * 10;
-        ret = prod + num % 10;
-        num /= 10;
-    }
-    
-    ret
-}
-fn sq_cub_rev_prime (n: u32) -> u32 {
-    
-    let n = n as usize;
-    let mut num;
-    let mut seq = MEM.lock().unwrap();
-
-    if seq.len() >= n { return seq[n - 1] as u32 }
-
-    match seq.last() {
-        Some (val) => num = *val,
-        None => num = 88,
-    }
-  
-    while seq.len() != n {
-        num += 1;
-        let sqr = num * num;
-        let cub = sqr * num;
-
-        if is_prime (rev(sqr)) == true && is_prime (rev(cub)) == true {
-            seq.push(num);
-        }
-    }
-
-    num as u32
-}
-fn show (n:u32) {
-
-    let res = sq_cub_rev_prime (n);
-    print!("[{n}] => {res}\n");
-}
-fn main () {
-    
-    let ve:Vec<u64> = Vec::with_capacity(250);
-
-    show(50); // 10499
-    show(1); 
-    show(50); // 10499
-
-
-    for k in 1..16 {
-    //    sq_cub_rev_prime (k);
-    }
-
-
     print!("\n");
 }
 
+fn mkcomb (num: u8) -> Vec<u8> {
+
+    let mut comb:Vec<u8> = Vec::new();
+    let mut acc = 0;
+
+    for i in 0..5 {
+        let bit =  num >> i &1;
+        acc += bit;
+
+        if (bit == 0 || i == 4) && acc != 0 {
+            comb.push(acc);
+            acc = 0;
+        }
+    }
+
+    comb
+}
+fn reduce (grid:&mut [[u8;5];5], (vertical, lateral): (&Vec<Vec<u8>>, &Vec<Vec<u8>>)) {
+
+    for y in 0..5 {
+        if vertical[y].len() == 1 {
+            let num = vertical[y][0];
+
+            for x in 0..5 {
+                grid[x][y] = num >> x &1; // if (num&1 << x) != 0 { 1 } else { 0 };
+            }
+        }
+        if lateral[y].len() == 1 {
+            let num = lateral[y][0];
+
+            for x in 0..5 {
+                grid[y][x] = num >> x &1;
+            }
+        }
+    }
+ }
+fn filter (grid:&mut [[u8;5];5], (vertical, lateral): (&Vec<Vec<u8>>, &Vec<Vec<u8>>)) {
 /*
-
-struct An {
-    n: i64,
-    elements: Vec<i64>,
-    generated: bool,
-}
-impl An {
-    fn new() -> An {
-        An {
-            n: 1,
-            elements: vec![7],
-            generated: false,
+    for y in 0..5 {
+        for x in 0..5 {
+            if grid[y][x] == 1 {
+                lateral[y] = lateral[y].iter().filter(|&&cmb| (cmb&1 << x) != 0).cloned().collect::<Vec<_>>();
+                vertical[x] = vertical[x].iter().filter(|&&cmb| (cmb&1 << y) != 0).cloned().collect::<Vec<_>>();
+            }
         }
     }
+    */
 }
-impl Iterator for An {
-    type Item = i64;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.generated {
-            let previous_last = *(self.elements.last().unwrap_or(&7i64));
+fn backtrack2 (grid: &mut [[u8;5];5], (top, left): (&Vec<Vec<u8>>, &Vec<Vec<u8>>), y:usize) -> bool { 
 
-            self.elements
-                .push(previous_last + gcd(self.n, previous_last));
+    if y == 5 {
+
+        for x in 0..5 {
+            let mut num = 0;
+
+            for i in 0..5 {
+                num |= grid[i][x] << i;
+            }
+           
+            if top[x].iter().position(|&p| p == num) == None { return false; }
         }
-        self.n += 1;
-        self.generated = true;
-        Some(*(self.elements.last().unwrap()))
+
+        return true;
     }
+
+    for mask in &left[y] {
+        for x in 0..5 {
+            grid[y][x] = mask >> x &1; 
+        }
+
+        if backtrack2 (grid, (top,left), y + 1) == true {
+            return true;
+        }
+    }
+ 
+    return false;
 }
+
+fn solve_nonogram ((top, left): ([&[u8]; 5], [&[u8]; 5])) -> [[u8; 5]; 5] {
+
+
+    let mut grid = [[0;5];5];
+
+    let mut lateral = vec![vec![];5];
+    let mut vertical = vec![vec![];5];
+
+    for num in 0..32 {
+        let comb = mkcomb (num);
+        
+        for i in 0..5 {
+            if comb == left[i] { lateral[i].push(num); }
+            if comb == top[i] { vertical[i].push(num); }
+        }
+    }
+
+    //reduce (&mut grid, (&vertical,&lateral));
+    backtrack2 (&mut grid, (&vertical,&lateral), 0);
+   // backtrack (&mut grid, (top,left), 0, 0);
+    print!("\n");
+
+    for i in 0..5 {
+        for j in 0..5 {
+            let cell = grid[i][j];
+            print!("{cell} ");
+        }
+        print!("\n");
+    }
+
+    grid
+}
+
+fn main () {
+
+    let n = 5;
+
+    solve_nonogram(([&[1, 1], &[4], &[1, 1, 1], &[3], &[1]], [&[1], &[2], &[3], &[2, 1], &[4]]));
+   
+    //csp.retain(|&num| (num&1 << x) != 1);
+
+        /*
+ [1,1,1], [1,3] [2,2], [3,1],[5]
+   
+    grid = 
+[[0, 0, 1, 0, 0],
+ [1, 1, 0, 0, 0],
+ [0, 1, 1, 1, 0],
+ [1, 1, 0, 1, 0],
+ [0, 1, 1, 1, 1]];
 
 */
+
+}
+////////////////////////////////////////Arkive///////////////////////////////////////////////
+fn getline (grid:&[[u8;5];5], y:usize) -> Vec<u8> {
+
+    let mut acc = 0;
+    let mut line = Vec::new();
+
+    for i in 0..5 {
+        let cell = grid[y][i];
+        acc += cell;
+        
+        if (cell == 0 || i == 4) && acc != 0 {
+            line.push(acc);
+            acc = 0;
+        }
+    }    
+
+    line
+}
+fn getcol (grid:&[[u8;5];5], x:usize) -> Vec<u8> {
+
+    let mut acc = 0;
+    let mut line = Vec::new();
+
+    for i in 0..5 {
+        let cell = grid[i][x];
+        acc += cell;
+        
+        if (cell == 0 || i == 4) && acc != 0 {
+            line.push(acc);
+            acc = 0;
+        }
+    }    
+
+    line
+}
+fn isvalid (grid:&[[u8;5];5], (top, left): ([&[u8]; 5], [&[u8]; 5])) -> bool {
+
+    for i in 0..5 {
+        if getline (&grid, i) != left[i] || getcol (&grid, i)  != top[i] {
+            return false
+        }
+    }
+
+    true
+}
+
+fn backtrack (grid:&mut [[u8;5];5], (top, left): ([&[u8]; 5], [&[u8]; 5]), mut x:usize, mut y:usize) -> bool {
+
+    if x == 5 {
+        if getline(grid, y) != left[y] { return false }
+        y += 1;
+        x = 0;
+    }
+
+    if y == 5 {
+        return true
+    }
+
+    grid[y][x] = 1;
+
+    if backtrack(grid, (top,left), x + 1, y) == true {
+        return true
+    }
+
+    grid[y][x] = 0;
+
+    return false;
+}
+fn to_bin (mask:u32) -> Vec<u8> {
+
+    let mut bset:Vec<u8> = vec![];
+
+    for i in 0..5 {
+        bset[i] = (mask >> i &1) as u8;
+    }
+
+    bset
+}
+fn to_int (bset:[u8;5]) -> u32 {
+
+    let mut mask = 0;
+
+    for i in 0..5 {
+        mask |= (bset[i] << i) as u32;
+    }
+
+    mask
+}
 
 
