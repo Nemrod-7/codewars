fa0db36e1e7edfb3152d464f2670832b7fa649d3
Nemrod-7 <ocopey@gmail.com>
2022-12-08 18:58:18 +0100

    update

--- rectangle_area.cpp
+++ rectangle_area.cpp
@@ -21,6 +21,7 @@ class Assert {
             }
         }
 };
+void Test();
 
 class check {
     public :
@@ -32,14 +33,55 @@ class check {
 
 using point = pair<int,int>;
 using rectangle = vector<int>;
-using node = pair<int,point>;
+
+void display (vector<vector<int>> graph) {
+
+    int maxx = 0, maxy = 0;
+    for (auto rec : graph) {
+        maxx = max (maxx, rec[2]);
+        maxy = max (maxy, rec[3]);
+    }
+
+    vector<vector<int>> grid(maxy + 1, vector<int>(maxx + 1));
+
+    for (int i = 0; i < graph.size(); i++) {
+        rectangle r = graph[i];
+        point a = {r[0], r[1]};
+        point b = {r[2], r[3]};
+
+        for (int y = r[1]; y <= r[3]; y++) {
+            grid[y][r[0]] = 1;
+            grid[y][r[2]] = 1;
+        }
+        for (int x = r[0]; x <= r[2]; x++) {
+            grid[r[1]][x] = 1;
+            grid[r[3]][x] = 1;
+        }
+
+    }
+
+
+    for (int y = 0; y < grid.size(); y++) {
+        for (int x = 0; x < grid[0].size(); x++) {
+            int cell = grid[y][x];
+
+            if (cell == 1) {
+                cout << cell << cell;
+            } else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+}
 
 int area (rectangle rec) { return abs (rec[2] - rec[0]) * abs (rec[3] - rec[1]); }
 bool is_in_rect (int px, int py, rectangle r) { return (px >= r[0] && px <= r[3] && py >= r[1] && py <= r[4]); }
 bool overlap (rectangle r1, rectangle r2) {
     return r1[0] <= r2[2] && r2[0] <= r1[2] && r1[1] <= r2[3] && r2[1] <= r1[3]; 
 }
-
 int overarea(rectangle r1, rectangle r2) {
     int width = min(r1[2], r2[2])- max(r1[0], r2[0]);
     int height = min(r1[3], r2[3]) - max(r1[1], r2[1]);
@@ -48,123 +90,191 @@ int overarea(rectangle r1, rectangle r2) {
 }
 int calculate (vector<vector<int>> graph) {
 
-    int total = 0;
-
-    for (auto rec : graph) {
-        int h = abs(rec[0] - rec[2]), l = abs(rec[1] - rec[3]);
-        int area = h * l;
-        total += area;
-
-    }
-
-    for (int i = 0; i < graph.size(); i++) {
-        for (int j = i + 1; j < graph.size(); j++) {
-
-            if (overlap (graph[i], graph[j]) == true) {
-                int over = overarea (graph[i], graph[j]);
-                total -= over;
-            }
-        }
-    }
-    cout << total << ' ';
-
-    return total;
-}
-
-int sweepline (vector<vector<int>> graph) {
-
-    map<int,vector<int>> sweep;
+    display (graph);
+    int area = 0;
+    map<int,vector<int>> hist;
+    set<int> event;
 
     for(int i = 0; i < graph.size(); i++) {
         rectangle r = graph[i];
         int x1 = r[0], x2 = r[2];
-        sweep[x1].push_back(i);
-        sweep[x2].push_back(i);
-
+        hist[x1].push_back(i);
+        hist[x2].push_back(i);
     }
 
-    vector<pair<int, vector<int>>> line = {sweep.begin(), sweep.end()};
-    pair<int, pair<int,int>> segment;
+    vector<pair<int, vector<int>>> line = {hist.begin(), hist.end()};
 
     for (int i = 0; i < line.size(); i++) {
         auto [x, rec] = line[i];
-        int m = rec.size();
+        //cout << x << " => ";
+        if (event.size()) {
 
+            int x2 = x, x1 = line[i - 1].first;
+            int y1 = 999, y2 = 0;
+            for (auto it : event) {
+                rectangle r2 = graph[it];
+                y1 = min (y1, r2[1]);
+                y2 = max (y2, r2[3]);
+            }
+            //cout << "abs("<< x << "-" << line[i - 1].first << ") * abs(" << y2 << "-" << y1 << ") " ;
+            int res = abs(x2-x1) * abs(y2-y1);
+            area += res;
+        }
+        /*
+        */
+        for (int j = 0; j < rec.size(); j++) {
+            rectangle r1 = graph[rec[j]];
+
+            if (x == r1[0]) event.insert(rec[j]); 
+            if (x == r1[2]) event.erase(rec[j]);
+        }
+        //cout << endl;
+    }
+
+
+    return area;
+}
+
+vector<pair<int,vector<int>>> mksweep (const vector<vector<int>> &graph) {
+
+    map<int,vector<int>> hist;
+
+    for(int i = 0; i < graph.size(); i++) {
+        rectangle r = graph[i];
+        int x1 = r[0], x2 = r[2];
+        hist[x1].push_back(i);
+        hist[x2].push_back(i);
+    }
+
+    return {hist.begin(), hist.end()};
+}
+int sweep (vector<vector<int>> graph) {
+
+    int area = 0;
+    set<int> event;
+    vector<pair<int, vector<int>>> line = mksweep(graph);
+
+    for (int i = 0; i < line.size(); i++) {
+        auto [x, rec] = line[i];
         cout << x << " => ";
-        //auto [x1, y1, y2] = segment;
 
-        int miny = 999, maxy = 0;
+        if (event.size()) {
 
-        for (int j = 0; j < m; j++) {
-            rectangle rr = graph[rec[j]];
+            int x2 = x, x1 = line[i - 1].first;
 
-            cout << "[" << rr[1] << "," << rr[3] << "] ";
+            set<int> vert;
+
+            for (auto it : event) {
+                rectangle r2 = graph[it];
+                vert.insert(r2[1]);
+                vert.insert(r2[3]);
+                //cout << "[" << r2[1] << "," << r2[3]<<"]";
+            }
+
+            int y1 = *vert.begin(), y2 = *prev(vert.end());
+            cout << "abs("<< x2 << "-" << x1 << ") * abs(" << y2 << "-" << y1 << ") " ;
+            int res = abs(x2-x1) * abs(y2-y1);
+            area += res;
         }
 
-        int x2 = x;
-
-        //int area = abs (x2 - x1) * abs (y2 - y1);
-
-       // cout << area << " ";
-        //cout << "abs("<< x2 << "-" << x1 << ") * abs(" << y2 <<  "-" << y1 << ")";
-        //segment = {x, miny, maxy};
-        cout << endl;
-    }
-
-    return 0;
-
-}
-
-void display (vector<vector<int>> grid) {
-
-    for (int y = 0; y < grid.size(); y++) {
-        for (int x = 0; x < grid[0].size(); x++) {
-            cout << grid[y][x] << grid[y][x];
+        for (int j = 0; j < rec.size(); j++) {
+            rectangle r1 = graph[rec[j]];
+            if (x == r1[0]) event.insert(rec[j]); 
+            if (x == r1[2]) event.erase(rec[j]);
         }
         cout << endl;
     }
 
-    cout << endl;
+
+    return area;
 }
+
+
 
 int main () {
 
 
-    vector<vector<int>> graph = {{3,3,8,5},{6,3,8,9},{11,6,14,12}};
-    
-    vector<vector<int>> grid (14, vector<int> (14));
-    
-    sweepline (graph);
+    //Assert::equals(calculate({ { 1, 4, 5, 6 },  { 2, 5, 6, 7 },{ 3, 6, 7, 8 },{ 4, 7, 8, 9 },{ 2, 3, 6, 5 },{ 3, 2, 7, 4 },{ 4, 1, 8, 3 }}), 38, "wings");
 
-    
-    //display (grid);
+    vector<vector<int>> graph = {{10,2,13,5}, {10,7,13,11},{11,9,14,13}};
+
+    //graph = {{3,3,8,5}, {6,3,8,9}, {11,6,14,12}};
+
+    set<int> line;
+    vector<pair<int, vector<int>>> sweep = mksweep(graph);
+
+    for (int i = 0; i < sweep.size(); i++) {
+        auto [x, rec] = sweep[i];
+        cout << x << " => ";
+
+        if (!line.empty()) {
+            int x2 = x, x1 = sweep[i - 1].first;
+            vector<pair<int,int>> bnd;
+
+            for (auto it : line) {
+                rectangle r2 = graph[it];
+                bool found = false;
+
+                for (auto &[aa,bb] : bnd) {
+                    if (r2[1] >= aa && r2[1] <= bb || r2[3] >= aa && r2[3] <= bb) {
+                        aa = min (r2[1], aa);
+                        bb = max (r2[3], bb);
+                        found = true;
+                    }
+                }
+                if (found == false) bnd.push_back({r2[1], r2[3]});
+            }
+            for (auto [y1,y2] : bnd) {
+
+                cout << "[" << y1 << "," << y2 <<"]";
+                /*
+                   int y1 = *vert.begin(), y2 = *prev(vert.end());
+                //cout << "abs("<< x2 << "-" << x1 << ") * abs(" << y2 << "-" << y1 << ") " ;
+                int res = abs(x2-x1) * abs(y2-y1);
+                */
+
+            }
+        }
+
+        for (int j = 0; j < rec.size(); j++) {
+            int index = rec[j];
+            rectangle r1 = graph[index];
+            if (x == r1[0]) line.insert(rec[j]); 
+            if (x == r1[2]) line.erase(rec[j]);
+        }
+        cout << endl;
+    }
 
 
-
-    //horizontal lines active events
     /*
-    */
+     *
+     *                      +-------+  13
+     *                      |       | 
+     *                   +--+----+  |  11
+     *                   |  |    |  |
+     *                   |  +----+--+  9
+     *                   |       |
+     *                   +-------+     7
+     *
+     *                   +-------+     5
+     *                   |       |
+     *                   |       |
+     *                   +-------+     2
+     *                   10 11   13 14
+     *
+     * 10 -> 0
+     * 11 -> [2-5][7-11]
+     * 13 -> [2-5][7-11][9-13]
+     * 14 -> [9-13]
+     *
+     */
 
-    // pseudocode
-    // input : list of rectangles : l
-    // output: overlap of rectangles :a
-    //
-    // clist contains all the x coordinates from all rectangles, each x coordinate has a reference too its rectangle
-    // int n = size of clist
-    //
-    // for i <- 0 to n do {
-    //     if clist[i] == clist[i].rectangle.left then
-    //          m <- size (clist)
-    //          for j <- 0 to m do [
-    //                  a <- a + compare (l[i], clist[j])
-    //          ]
-    //          clist.add(list[i])
-    //     else
-    //          clist.remove(l[i].rectangle)
-    //
-    // }
-
-
+    //sweep(graph);
+    /*
+       Assert::equals(calculate({{1,2,3,7},{2,1,7,3},{6,2,8,7},{2,6,7,8},{4,4,5,5}}), 37, "circle");
+       Assert::equals(calculate({{3,3,6,5},{4,4,6,6},{4,3,7,5},{4,2,8,5},{4,3,8,6},{9,0,11,4},{9,1,10,6},{9,0,12,2},{10,1,13,5},{12,4,15,6},{14,1,16,5},{12,1,17,2}}), 52, "very hard!");
+       */
+    //Test();
 }
 
 void Test () {
@@ -186,14 +296,15 @@ void Test () {
     Assert::equals(calculate({{ 1, 1, 6, 6 },{ 1, 3, 4, 6 },{ 2, 3, 4, 6 },{ 2, 4, 5, 6 },{ 3, 5, 4, 6 }}), 25, "intersection");
     Assert::equals(calculate({{1,1,6,6},{2,1,6,6},{3,1,6,6},{4,1,6,6},{5,2,6,5}}), 25, "shift right");
     Assert::equals(calculate({{1,1,7,6},{2,2,8,7},{3,3,9,8},{4,4,10,9},{5,5,11,10}}), 70, "shift right down");
-    Assert::equals(calculate({ { 1, 4, 5, 6 },  { 2, 5, 6, 7 },{ 3, 6, 7, 8 },{ 4, 7, 8, 9 },{ 2, 3, 6, 5 },{ 3, 2, 7, 4 },{ 4, 1, 8, 3 }}), 38, "wings");
     Assert::equals(calculate({ { 9, 5, 12, 6 },  { 10, 4, 11, 7 }}), 5, "intersection cross");
     Assert::equals(calculate({ { 7, 1, 11, 7 },  { 8, 0, 12, 3 },  { 8, 4, 13, 5 },  { 9, 5, 14, 8 },{ 10, 2, 15, 6 }}), 53, "intersection 2");
     Assert::equals(calculate({{1,2,6,6},{1,3,5,5},{1,1,7,7}}), 36, "pyramid");
-    Assert::equals(calculate({{1,2,3,7},{2,1,7,3},{6,2,8,7},{2,6,7,8},{4,4,5,5}}), 37, "circle");
     Assert::equals(calculate({{1,1,2,2},{1,1,2,2},{1,1,2,2},{1,1,2,2},{1,1,2,2},{1,1,2,2}}), 1, "one");
+    Assert::equals(calculate({ { 1, 4, 5, 6 },  { 2, 5, 6, 7 },{ 3, 6, 7, 8 },{ 4, 7, 8, 9 },{ 2, 3, 6, 5 },{ 3, 2, 7, 4 },{ 4, 1, 8, 3 }}), 38, "wings");
+    Assert::equals(calculate({{1,2,3,7},{2,1,7,3},{6,2,8,7},{2,6,7,8},{4,4,5,5}}), 37, "circle");
     Assert::equals(calculate({{3,3,6,5},{4,4,6,6},{4,3,7,5},{4,2,8,5},{4,3,8,6},{9,0,11,4},{9,1,10,6},{9,0,12,2},{10,1,13,5},{12,4,15,6},{14,1,16,5},{12,1,17,2}}), 52, "very hard!");
     Assert::equals(calculate({{2, 2, 17, 2}, {2, 2, 17, 4}, {2, 2, 17, 6}, {2, 2, 17, 8}, {2, 2, 17, 10}, {2, 2, 17, 12}, {2, 2, 17, 14}, {2, 2, 17, 16}, {2, 2, 17, 18}, {2, 2, 17, 20}, {2, 2, 17, 22}, {2, 2, 17, 24}, {2, 2, 17, 26}, {2, 2, 17, 28}}), 390, "waterfall");
+    /*
 
-
+*/
 }
