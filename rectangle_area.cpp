b2505c65672f93a6e81f502575b6ef07907b9287
Nemrod-7 <ocopey@gmail.com>
2022-12-09 18:25:00 +0100

    update

--- rectangle_area.cpp
+++ rectangle_area.cpp
@@ -33,6 +33,123 @@ class check {
 
 using point = pair<int,int>;
 using rectangle = vector<int>;
+vector<vector<int>> merge(vector<vector<int>> interv) {
+
+    vector<vector<int>> merge;
+    sort(interv.begin(), interv.end());
+
+    for (auto curr: interv) {
+        if (merge.empty() || merge.back()[1] < curr[0]) {
+            merge.push_back(curr);
+        } else {
+            merge.back()[1] = max (merge.back()[1], curr[1]);
+        }
+    }
+    return merge;
+}
+int calculate (vector<vector<int>> graph) {
+
+    int area = 0;
+    set<int> line;
+    map<int,vector<int>> hist;
+
+    sort (graph.begin(), graph.end());
+    for(int i = 0; i < graph.size(); i++) {
+        rectangle r = graph[i];
+        int x1 = r[0], x2 = r[2];
+        hist[x1].push_back(i);
+        hist[x2].push_back(i);
+    }
+
+    vector<pair<int, vector<int>>> sweep = {hist.begin(), hist.end()};
+
+    for (int i = 0; i < sweep.size(); i++) {
+        auto [pos, rec] = sweep[i];
+        //cout << pos << " => ";
+        if (!line.empty()) {
+
+            int x2 = pos, x1 = sweep[i - 1].first;
+            vector<vector<int>> interv;
+
+            for (auto index : line) {
+                interv.push_back({graph[index][1], graph[index][3]});
+            }
+            interv = merge (interv);
+
+            for (auto curr : interv) {
+                int y1 = curr[0], y2 = curr[1];
+                area += abs(x2 - x1) * abs(y2 - y1);
+            }
+        }
+
+        for (auto index : rec) {
+            rectangle r1 = graph[index];
+            if (r1[0] == pos) line.insert(index); 
+            if (r1[2] == pos) line.erase(index);
+        }
+        //cout << endl;
+    }
+
+    return area;
+}
+
+int main () {
+
+
+    //Assert::equals(calculate({ { 1, 4, 5, 6 },  { 2, 5, 6, 7 },{ 3, 6, 7, 8 },{ 4, 7, 8, 9 },{ 2, 3, 6, 5 },{ 3, 2, 7, 4 },{ 4, 1, 8, 3 }}), 38, "wings");
+    Assert::equals(calculate({ { 7, 1, 11, 7 },  { 8, 0, 12, 3 },  { 8, 4, 13, 5 },  { 9, 5, 14, 8 },{ 10, 2, 15, 6 }}), 53, "intersection 2");
+
+    //sweep(graph);
+    /*
+       Assert::equals(calculate({{1,2,3,7},{2,1,7,3},{6,2,8,7},{2,6,7,8},{4,4,5,5}}), 37, "circle");
+       Assert::equals(calculate({{3,3,6,5},{4,4,6,6},{4,3,7,5},{4,2,8,5},{4,3,8,6},{9,0,11,4},{9,1,10,6},{9,0,12,2},{10,1,13,5},{12,4,15,6},{14,1,16,5},{12,1,17,2}}), 52, "very hard!");
+       */
+    Test();
+}
+
+void Test () {
+    vector<vector<int>> graph {{1, 4, 2, 7}, {1, 4, 2, 6}, {1, 4, 4, 5}, {2, 5, 6, 7}, {4, 3, 7, 6}}; // multiple intersections =>  1 point belongs to 3 rectanles at least.
+                                                                                                      //
+    Assert::equals(calculate({ { 1, 7, 3, 10 },  { 1, 8, 3, 9 }}), 6, "1 under 2");
+    Assert::equals(calculate({ { 6, 7, 9, 10 },  { 7, 8, 8, 9 }}), 9, "nested");
+    Assert::equals(calculate({ { 1, 1, 4, 3 },  { 2, 2, 3, 4 }}), 7, "intersection up");
+    Assert::equals(calculate({ { 5, 0, 7, 3 },  { 6, 1, 8, 2 }}), 7, "intersetion right");
+    Assert::equals(calculate({ { 9, 0, 11, 2 },  { 10, 1, 12, 3 }}), 7, "intersection up right");
+    Assert::equals(calculate({ { 13, 5, 15, 6 },  { 14, 4, 16, 7 }}), 7, "intersection of the entire right side");
+    Assert::equals(calculate({ { 17, 1, 19, 3 },  { 18, 0, 20, 2 }}), 7, "intersection down right");
+    Assert::equals(calculate({ { 13, 1, 16, 3 },  { 14, 0, 15, 2 }}), 7, "intersection down");
+
+    Assert::equals(calculate({ { 1, 7, 4, 10 },  { 2, 7, 4, 9 },  { 3, 7, 4, 9 }}), 9, "nested 2");
+    Assert::equals(calculate({ { 1, 3, 4, 6 },  { 2, 1, 5, 4 },  { 3, 2, 6, 5 }}), 20, "intersection 3 rect");
+
+    Assert::equals(calculate({{1,1,2,2},{2,1,3,2},{3,1,4,2},{1,2,2,3},{2,2,3,3},{3,2,4,3},{1,3,2,4},{2,3,3,4},{3,3,4,4}}), 9, "3*3");
+    Assert::equals(calculate({{ 1, 1, 6, 6 },{ 1, 3, 4, 6 },{ 2, 3, 4, 6 },{ 2, 4, 5, 6 },{ 3, 5, 4, 6 }}), 25, "intersection");
+    Assert::equals(calculate({{1,1,6,6},{2,1,6,6},{3,1,6,6},{4,1,6,6},{5,2,6,5}}), 25, "shift right");
+    Assert::equals(calculate({{1,1,7,6},{2,2,8,7},{3,3,9,8},{4,4,10,9},{5,5,11,10}}), 70, "shift right down");
+    Assert::equals(calculate({ { 9, 5, 12, 6 },  { 10, 4, 11, 7 }}), 5, "intersection cross");
+    Assert::equals(calculate({ { 7, 1, 11, 7 },  { 8, 0, 12, 3 },  { 8, 4, 13, 5 },  { 9, 5, 14, 8 },{ 10, 2, 15, 6 }}), 53, "intersection 2");
+    Assert::equals(calculate({{1,2,6,6},{1,3,5,5},{1,1,7,7}}), 36, "pyramid");
+    Assert::equals(calculate({{1,1,2,2},{1,1,2,2},{1,1,2,2},{1,1,2,2},{1,1,2,2},{1,1,2,2}}), 1, "one");
+    Assert::equals(calculate({ { 1, 4, 5, 6 },  { 2, 5, 6, 7 },{ 3, 6, 7, 8 },{ 4, 7, 8, 9 },{ 2, 3, 6, 5 },{ 3, 2, 7, 4 },{ 4, 1, 8, 3 }}), 38, "wings");
+    Assert::equals(calculate({{1,2,3,7},{2,1,7,3},{6,2,8,7},{2,6,7,8},{4,4,5,5}}), 37, "circle");
+    Assert::equals(calculate({{3,3,6,5},{4,4,6,6},{4,3,7,5},{4,2,8,5},{4,3,8,6},{9,0,11,4},{9,1,10,6},{9,0,12,2},{10,1,13,5},{12,4,15,6},{14,1,16,5},{12,1,17,2}}), 52, "very hard!");
+    Assert::equals(calculate({{2, 2, 17, 2}, {2, 2, 17, 4}, {2, 2, 17, 6}, {2, 2, 17, 8}, {2, 2, 17, 10}, {2, 2, 17, 12}, {2, 2, 17, 14}, {2, 2, 17, 16}, {2, 2, 17, 18}, {2, 2, 17, 20}, {2, 2, 17, 22}, {2, 2, 17, 24}, {2, 2, 17, 26}, {2, 2, 17, 28}}), 390, "waterfall");
+    /*
+
+*/
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int area (rectangle rec) { return abs (rec[2] - rec[0]) * abs (rec[3] - rec[1]); }
+bool is_in_rect (int px, int py, rectangle r) { return (px >= r[0] && px <= r[3] && py >= r[1] && py <= r[4]); }
+bool overlap (rectangle r1, rectangle r2) {
+    return r1[0] <= r2[2] && r2[0] <= r1[2] && r1[1] <= r2[3] && r2[1] <= r1[3]; 
+}
+int overarea(rectangle r1, rectangle r2) {
+    int width = min(r1[2], r2[2])- max(r1[0], r2[0]);
+    int height = min(r1[3], r2[3]) - max(r1[1], r2[1]);
+    if (width >= 0 && height >= 0) return width * height;
+    return 0;
+}
 
 void display (vector<vector<int>> graph) {
 
@@ -76,79 +193,6 @@ void display (vector<vector<int>> graph) {
 
     cout << endl;
 }
-
-int area (rectangle rec) { return abs (rec[2] - rec[0]) * abs (rec[3] - rec[1]); }
-bool is_in_rect (int px, int py, rectangle r) { return (px >= r[0] && px <= r[3] && py >= r[1] && py <= r[4]); }
-bool overlap (rectangle r1, rectangle r2) {
-    return r1[0] <= r2[2] && r2[0] <= r1[2] && r1[1] <= r2[3] && r2[1] <= r1[3]; 
-}
-int overarea(rectangle r1, rectangle r2) {
-    int width = min(r1[2], r2[2])- max(r1[0], r2[0]);
-    int height = min(r1[3], r2[3]) - max(r1[1], r2[1]);
-    if (width >= 0 && height >= 0) return width * height;
-    return 0;
-}
-int calculate (vector<vector<int>> graph) {
-
-    //display (graph);
-    int area = 0;
-    set<int> line;
-    map<int,vector<int>> hist;
-
-    for(int i = 0; i < graph.size(); i++) {
-        rectangle r = graph[i];
-        int x1 = r[0], x2 = r[2];
-        hist[x1].push_back(i);
-        hist[x2].push_back(i);
-    }
-
-    vector<pair<int, vector<int>>> sweep = {hist.begin(), hist.end()};
-
-    for (int i = 0; i < sweep.size(); i++) {
-        auto [pos, rec] = sweep[i];
-        cout << pos << " => ";
-
-        if (!line.empty()) {
-            int x2 = pos, x1 = sweep[i - 1].first;
-            vector<pair<int,int>> bnd;
-
-            for (auto index : line) {
-                rectangle r2 = graph[index];
-                bool found = false;
-
-                cout << "[" << r2[1] << "," << r2[3] <<"]";
-                for (auto &[y1,y2] : bnd) {
-                    if (r2[1] >= y1 && r2[1] <= y2 || r2[3] >= y1 && r2[3] <= y2) {
-                        y1 = min (r2[1], y1);
-                        y2 = max (r2[3], y2);
-                        found = true;
-                    }
-                }
-                if (found == false) bnd.push_back({r2[1], r2[3]});
-            }
-
-            for (auto [y1,y2] : bnd) {
-                area += abs(x2-x1) * abs(y2-y1);
-                /*
-                cout << "[" << y1 << "," << y2 <<"]";
-                cout << "abs("<< x2 << "-" << x1 << ") * abs(" << y2 << "-" << y1 << ") " ;
-                */
-
-            }
-        }
-
-        for (int j = 0; j < rec.size(); j++) {
-            int index = rec[j];
-            rectangle r1 = graph[index];
-            if (r1[0] == pos) line.insert(rec[j]); 
-            if (r1[2] == pos) line.erase(rec[j]);
-        }
-        cout << endl;
-    }
-
-    return area;
-}
-
 vector<pair<int,vector<int>>> mksweep (const vector<vector<int>> &graph) {
 
     map<int,vector<int>> hist;
@@ -161,79 +205,4 @@ vector<pair<int,vector<int>>> mksweep (const vector<vector<int>> &graph) {
     }
 
     return {hist.begin(), hist.end()};
-}
-
-int main () {
-
-
-    //Assert::equals(calculate({ { 1, 4, 5, 6 },  { 2, 5, 6, 7 },{ 3, 6, 7, 8 },{ 4, 7, 8, 9 },{ 2, 3, 6, 5 },{ 3, 2, 7, 4 },{ 4, 1, 8, 3 }}), 38, "wings");
-
-    vector<vector<int>> graph = {{10,2,13,5}, {10,7,13,11},{11,9,14,13}};
-
-    graph = {{3,3,8,5}, {6,3,8,9}, {11,6,14,12}};
-
-
-    calculate (graph);
-
-    /*
-     *
-     *                      +-------+  13
-     *                      |       | 
-     *                   +--+----+  |  11
-     *                   |  |    |  |
-     *                   |  +----+--+  9
-     *                   |       |
-     *                   +-------+     7
-     *
-     *                   +-------+     5
-     *                   |       |
-     *                   |       |
-     *                   +-------+     2
-     *                   10 11   13 14
-     *
-     * 10 -> 0
-     * 11 -> [2-5][7-11]
-     * 13 -> [2-5][7-11][9-13]
-     * 14 -> [9-13]
-     *
-     */
-
-    //sweep(graph);
-    /*
-       Assert::equals(calculate({{1,2,3,7},{2,1,7,3},{6,2,8,7},{2,6,7,8},{4,4,5,5}}), 37, "circle");
-       Assert::equals(calculate({{3,3,6,5},{4,4,6,6},{4,3,7,5},{4,2,8,5},{4,3,8,6},{9,0,11,4},{9,1,10,6},{9,0,12,2},{10,1,13,5},{12,4,15,6},{14,1,16,5},{12,1,17,2}}), 52, "very hard!");
-       */
-    //Test();
-}
-
-void Test () {
-    vector<vector<int>> graph {{1, 4, 2, 7}, {1, 4, 2, 6}, {1, 4, 4, 5}, {2, 5, 6, 7}, {4, 3, 7, 6}}; // multiple intersections =>  1 point belongs to 3 rectanles at least.
-                                                                                                      //
-    Assert::equals(calculate({ { 1, 7, 3, 10 },  { 1, 8, 3, 9 }}), 6, "1 under 2");
-    Assert::equals(calculate({ { 6, 7, 9, 10 },  { 7, 8, 8, 9 }}), 9, "nested");
-    Assert::equals(calculate({ { 1, 1, 4, 3 },  { 2, 2, 3, 4 }}), 7, "intersection up");
-    Assert::equals(calculate({ { 5, 0, 7, 3 },  { 6, 1, 8, 2 }}), 7, "intersetion right");
-    Assert::equals(calculate({ { 9, 0, 11, 2 },  { 10, 1, 12, 3 }}), 7, "intersection up right");
-    Assert::equals(calculate({ { 13, 5, 15, 6 },  { 14, 4, 16, 7 }}), 7, "intersection of the entire right side");
-    Assert::equals(calculate({ { 17, 1, 19, 3 },  { 18, 0, 20, 2 }}), 7, "intersection down right");
-    Assert::equals(calculate({ { 13, 1, 16, 3 },  { 14, 0, 15, 2 }}), 7, "intersection down");
-
-    Assert::equals(calculate({ { 1, 7, 4, 10 },  { 2, 7, 4, 9 },  { 3, 7, 4, 9 }}), 9, "nested 2");
-    Assert::equals(calculate({ { 1, 3, 4, 6 },  { 2, 1, 5, 4 },  { 3, 2, 6, 5 }}), 20, "intersection 3 rect");
-
-    Assert::equals(calculate({{1,1,2,2},{2,1,3,2},{3,1,4,2},{1,2,2,3},{2,2,3,3},{3,2,4,3},{1,3,2,4},{2,3,3,4},{3,3,4,4}}), 9, "3*3");
-    Assert::equals(calculate({{ 1, 1, 6, 6 },{ 1, 3, 4, 6 },{ 2, 3, 4, 6 },{ 2, 4, 5, 6 },{ 3, 5, 4, 6 }}), 25, "intersection");
-    Assert::equals(calculate({{1,1,6,6},{2,1,6,6},{3,1,6,6},{4,1,6,6},{5,2,6,5}}), 25, "shift right");
-    Assert::equals(calculate({{1,1,7,6},{2,2,8,7},{3,3,9,8},{4,4,10,9},{5,5,11,10}}), 70, "shift right down");
-    Assert::equals(calculate({ { 9, 5, 12, 6 },  { 10, 4, 11, 7 }}), 5, "intersection cross");
-    Assert::equals(calculate({ { 7, 1, 11, 7 },  { 8, 0, 12, 3 },  { 8, 4, 13, 5 },  { 9, 5, 14, 8 },{ 10, 2, 15, 6 }}), 53, "intersection 2");
-    Assert::equals(calculate({{1,2,6,6},{1,3,5,5},{1,1,7,7}}), 36, "pyramid");
-    Assert::equals(calculate({{1,1,2,2},{1,1,2,2},{1,1,2,2},{1,1,2,2},{1,1,2,2},{1,1,2,2}}), 1, "one");
-    Assert::equals(calculate({ { 1, 4, 5, 6 },  { 2, 5, 6, 7 },{ 3, 6, 7, 8 },{ 4, 7, 8, 9 },{ 2, 3, 6, 5 },{ 3, 2, 7, 4 },{ 4, 1, 8, 3 }}), 38, "wings");
-    Assert::equals(calculate({{1,2,3,7},{2,1,7,3},{6,2,8,7},{2,6,7,8},{4,4,5,5}}), 37, "circle");
-    Assert::equals(calculate({{3,3,6,5},{4,4,6,6},{4,3,7,5},{4,2,8,5},{4,3,8,6},{9,0,11,4},{9,1,10,6},{9,0,12,2},{10,1,13,5},{12,4,15,6},{14,1,16,5},{12,1,17,2}}), 52, "very hard!");
-    Assert::equals(calculate({{2, 2, 17, 2}, {2, 2, 17, 4}, {2, 2, 17, 6}, {2, 2, 17, 8}, {2, 2, 17, 10}, {2, 2, 17, 12}, {2, 2, 17, 14}, {2, 2, 17, 16}, {2, 2, 17, 18}, {2, 2, 17, 20}, {2, 2, 17, 22}, {2, 2, 17, 24}, {2, 2, 17, 26}, {2, 2, 17, 28}}), 390, "waterfall");
-    /*
-
-*/
 }
--- rust/rectangle.rs
+++ rust/rectangle.rs
@@ -1,59 +1,104 @@
+use std::collections::{BTreeMap, HashSet};
 
-fn area (rec: [i32;4]) -> i64 {
-    ((rec[0] - rec[2]).abs() * (rec[1] - rec[3]).abs()) as i64
-}
-fn overlap(r1:[i32;4], r2:[i32;4]) -> bool {
-    return r1[0] <= r2[2] && r2[0] <= r1[2] && r1[1] <= r2[3] && r2[1] <= r1[3]; 
-}
-
-fn overarea(r1:[i32;4], r2:[i32;4]) -> i64 {
-    let width = std::cmp::min(r1[2], r2[2]) as i64 - std::cmp::max(r1[0], r2[0]) as i64 ;
-    let height = std::cmp::min(r1[3], r2[3]) as i64 - std::cmp::max(r1[1], r2[1]) as i64;
-
-    if width >= 0 && height >= 0 { return width * height } else { return 0  }
-}
 fn calculate(graph: &[[i32; 4]]) -> i64 {
     
-    let mut sum = 0;
-    sum = graph.iter().map(|x| area(*x)).sum::<i64>();
-    
-    for i in 0..graph.len() {
-        for j in  i + 1..graph.len() {
+    let mut area = 0;
+    let mut line:HashSet<usize> = HashSet::new();
+    let mut sweep:BTreeMap<i32,Vec<usize>> = BTreeMap::new();
+    let mut x0 = 0;
 
-            sum -= overarea(graph[i], graph[j]);
+    for i in 0..graph.len() {
+        let x1 = graph[i][0];
+        let x2 = graph[i][2];
+
+        match sweep.get_mut(&x1) {
+            Some(index) => { index.push(i); },
+            None => { sweep.insert(x1, vec![i]); }
+        }
+        match sweep.get_mut(&x2) {
+            Some(index) => { index.push(i); },
+            None => { sweep.insert(x2, vec![i]); }
         }
     }
 
-    print!("{sum} ");
-    sum
+    for (pos, rec) in sweep {
+        print!("{pos} => ");
+
+        
+        if !line.is_empty() {
+            let x1 = pos;
+            let mut inter:Vec<(i32,i32)> = Vec::new();
+            let mut merge:Vec<(i32,i32)> = Vec::new();
+
+            for index in &line {
+                let y1 = graph[*index][1];
+                let y2 = graph[*index][3];
+                inter.push((y1,y2));
+            }
+
+            inter.sort();
+
+            for y in &inter {
+                if merge.is_empty() {
+                    merge.push (*y);
+                } else {
+                    let last = merge.len() - 1;
+
+                    if merge[last].1 < y.0 {
+                        merge.push(*y);
+                    } else {
+                        merge[last].1 = std::cmp::max(merge[last].1, y.1);
+                    }
+                }
+            }
+
+            for y in merge {
+                print!("[{},{}]",y.0, y.1);
+                area += ((x1 - x0).abs() * (y.1 - y.0).abs()) as i64;
+            }
+        }
+
+        for index in rec {
+            if graph[index][0] == pos { line.insert(index); }
+            if graph[index][2] == pos { line.remove(&index); }
+        }
+
+        print!("\n");
+        x0 = pos;
+    }
+
+    area
 }
 
 fn main() {
-
+    let area = calculate(&[[3,3,8,5], [6,3,8,9], [11,6,14,12]]);
+    print!(" => {area}");
 }
 
 fn test () {
-    assertEquals(calculate([ [ 1, 7, 3, 10 ],  [ 1, 8, 3, 9 ]]), 6, '1 under 2');
-    assertEquals(calculate([ [ 6, 7, 9, 10 ],  [ 7, 8, 8, 9 ]]), 9, "nested");
-    assertEquals(calculate([ [ 1, 7, 4, 10 ],  [ 2, 7, 4, 9 ],  [ 3, 7, 4, 9 ]]), 9, "nested 2");
-    assertEquals(calculate([ [ 1, 1, 4, 3 ],  [ 2, 2, 3, 4 ]]), 7, "intersection up");
-    assertEquals(calculate([ [ 5, 0, 7, 3 ],  [ 6, 1, 8, 2 ]]), 7, "intersetion right");
-    assertEquals(calculate([ [ 9, 0, 11, 2 ],  [ 10, 1, 12, 3 ]]), 7, "intersection up right");
-    assertEquals(calculate([ [ 13, 5, 15, 6 ],  [ 14, 4, 16, 7 ]]), 7, "intersection of the entire right side");
-    assertEquals(calculate([ [ 17, 1, 19, 3 ],  [ 18, 0, 20, 2 ]]), 7, "intersection down right");
-    assertEquals(calculate([ [ 13, 1, 16, 3 ],  [ 14, 0, 15, 2 ]]), 7, "intersection down");
-    assertEquals(calculate([ [ 1, 3, 4, 6 ],  [ 2, 1, 5, 4 ],  [ 3, 2, 6, 5 ]]), 20, "intersection 3 rect");
-    assertEquals(calculate([[1,1,2,2],[2,1,3,2],[3,1,4,2],[1,2,2,3],[2,2,3,3],[3,2,4,3],[1,3,2,4],[2,3,3,4],[3,3,4,4]]), 9, "3*3");
-    assertEquals(calculate([[ 1, 1, 6, 6 ],[ 1, 3, 4, 6 ],[ 2, 3, 4, 6 ],[ 2, 4, 5, 6 ],[ 3, 5, 4, 6 ]]), 25, "intersection");
-    assertEquals(calculate([[1,1,6,6],[2,1,6,6],[3,1,6,6],[4,1,6,6],[5,2,6,5]]), 25, "shift right");
-    assertEquals(calculate([[1,1,7,6],[2,2,8,7],[3,3,9,8],[4,4,10,9],[5,5,11,10]]), 70, "shift right down");
-    assertEquals(calculate([ [ 1, 4, 5, 6 ],  [ 2, 5, 6, 7 ],[ 3, 6, 7, 8 ],[ 4, 7, 8, 9 ],[ 2, 3, 6, 5 ],[ 3, 2, 7, 4 ],[ 4, 1, 8, 3 ]]), 38, "wings");
-    assertEquals(calculate([ [ 9, 5, 12, 6 ],  [ 10, 4, 11, 7 ]]), 5, "intersection cross");
-    assertEquals(calculate([ [ 7, 1, 11, 7 ],  [ 8, 0, 12, 3 ],  [ 8, 4, 13, 5 ],  [ 9, 5, 14, 8 ],[ 10, 2, 15, 6 ]]), 53, "intersection 2");
-    assertEquals(calculate([[1,2,6,6],[1,3,5,5],[1,1,7,7]]), 36, "pyramid");
-    assertEquals(calculate([[1,2,3,7],[2,1,7,3],[6,2,8,7],[2,6,7,8],[4,4,5,5]]), 37, "circle");
-    assertEquals(calculate([[1,1,2,2],[1,1,2,2],[1,1,2,2],[1,1,2,2],[1,1,2,2],[1,1,2,2]]), 1, "one");
-    assertEquals(calculate([[3,3,6,5],[4,4,6,6],[4,3,7,5],[4,2,8,5],[4,3,8,6],[9,0,11,4],[9,1,10,6],[9,0,12,2],[10,1,13,5],[12,4,15,6],[14,1,16,5],[12,1,17,2]]), 52, 'very hard!');
-    assertEquals(calculate([[2, 2, 17, 2], [2, 2, 17, 4], [2, 2, 17, 6], [2, 2, 17, 8], [2, 2, 17, 10], [2, 2, 17, 12], [2, 2, 17, 14], [2, 2, 17, 16], [2, 2, 17, 18], [2, 2, 17, 20], [2, 2, 17, 22], [2, 2, 17, 24], [2, 2, 17, 26], [2, 2, 17, 28]]), 390, "waterfall");
-		});
+    assert_eq!(calculate(&[[ 1, 7, 3, 10 ],  [ 1, 8, 3, 9 ]]), 6, "1 under 2");
+    assert_eq!(calculate(&[[ 6, 7, 9, 10 ],  [ 7, 8, 8, 9 ]]), 9, "nested");
+    assert_eq!(calculate(&[[ 1, 7, 4, 10 ],  [ 2, 7, 4, 9 ],  [ 3, 7, 4, 9 ]]), 9, "nested 2");
+    assert_eq!(calculate(&[[ 1, 1, 4, 3 ],  [ 2, 2, 3, 4 ]]), 7, "intersection up");
+    assert_eq!(calculate(&[[ 5, 0, 7, 3 ],  [ 6, 1, 8, 2 ]]), 7, "intersetion right");
+    assert_eq!(calculate(&[[ 9, 0, 11, 2 ],  [ 10, 1, 12, 3 ]]), 7, "intersection up right");
+    assert_eq!(calculate(&[[ 13, 5, 15, 6 ],  [ 14, 4, 16, 7 ]]), 7, "intersection of the entire right side");
+    assert_eq!(calculate(&[[ 17, 1, 19, 3 ],  [ 18, 0, 20, 2 ]]), 7, "intersection down right");
+    assert_eq!(calculate(&[[ 13, 1, 16, 3 ],  [ 14, 0, 15, 2 ]]), 7, "intersection down");
+    assert_eq!(calculate(&[[ 1, 3, 4, 6 ],  [ 2, 1, 5, 4 ],  [ 3, 2, 6, 5 ]]), 20, "intersection 3 rect");
+    assert_eq!(calculate(&[[1,1,2,2],[2,1,3,2],[3,1,4,2],[1,2,2,3],[2,2,3,3],[3,2,4,3],[1,3,2,4],[2,3,3,4],[3,3,4,4]]), 9, "3*3");
+    assert_eq!(calculate(&[[ 1, 1, 6, 6 ],[ 1, 3, 4, 6 ],[ 2, 3, 4, 6 ],[ 2, 4, 5, 6 ],[ 3, 5, 4, 6 ]]), 25, "intersection");
+    assert_eq!(calculate(&[[1,1,6,6],[2,1,6,6],[3,1,6,6],[4,1,6,6],[5,2,6,5]]), 25, "shift right");
+    assert_eq!(calculate(&[[1,1,7,6],[2,2,8,7],[3,3,9,8],[4,4,10,9],[5,5,11,10]]), 70, "shift right down");
+    assert_eq!(calculate(&[[ 1, 4, 5, 6 ],  [ 2, 5, 6, 7 ],[ 3, 6, 7, 8 ],[ 4, 7, 8, 9 ],[ 2, 3, 6, 5 ],[ 3, 2, 7, 4 ],[ 4, 1, 8, 3 ]]), 38, "wings");
+    assert_eq!(calculate(&[[ 9, 5, 12, 6 ],  [ 10, 4, 11, 7 ]]), 5, "intersection cross");
+    assert_eq!(calculate(&[[ 7, 1, 11, 7 ],  [ 8, 0, 12, 3 ],  [ 8, 4, 13, 5 ],  [ 9, 5, 14, 8 ],[ 10, 2, 15, 6 ]]), 53, "intersection 2");
+    assert_eq!(calculate(&[[1,2,6,6],[1,3,5,5],[1,1,7,7]]), 36, "pyramid");
+    assert_eq!(calculate(&[[1,2,3,7],[2,1,7,3],[6,2,8,7],[2,6,7,8],[4,4,5,5]]), 37, "circle");
+    assert_eq!(calculate(&[[1,1,2,2],[1,1,2,2],[1,1,2,2],[1,1,2,2],[1,1,2,2],[1,1,2,2]]), 1, "one");
+    assert_eq!(calculate(&[[3,3,6,5],[4,4,6,6],[4,3,7,5],[4,2,8,5],[4,3,8,6],[9,0,11,4],[9,1,10,6],[9,0,12,2],[10,1,13,5],[12,4,15,6],[14,1,16,5],[12,1,17,2]]), 52, "very hard!");
+    assert_eq!(calculate(&[[2, 2, 17, 2], [2, 2, 17, 4], [2, 2, 17, 6], [2, 2, 17, 8], [2, 2, 17, 10], [2, 2, 17, 12], [2, 2, 17, 14], [2, 2, 17, 16], [2, 2, 17, 18], [2, 2, 17, 20], [2, 2, 17, 22], [2, 2, 17, 24], [2, 2, 17, 26], [2, 2, 17, 28]]), 390, "waterfall");
+	
+    /*
+    */
 }
