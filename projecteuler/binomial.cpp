569b043661f83d92727d06927e8fe43a6d19e096
Nemrod-7 <ocopey@gmail.com>
2023-02-12 19:41:27 +0100

    update

--- projecteuler/binomial.cpp
+++ projecteuler/binomial.cpp
@@ -14,40 +14,7 @@ class check {
       }
 };
 
-inline int mod3 (int n) {
-
-    if (n < 0) return ((n %= 3) < 0) ? n + 3 : n;
-    if (n * 0xAAAAAAAB <= n) return 0;
-    return n % 3;
-}
-int lucas3 (int n, int k) {
-
-    int prod = 1, np, kp;
-    // => lucas (n k) = prod from i = 0 to infinity (binom(n_i k_i) mod p)
-    do {
-        np = mod3 (n), kp = mod3 (k);
-        //cout << '(' << np << ' ' << kp << ") => " << binomial (np, kp) << endl;
-        if (np < kp) return 0;
-        if (np == 2 && kp == 1) prod *= 2; //prod *= binomial (np, kp);
-
-        n /= 3, k /= 3;
-
-    } while (n || k);
-
-    return mod3 (prod);
-}
-
-char triangle (const std::string &row) { // insane colored triangle
-    const std::vector<char> colors = {'B','R','G'};
-    int N = row.size() - 1, sum = 0, k = row.size();
-
-   while (k-->0)
-        sum += lucas3 (N, k) * row[k] % 3;
-
-    return colors[mod3 (pow (-1, N) * sum)];
-}
-
-inline uint64_t factorial (int n) {
+uint64_t factorial (int n) {
   uint64_t mult = 1;
   for (int i = 1; i <= n ; ++i)
       mult *= i;
@@ -55,7 +22,7 @@ inline uint64_t factorial (int n) {
   return mult;
 }
 
-inline uint64_t binom_fact (int n, int k) { // binom_fact (9, 5); -> 10 015 656 in 1s
+uint64_t binom_fact (int n, int k) { // binom_fact (9, 5); -> 10 015 656 in 1s : limit n = ~20, k ~20
     return factorial (n) / (factorial (k) * factorial (n - k));
 }
 double binom_beta (int n, int k) {     // binom_beta (9, 5); -> 6 042 814 in 1s
@@ -81,48 +48,28 @@ uint64_t binomial (int n, int k) {        // binomial (9,5); -> 9 795 281 in 1s
     return static_cast<uint64_t> (res);
 }
 
-vector<vector<int>> mk_triangle (int size) {
-    vector<vector<int>> tri;
-    int num;
-    for (int n = 0; n <= size; ++n) {
-        tri.push_back(vector<int>());
-        for (int k = 0; k <= n; ++k) {
-            //num = round (binomial (n, k));
-            num = lucas3 (n, k);
+inline int mod3 (int n) {
 
-            tri[n].push_back (num);
-        }
-    }
-
-    return tri;
+    if (n < 0) return ((n %= 3) < 0) ? n + 3 : n;
+    if (n * 0xAAAAAAAB <= n) return 0;
+    return n % 3;
 }
-void display_tri (vector<vector<int>> &tria) {
+int lucas3 (int n, int k) {
 
-    for (auto &n : tria) {
-        cout << string ((tria.size() - n.size()) * 2, ' ');
-        for (auto &k : n) {
-            if (k)
-                cout << setw(3) << k << ' ';
-            else
-                cout << "    ";
-        }
-        cout << endl;
-    }
+    int prod = 1, np, kp;
+    // => lucas (n k) = prod from i = 0 to infinity (binom(n_i k_i) mod p)
+    do {
+        np = mod3 (n), kp = mod3 (k);
+        //cout << '(' << np << ' ' << kp << ") => " << binomial (np, kp) << endl;
+        if (np < kp) return 0;
+        if (np == 2 && kp == 1) prod *= 2; //prod *= binomial (np, kp);
+
+        n /= 3, k /= 3;
+
+    } while (n || k);
+
+    return mod3 (prod);
 }
-
-int main () {
-
-  Timer clock;
-
-
-
-  clock.stop();
-  clock.get_duration();
-
-  return EXIT_SUCCESS;
-}
-
-/////////////////////////////////Arkive////////////////////////////////////////
 int lucasth (int n, int k, int p) {
 
     int prod = 1, np, kp;
@@ -139,4 +86,64 @@ int lucasth (int n, int k, int p) {
     } while (n || k);
 
     return prod % p;
+}
+
+vector<vector<uint64_t>> mk_triangle (int size) {
+    vector<vector<uint64_t>> tri;
+    uint64_t num;
+    for (int n = 0; n <= size; ++n) {
+        tri.push_back(vector<uint64_t>());
+        for (int k = 0; k <= n; ++k) {
+            //num = round (binomial (n, k));
+            num = binomial (n, k);
+
+            tri[n].push_back (num);
+        }
+    }
+
+    return tri;
+}
+void display_tri (vector<vector<uint64_t>> &tri) {
+
+    for (auto &n : tri) {
+        cout << string ((tri.size() - n.size()) * 2, ' ');
+        for (auto &k : n) {
+            if (k)
+                cout << setw(3) << k << ' ';
+            else
+                cout << "    ";
+        }
+        cout << endl;
+    }
+}
+
+int main () {
+
+  Timer clock;
+  const uint64_t limit = 23, epsilon = 1e6;
+  vector<vector<uint64_t>> tri (limit + 1);
+  uint64_t num = 0;
+  int cnt = 0;
+
+  for (int n = 0; n <= limit; ++n) {
+      for (int k = 0; k <= n; ++k) {
+          if (k == 0 || k == n) {
+              num = 1;
+          } else {
+              num = tri[n-1][k-1] + tri[n-1][k];
+          }
+          tri[n].push_back (num);
+          if (num >= epsilon)
+              cnt++;
+          
+          cout << tri[n][k] << " ";
+      }
+      cout << endl;
+  }
+  //display_tri (tri);
+
+  clock.stop();
+  clock.get_duration();
+
+  return EXIT_SUCCESS;
 }
--- projecteuler/test.cpp
+++ projecteuler/test.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
+
 #include <chrono>
 
 // NEQ5xBztxeg43aP
 using namespace std;
+
+int gcd (int a, int b) { return b == 0 ? a : gcd (b, a % b); }
 
 vector<int> sieve3 (int num) {
 
@@ -49,42 +53,62 @@ int phi (int num) { // totient funtion
 
     return (num > 1) ? res - res / num : res;
 }
+int phi2 (int num, vector<int> &prime) { // totient funtion
+
+    int res = num;
+    int *p = prime.data();
+
+    for (int i = 0; p[i] * p[i] <= num; i++) {
+        if (num % p[i] == 0) {
+            while (num % p[i] == 0)
+                num /= p[i];
+
+            res -= res / p[i];
+        }
+    }
+
+    return (num > 1) ? res - res / num : res;
+}
+vector<int> phi3 (int lim) {
+    vector<int> sieve (lim + 1);
+
+    for (int i = 0; i <= lim; i++)
+        sieve[i] = i;
+
+    for (int i = 2; i <= lim; i++) {
+        if (sieve[i] == i) {
+            for (int j = i; j <= lim; j += i)
+                sieve[j] -= sieve[j] / i;
+        }
+    }
+    return sieve;
+}
+
+
+void farey (int n) {
+    // We know first two terms are 0/1 and 1/n
+    double x1 = 0, y1 = 1, x2 = 1, y2 = n;
+    printf("%.0f/%.0f %.0f/%.0f", x1, y1, x2, y2);
+    double x, y = 0; // For next terms to be evaluated
+
+    while (y != 1.0) {
+        // Using recurrence relation to find the next term
+        x = floor((y1 + n) / y2) * x2 - x1;
+        y = floor((y1 + n) / y2) * y2 - y1;
+        // Print next term
+        printf(" %.0f/%.0f", x, y);
+        // Update x1, y1, x2 and y2 for next iteration
+        x1 = x2, x2 = x, y1 = y2, y2 = y;
+    }
+}
 
 int main () {
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    const int lim = 1e6;
-
-    int index = 0;
-    double maxv = 0.0;
-    vector<int> p = sieve3 (lim);
+    const int lim = 1e3;
 
 
-    vector<int> phi3 (lim + 1);
-
-    for (int i = 0; i <= lim; i++)
-        phi3[i] = i;
-
-    for (int i = 2; i <= lim; i++) {
-        if (phi3[i] == i) {
-            for (int j = i; j <= lim; j += i)
-                phi3[j] -= phi3[j] / i;
-        }
-    }
-
-    for (int k = 2; k < lim; k++) {
-        double val = k / static_cast<double> (phi3[k]);
-
-        if (val > maxv) {
-            maxv = val;
-            index = k;
-        }
-
-    }
-
-
-    cout << index;
 
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
