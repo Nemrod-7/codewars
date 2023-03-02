ed358e783a665807a003abf27f91383c7266bb89
Nemrod-7 <ocopey@gmail.com>
2023-03-02 12:32:00 +0100

    update

--- projecteuler/graph.cpp
+++ projecteuler/graph.cpp
@@ -121,6 +121,57 @@ int main () {
 
     Timer chrono;
 
+    vector<vector<int>> adj {{0,16,12,21,0,0,0}, {16,0,0,17,20,0,0}, {12,0,0,28,0,31,0}, {21,17,28,0,18,19,23}, {0,20,0,18,0,0,11}, {0,0,31,19,0,0,27}, {0,0,0,23,11,27,0}};
+
+    using vertex = pair<int,int>; // weight, id
+
+    const int size = adj.size();
+    vector<vertex> edge;
+    vector<vector<int>> graph (size);
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (adj[i][j] != 0) {
+                int id = j, weight = adj[i][j];
+                //graph[i].push_back(j);
+                edge.push_back({weight,j});
+            }
+        }
+    }
+    sort (edge.begin(), edge.end());
+
+    int i = 0;
+    auto [weight, id] = edge[i];
+    cout << id << " => ";
+
+    int minv = 999, nxv;
+
+    for (int j = 0; j < size; j++) {
+        int node = adj[id][j];
+
+        if (node > 0 && node != weight) {
+            if (node < minv) {
+                minv = node;
+                nxv = j;
+            }
+        }
+    }
+
+    graph[id].push_back(nxv);
+    cout << nxv;
+
+    /*
+    for (int i = 0; i < graph.size(); i++) {
+        cout << static_cast<char> (i + 65) << " => ";
+        //sort (edge[i].begin(), edge[i].end());
+        for (int j = 0; j < graph[i].size(); j++) {
+            //cout << edge[i][j].first << " ";
+            //cout << static_cast<char> (tree[i][j].second + 65) << " ";
+        }
+        cout << "\n";
+    }
+    */
+    /*
     auto file = getfile ("files/p107_network.txt");
     // find the maximum saving weight while verifying the graph remains connected
     const int size = file.size();
@@ -131,7 +182,7 @@ int main () {
     adj = reduce (adj);
 
     cout << total - getweight (adj);
-
+    */
     chrono.stop();
     chrono.get_duration();
 }
