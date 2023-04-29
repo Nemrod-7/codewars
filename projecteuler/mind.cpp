ee0d46fe717fbb49d51365f1259a655f9ff197e3
Nemrod-7 <ocopey@gmail.com>
2023-04-29 19:18:50 +0200

    update

--- projecteuler/mind.cpp
+++ projecteuler/mind.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <random>
-#include <chrono>
 #include <algorithm>
+#include <chrono>
 
 using namespace std;
 
@@ -13,48 +14,6 @@ void showvec (vector<int> pass) {
   cout << "\n";
 }
 
-vector<vector<pair<int,char>>> mkhist2 (vector<pair<string,int>> clue, int size) {
-    vector<vector<pair<int,char>>> base (size);
-    vector<vector<int>> hist (size, vector<int> (10,1));
-
-    for (auto [code,res] : clue) {
-        for (int i = 0; i < size; i++) {
-          int dig = code[i] - '0';
-          hist[i][dig]++;
-        }
-    }
-
-    for (auto [code,res] : clue) {
-        for (int i = 0; i < size; i++) {
-          int dig = code[i] - '0';
-          hist[i][dig] *= res;
-        }
-    }
-
-    for (int i = 0; i < size; i++) {
-        vector<pair<int,char>> prob;
-        // cout << i << " => ";
-        for (int j = 0; j < 10; j++) {
-            if (hist[i][j] != 0)
-                prob.push_back ({hist[i][j],j + '0'});
-
-            // cout << hist[i][j] << ' ';
-        }
-        // cout << "\n";
-        sort(prob.begin(),prob.end());
-
-        base[i] = prob;
-    }
-
-    return base;
-}
-int count (const string &a, const string &b) {
-  int cnt = 0;
-  for (int i = 0; i < a.size(); i++) {
-    if (a[i] == b[i]) cnt++;
-  }
-  return cnt;
-}
 
 vector<vector<char>> mkhist (vector<pair<string,int>> clue, int size) {
     vector<vector<char>> base (size);
@@ -90,6 +49,49 @@ vector<vector<char>> mkhist (vector<pair<string,int>> clue, int size) {
 
     return base;
 }
+vector<vector<pair<int,char>>> mkhist2 (vector<pair<string,int>> clue, int size) {
+    vector<vector<pair<int,char>>> base (size);
+    vector<vector<int>> hist (size, vector<int> (10,1));
+
+    for (auto [code,res] : clue) {
+        for (int i = 0; i < size; i++) {
+          int dig = code[i] - '0';
+          hist[i][dig]++;
+        }
+    }
+
+    for (auto [code,res] : clue) {
+        for (int i = 0; i < size; i++) {
+          int dig = code[i] - '0';
+          hist[i][dig] *= res;
+        }
+    }
+
+    for (int i = 0; i < size; i++) {
+        vector<pair<int,char>> prob;
+        // cout << i << " => ";
+        for (int j = 0; j < 10; j++) {
+            if (hist[i][j] != 0)
+                prob.push_back ({hist[i][j],j + '0'});
+
+            // cout << hist[i][j] << ' ';
+        }
+        // cout << "\n";
+        sort(prob.begin(),prob.end());
+
+        base[i] = prob;
+    }
+
+    return base;
+}
+
+int count (const string &a, const string &b) {
+  int cnt = 0;
+  for (int i = 0; i < a.size(); i++) {
+    if (a[i] == b[i]) cnt++;
+  }
+  return cnt;
+}
 
 int eval (const vector<pair<string,int>> &clue, const string &pass) {
     int cnt = 0;
@@ -103,57 +105,136 @@ int eval (const vector<pair<string,int>> &clue, const string &pass) {
 void simulated_annealing (vector<pair<string,int>> clue) {
   int size = clue[0].first.size();
 
+  auto base = mkhist (clue,size);
+  int nd = base[0].size() - 1;
+
   random_device rd;  // Will be used to obtain a seed for the random number engine
   mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
-  geometric_distribution<> d;
+  binomial_distribution<> d(nd, 0.9); // geometric_distribution<> d;  // nd - d(gen)
+  uniform_real_distribution<> fx (0, 1);
+  uniform_int_distribution<> pos (0, size - 1);
+  string curr (size,'0');
 
-  string pass;
-  auto base = mkhist2 (clue,size);
-
-  for (int i = 0; i < base.size(); i++) { /// first generation
-    auto [prob,dig] = base[i].back();
-    pass += dig;
+  for (int i = 0; i < base.size(); i++) { // first gene
+      curr[i] = base[i].back();
   }
 
-  int rate = eval (clue,pass);
+  const float alpha = 0.99, t_min = 0.5;
+  float T = 5000.0;
+  cout << fixed;
 
-  if (rate == clue.size()) {
-    cout << "password : " << pass << "\n";
-  } else {
-    int minv = numeric_limits<int>::max();
+  while (T > t_min) {
+      // mutation
+      int x = pos(gen), c0 = eval (clue,curr);
+      string next = curr;
 
-    for (int i = 0; i < base.size(); i++) {
-      auto [prob,dig] = base[i].back();
-      minv = min (minv,prob);
-    }
+      if (c0 == clue.size()) {
+          cout << "password : " << curr << "\n";
+          break;
+      }
 
-    vector<int> V;
-    for (int i = 0; i < base.size(); i++) {
-      auto [prob,dig] = base[i].back();
-      if (prob == minv) {
-        V.push_back(i);
+      do {
+          next[x] = base[x][d(gen)];
+      } while (next[x] == curr[x]);
+
+      int c1 = eval (clue,next);
+
+      if (c1 > c0) {
+          curr = next;
+      } else {
+          float dx = 1.0 - exp((c1 - c0) / T);
+          // cout << T << " " << dx << " " << fx(gen) << "\n";
+          if (dx < fx(gen)) {
+              curr = next;
+          }
+     }
+
+      T *= alpha;
+  }
+
+
+}
+
+vector<string> rnd_walk (vector<pair<string,int>> &clue, vector<vector<pair<int,char>>> &base, string pass) {
+    int size = pass.size();
+    int maxv = eval (clue,pass);
+    string tmp = pass;
+    vector<string> vec;
+
+    for (int x = 0; x < size; x++) {
+      for (int j = 0; j < base[x].size(); j++) {
+        tmp[x] = base[x][j].second;
+        maxv = max (maxv,eval (clue,tmp));
       }
     }
 
-    uniform_int_distribution<int> dist (0, V.size() - 1);
-    int x = V[dist(gen)];
-    int bak = pass[x], nxt;
-    int nd = base[x].size() - 1;
+    tmp = pass;
+    for (int x = 0; x < size; x++) {
+      for (int j = 0; j < base[x].size(); j++) {
+        tmp[x] = base[x][j].second;
+        if (eval (clue,tmp) == maxv) {
+          vec.push_back(tmp);
+        }
+      }
+    }
+    return vec;
+}
 
-    do {
-      nxt = base[x][nd - d(gen)].second;
-    } while (nxt == bak);
+void simulated_annealing2 (vector<pair<string,int>> clue) {
+  int size = clue[0].first.size();
 
-    pass[x] = nxt;
+  auto base = mkhist (clue,size);
+  int nd = base[0].size() - 1;
 
-    cout << rate << " " << eval (clue,pass);
-    // cout << pass << "\n";
+  random_device rd;  // Will be used to obtain a seed for the random number engine
+  mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
+  binomial_distribution<> d(nd, 0.9); // geometric_distribution<> d;  // nd - d(gen)
+  uniform_real_distribution<> rng (0, 1);
+  uniform_int_distribution<> pos (0, size - 1);
+  string curr (size,'0');
+
+  for (int i = 0; i < base.size(); i++) { // first gene
+      curr[i] = base[i].back();
   }
+
+  const float alpha = 0.999, t_min = 0.5;
+  float T = 5.0;
+  cout << fixed;
+
+  while (T > t_min) {
+      // mutation
+      int x = pos(gen), c0 = eval (clue,curr);
+      string next = curr;
+
+      if (c0 == clue.size()) {
+          cout << "password : " << curr << "\n";
+          break;
+      }
+
+      do {
+          next[x] = base[x][d(gen)];
+      } while (next[x] == curr[x]);
+
+      int c1 = eval (clue,next);
+
+      if (c1 > c0) {
+          curr = next;
+      } else {
+          float dx = exp((c1 - c0) / T);
+
+          if (dx > rng(gen)) {
+              curr = next;
+          }
+     }
+
+      T *= alpha;
+  }
+
 
 }
 int main () {
 
-      chrono::steady_clock::time_point alpha = chrono::steady_clock::now (), end;
+      chrono::steady_clock::time_point start = chrono::steady_clock::now (), end;
 
       // Problem 185
       vector<pair<string,int>> clue;
@@ -163,64 +244,37 @@ int main () {
       {"5251583379644322",2}, {"1748270476758276",3}, {"4895722652190306",1}, {"3041631117224635",3}, {"1841236454324589",3}, {"2659862637316867",2}};
 
       clue = { {"70794",0}, {"90342",2}, {"51545",2}, {"39458",2}, {"34109",1}, {"12531",1}};
+      cout << fixed;
+      const float kmax = 600.0;
 
-      int size = clue[0].first.size();
-      string pass ;
-      // auto base = mkhist (clue,size);
-      auto base = mkhist2 (clue,size);
 
-      for (int i = 0; i < base.size(); i++) {
-          auto [prob,dig] = base[i].back();
-          pass += dig;
+      float T = 5.0, alpha = 0.99, t_min = 1.0;
+
+      while (T > t_min) {
+          float delta = -1.0 ;
+          float dx = exp(delta / T);
+          printf ("%4.2f %4.0f %f\n", T, delta, dx);
+
+          T *= alpha;
       }
 
-      for (int x = 0; x < size; x++) {
-
-        int curr = eval (clue,pass);
-        int maxv = curr;
-        string tmp = pass, next = pass;
-
-        for (int j = base[x].size() - 1; j >= 0; j--) {
-          char dig = base[x][j].second;
-          tmp[x] = dig;
-          int rate = eval (clue,tmp);
-
-          if (rate > maxv) {
-            maxv = rate;
-            next = tmp;
-          }
-          cout << curr << " " << rate << " " << tmp <<  "\n";
-        }
-        pass = next;
-      }
-
-      // pass[x]
-
-      // simulated_annealing (clue);
-
-
-/*
-      vector<int> index;
-      vector<pair<int,int>> hist;
-
-      for (int i = 0; i < size; i++) {
-          auto [prob,dig] = base[i].back();
-          hist.push_back({prob,i});
-      }
-
-      sort(hist.begin(),hist.end());
-
-      for (int i = 0; i < size; i++) {
-          index.push_back(hist[i].second);
-      }
-
-      // auto hd = mkhist (clue,size);
-
-      // backtrack (clue, hd, string (size,'0'), 0);
+      // simulated_annealing2 (clue);
 
       // codebreaker (clue, base, index, pass, 0);
-
       /*
+      vector<float> temp {500,400,300,100,50,20};
+      vector<float> dex {-1,-5,-10,-100,-200};
+
+      for (auto T : temp) {
+        for (auto delta : dex) {
+          // float de = delta / T;
+          float dx = exp(delta / T);
+
+          // cout << T << " " << delta << " " << dx << "\n";
+        }
+      }
+
+
       V = {0,1,2,3,4,5,6,7,8,9};
       int hist[10] = {0};
 
@@ -237,7 +291,7 @@ int main () {
 
 
     end = chrono::steady_clock::now ();
-    std::chrono::duration<double> elapsed = end - alpha;
+    std::chrono::duration<double> elapsed = end - start;
     cout << "\nDuration " << fixed << elapsed.count() << " ms" << endl;
 
 }
@@ -261,7 +315,6 @@ void Test () {
 
    */
 };
-
 
 int count (const string &a, const string &b, int x) {
     int cnt = 0;
--- temp.cpp
+++ temp.cpp
@@ -1,46 +1,71 @@
+#include <random>
+#include <map>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
 #include <iostream>
+#include <vector>
 
-bool cantor (double x, double y, double num, unsigned cycle, unsigned lim) {
+template<int Height = 5, int BarWidth = 1, int Padding = 1, int Offset = 0, class Seq>
 
-    if (num < x || num > y) return false;
-    if (cycle >= lim) return true;
-    const double md = y / 3.0;
-    const bool left = cantor (x, md, num, cycle + 1, lim);
-    const bool right = cantor (y - md, y, num, cycle + 1, lim);
-    // printf ("%f ", md);
-    printf ("%f %f %f\n", x, num, y);
+void draw_vbars(Seq&& s, const bool DrawMinMax = true) {
+    static_assert(0 < Height and 0 < BarWidth and 0 <= Padding and 0 <= Offset);
 
-    if (left || right) return true;
-    return false;
-}
-bool is_in_cantor (unsigned num, unsigned den, unsigned n) {
+    auto cout_n = [](auto&& v, int n = 1) {
+        while (n--> 0)
+            std::cout << v;
+    };
 
+    const auto [min, max] = std::minmax_element(std::cbegin(s), std::cend(s));
 
-  if (num > den) return false;
-  const double frac = num / (double) den;
+    std::vector<std::div_t> qr;
+    for (typedef decltype(*std::cbegin(s)) V; V e : s)
+        qr.push_back(std::div(std::lerp(V(0), 8 * Height, (e - *min) / (*max - *min)), 8));
 
-  return cantor (0.0, 1.0, frac, 0, n);
-}
+    for (auto h {Height}; h-- > 0; cout_n('\n')) {
+        cout_n(' ', Offset);
 
-int main () {
-  int64_t mod = 50515093;
+        for (auto dv : qr) {
+            const auto q {dv.quot}, r {dv.rem};
+            unsigned char d[] {0xe2, 0x96, 0x88, 0}; // Full Block: '█'
+            q < h ? d[0] = ' ', d[1] = 0 : q == h ? d[2] -= (7 - r) : 0;
+            cout_n(d, BarWidth), cout_n(' ', Padding);
+        }
 
-  int64_t s0 = 290797;
-  int64_t s1 = (s0 * s0) % mod;
-
-  for (int i = 0; i < 14; i++) {
-      s0 = (s0 * s0) % mod;
-      printf ("%zu ", s0);
-  }
-  /*
-  num = 1
-  den = 613999
-  n   = 40
-    bool res = is_in_cantor (1,37,6); // false
-
-    switch (res) {
-        case 0 : printf ("false"); break;
-        case 1 : printf ("true"); break;
+        if (DrawMinMax && Height > 1)
+            Height - 1 == h ? std::cout << "┬ " << *max:
+                          h ? std::cout << "│ "
+                            : std::cout << "┴ " << *min;
     }
-    */
+}
+
+int main() {
+    std::random_device rd{};
+    std::mt19937 gen{rd()};
+
+    std::extreme_value_distribution<> d{-1.618f, 1.618f};
+
+    const int norm = 10'000;
+    const float cutoff = 0.000'3f;
+
+    std::map<int, int> hist{};
+    for (int n = 0; n != norm; ++n)
+        ++hist[std::round(d(gen))];
+
+    std::vector<float> bars;
+    std::vector<int> indices;
+    for (const auto& [n, p] : hist)
+    {
+        if (const float x = p * (1.0f / norm); x > cutoff)
+        {
+            bars.push_back(x);
+            indices.push_back(n);
+        }
+    }
+
+    draw_vbars<8,4>(bars);
+
+    for (int n : indices)
+        std::cout << " " << std::setw(2) << n << "  ";
+    std::cout << '\n';
 }
