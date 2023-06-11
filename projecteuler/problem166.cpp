8f389917ffcc6241b4c5a8f387211def29fbaacb
Nemrod-7 <ocopey@gmail.com>
2023-06-11 15:22:57 +0200

    update

--- projecteuler/problem166.cpp
+++ projecteuler/problem166.cpp
@@ -15,15 +15,16 @@ void showgrid (vector<vector<int>> &grid) {
   }
   cout << "\n";
 }
-bool is_valid (vector<vector<int>> &grid, int ref) {
-    int size = grid.size();
+bool is_valid (const vector<vector<int>> &grid, int ref) {
+    const int size = grid.size();
     int diag = 0, aiag = 0;
 
     for (int i = 0; i < size; i++) {
         diag += grid[i][i];
-        aiag += grid[size-i-1][size-i-1];
+        aiag += grid[i][size-i-1];
     }
-    if (diag != ref || diag != ref) return false;
+
+    if (diag != ref || aiag != ref) return false;
 
     for (int i = 0; i < size; i++) {
         int row = 0, col = 0;
@@ -35,32 +36,38 @@ bool is_valid (vector<vector<int>> &grid, int ref) {
 
     return true;
 }
+bool partial2 (vector<vector<int>> &grid, int y, int sum) {
 
-bool backtrack2 (vector<vector<int>> grid, vector<int> &base, int y) {
+    int diag = 0;
+    for (int i = 0; i <= y; i++) {
+        diag += grid[i][i];
+        if (diag > sum) return false;
+    }
+
+    return true;
+}
+bool backtrack2 (vector<vector<int>> &grid, const vector<int> &base, int y, int sum) {
 
   if (y == grid.size()) {
-      int sum = 0;
-      for (int i = 0; i < grid.size(); i++) {
-          sum += grid[i][0];
-      }
-
       if (is_valid(grid, sum) == true) {
-
-          // showgrid(grid);
+          showgrid(grid);
           return true;
       } else {
           return false;
       }
   }
 
-  for (int num : base) {
-      int nn = num, x = 0;
+  for (int comb : base) {
+      int num = comb, x = 0;
 
       do {
-         grid[x++][y] = nn % 10;
-      } while (nn /= 10);
+         grid[x++][y] = num % 10;
+      } while (num /= 10);
 
-      backtrack2 (grid, base, y + 1);
+      if (partial2 (grid, y, sum) == true)
+
+          backtrack2 (grid, base, y + 1, sum);
+
   }
 
   return false;
@@ -72,11 +79,13 @@ int main () {
     chrono::duration<double> elapsed;
     // Problem 166 Criss Cross
 
-    const int size = 3;
+    const int size = 4;
+    int ss = 6;
     vector<vector<int>> grid (size, vector<int> (size));
     map<int,vector<int>> base;
 
-    for (int i = 0; i < 1000; i++) {
+
+    for (int i = 0; i < 10000; i++) {
         int num = i, sum = 0;
 
         do {
@@ -86,61 +95,17 @@ int main () {
         base[sum].push_back(i);
     }
 
-    // backtrack1 (grid, 0, 0, 12);
-    backtrack2 (grid,base[12],0);
+    for (auto [num, comb] : base) {
+        // cout << num << " => " << comb.size() << "\n";
+    }
+
+    backtrack2 (grid,base[ss],0, ss);
+
+    /*
+    */
 
 
     end = chrono::steady_clock::now (), elapsed = end - alpha;
     std::cout << "\nDuration " <<fixed<< elapsed.count() << " ms" << std::endl;
 
-}
-
-bool partial (vector<vector<int>> grid, int x, int y, int sum) {
-
-    int row = 0, col = 0;
-    for (int i = 0; i <= x; i++) {
-        row += grid[i][y];
-        if (row > sum) return false;
-    }
-
-    for (int i = 0; i <= y; i++) {
-        col += grid[x][i];
-        if (col > sum) return false;
-    }
-
-    return true;
-}
-bool backtrack1 (vector<vector<int>> grid, int x, int y, int sum) {
-    int size =  grid.size();
-
-    if (x == size) {
-        int line = 0;
-        for (int i = 0; i < size; i++) {
-            line += grid[i][y];
-        }
-        if (line != sum) return false;
-
-        x = 0;
-        y++;
-    }
-
-    if (y == size) {
-        if (is_valid (grid,sum) == true) {
-            //cout << x << " " << y << "\n";
-            showgrid(grid);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    for (int dig = 0; dig < 10; dig++) {
-        grid[x][y] = dig;
-
-        if (partial (grid,x,y,sum) == true) {
-            backtrack1 (grid, x + 1 , y, sum);
-        }
-    }
-
-    return true;
 }
