d6394c548b26122d22e289c5daa394c400e05bb7
Nemrod-7 <ocopey@gmail.com>
2023-06-14 19:14:02 +0200

    update

--- projecteuler/problem166.cpp
+++ projecteuler/problem166.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <map>
 #include <chrono>
@@ -6,12 +7,13 @@
 using namespace std;
 
 const int N = 4;
+const int pow[N] = {1000,100,10,1};
 
 void showgrid (const int grid[]) {
 
   for (int i = 0; i < N; i++) {
       for (int j = 0; j < N; j++) {
-          cout << grid[i * N + j];
+          cout << grid[i * N + j] << " ";
       }
       cout << "\n";
   }
@@ -58,30 +60,25 @@ bool partial  (const int grid[], int y, int sum) {
   return true;
 }
 
-bool backtrack (int grid[], const vector<int> &base, int y, int sum, int &cnt) {
+bool backtrack1 (int grid[], const vector<int> &base, int y, int sum, int &cnt) {
 
   if (y == N) {
       if (grid[0] + grid[5] + grid[10] + grid[15] != sum) return false;
       if (grid[3] + grid[6] + grid[9] + grid[12] != sum) return false;
       // if (is_valid (grid,sum)) {
       // } else { return false; }
-
       // showgrid (grid);
       cnt++;
       return true;
   }
 
   for (int num : base) {
-      int  x = 0;
-
-      do {
-          // int dig = num % 10;
-          grid[y * N + x] = num % 10;
-          x++;
-      } while (num /= 10);
+      for (int i = 0; i < N; i++) {
+        grid[y * N + i] = (num / pow[i]) % 10;
+      }
 
       if (partial (grid, y, sum) == true) {
-          backtrack (grid, base, y + 1, sum, cnt);
+          backtrack1 (grid, base, y + 1, sum, cnt);
       }
   }
 
@@ -101,7 +98,11 @@ int limit (int grid[], int x, int y, int sum) {
 }
 
 bool checkline (int grid[], int y, int sum) {
-    int row = 0;
+    int row = 0, diag = 0;
+    for (int i = 0; i < y; i++) {
+        diag += grid[i * N + i];
+        if (diag > sum) return false;
+    }
 
     for (int x = 0; x < N; x++) {
         row += grid[y * N + x];
@@ -125,12 +126,56 @@ bool backtrack2 (int grid[], int x, int y, int sum, int &cnt) {
         }
         return false;
     }
-    int nd = limit (grid,x,y,sum);
+    int col = sum, row = sum;
+
+    for (int i = 0; i < y; i++)
+        col -= grid[i * N + x];
+
+    for (int i = 0; i < x; i++)
+        row -= grid[y * N + i];
+
+    int nd = min(min(col, row), 9);
     // cout << nd << ' ';
     for (int dig = nd; dig >= 0 ; dig--) {
         grid[y * N + x] = dig;
         backtrack2 (grid, x+1, y, sum, cnt);
     }
+
+    return false;
+}
+bool backtrack3 (int grid[], int row[], int col[], int x, int y, int sum, int &cnt) {
+
+
+
+    if (x == N) {
+        if (checkline (grid,y,sum) == false) {
+            return false;
+        }
+        x = 0;
+        y++;
+    }
+    if (y == N) {
+        if (is_valid(grid, sum)) {
+            cnt++;
+            return true;
+        }
+        return false;
+    }
+
+    int maxx = sum - max (row[y],col[x]);
+    int lim = min (9, maxx);
+
+        for (int dig = lim; dig >= 0 ; dig--) {
+          grid[y * N + x] = dig;
+          col[x] += dig;
+          row[y] += dig;
+
+          backtrack3 (grid, row, col, x + 1, y, sum, cnt);
+
+          col[x] -= dig;
+          row[y] -= dig;
+        }
+    // cout << max (row[y],col[x]) << ' ';
 
     return false;
 }
@@ -141,86 +186,85 @@ int main () {
     chrono::duration<double> elapsed;
     // Problem 166 Criss Cross
 
+    int sum = 12, total = 0;
     int grid[N * N] = {0};
-    int sum = 32, total = 0;
+    int mask[N * N] = {0};
+    int col[N] = {0}, row[N] = {0};
+    // vector<int> col(N), row(N);
 
-    for (int i = 0; i < 37;i++){
+    // // fill_n(mask, N*N, sum);
+    // grid[0] = 6, grid[1] = 3, grid[2] = 3;
+    // grid[4] = 5, grid[6] = 4, grid[7] = 3;
+    // grid[9] = 7, grid[10] = 1, grid[11] = 4;
+    // grid[12]=1, grid[13] = 2, grid[14]=4, grid[15]=5;
+    vector<int> seq = {1,8,48,200,675,1904,4736,10608,21925,42328,76976,131320,209127,309968,427440,549184};
+
+    // showgrid(grid);
+    // for (int y = 0; y < N; y++) {
+    //     for (int x = 0; x < N; x++) {
+    //           int lim = min (9, sum - max (row[y],col[x]));
+    //           cout << lim << " ";
+    //           col[x] += grid[y*N+x];
+    //           row[y] += grid[y*N+x];
+    //     }
+    //     cout << "\n";
+    // }
+    // cout << "\n";
+
+    for (int i = 16; i < 17; i++){
       int cnt = 0;
-      cout << i << " => " ;
-      backtrack2 (grid,0,0,i,cnt);
+      cout << setw(2) << i << " => " ;
+      // backtrack1(grid, base[i], 0, i, cnt);
+      backtrack2 (grid, 0, 0, i, cnt);
+      // backtrack3 (grid, row, col, 0, 0, i, cnt);
+
       total += cnt;
       cout << cnt;
       cout << "\n";
     }
 
     cout << total;
-    // showgrid(grid);
-    /*
 
-    6 3 3 0
-    5 0 4 3
-    0 7 1 4
-    1 2 4 5
-
-    0 =>   1 ::    0
-    1 =>   4 ::    0
-    2 =>  10 ::    0
-    3 =>  20 ::    0
-    4 =>  35 ::   33
-    5 =>  56 ::  228
-    6 =>  84 ::  902
-    7 => 120 :: 2736
-    8 => 165 :: 6955
-    9 => 220 :: 15680
-    10 => 282 :: 32212
-    11 => 348 :: 61367
-    12 => 415 :: 108276
-
-
-    ...................
-
-
-    27 => 220 :: 42248
-    28 => 165 :: 21925
-    29 => 120 :: 10608
-    30 =>  84 :: 4736
-    31 =>  56 :: 1904
-    32 =>  35 :: 675
-    33 =>  20 :: 200
-    34 =>  10 :: 48
-    35 =>   4 :: 8
-    36 =>   1 :: 1
-
-
-    vector<int> seq {0,0,0,0,33,228,902,2736,6955,15680,32212,61367,108276};
-
-    */
-    /*
-    map<int,vector<int>> base;
-    for (int i = 0; i < 10000; i++) {
-        int num = i, sum = 0;
-
-        do {
-            sum += num % 10;
-        } while (num /= 10);
-
-        base[sum].push_back(i);
-    }
-
-    for (auto [num, comb] : base) {
-        // cout << num << " => " << comb.size() << "\n";
-    }
-
-
-    for (int num : base[1]) {
-      cout << num << " ";
-    }
-    */
-
-    // auto comb = base[i];
-    // cout << comb.size() << " :: ";
-    // backtrack (grid, base[i], 0, i, cnt);
 
     end = chrono::steady_clock::now (), elapsed = end - alpha;
     std::cout << "\nDuration " <<fixed<< elapsed.count() << " ms" << std::endl;
+}
+
+void ark() {
+  // showgrid(grid);
+  /*
+
+  6 3 3 0
+  5 0 4 3
+  0 7 1 4
+  1 2 4 5
+
+  vector<int> seq {0,0,0,0,33,228,902,2736,6955,15680,32212,61367,108276};
+
+  */
+  /*
+  map<int,vector<int>> base;
+  for (int i = 0; i < 10000; i++) {
+      int num = i, sum = 0;
+
+      do {
+          sum += num % 10;
+      } while (num /= 10);
+
+      base[sum].push_back(i);
+  }
+
+  for (auto [num, comb] : base) {
+      // cout << num << " => " << comb.size() << "\n";
+  }
+
+
+  for (int num : base[1]) {
+    cout << num << " ";
+  }
+  */
+
+  // auto comb = base[i];
+  // cout << comb.size() << " :: ";
+  // backtrack (grid, base[i], 0, i, cnt);
 }
