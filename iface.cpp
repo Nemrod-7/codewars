ed3a1705c7fde3f21fabf026b2c7f5c17be04e9a
Nemrod-7 <ocopey@gmail.com>
2023-07-05 19:15:00 +0200

    update

--- iface.cpp
+++ iface.cpp
@@ -1,45 +1,137 @@
 #include <iostream>
 #include <iomanip>
-#include <string>
 #include <vector>
-#include <sys/ioctl.h>
+#include <string>
 
-void head (int width) {
-    std::cout << "┌";
-    for (int i = 0; i < width - 2; i++) {
-        std::cout << "─";
+#include <functional>
+
+#include <thread>
+#include <chrono>
+
+using namespace std;
+using func = std::function<std::string(int,int)>;
+
+
+std::string u8 (const wchar_t wc) {
+    char buffer[8] = {0};
+    bool shifts = std::wctomb(nullptr, 0); // reset the conversion state
+    const int ret = std::wctomb (buffer, wc);
+    if (ret < 0) throw std::invalid_argument("inconvertible wide characters in the current locale");
+    buffer[ret] = '\0';  // make 'buffer' contain a C-style string
+    return std::string (buffer);
+}
+
+void header (vector<int> row) {
+    for (int width : row) { // make head
+        cout <<  "┌";
+        for (int j = 0; j < width; j++) {
+            cout << "─";
+        }
+        cout << "┐";
     }
-    std::cout << "┐\n";
+    cout << "\n";
+}
+void bottom (vector<int> row) {
+  for (int width : row) { // make bottom
+      cout <<  "└";
+      for (int j = 0; j < width; j++) {
+          cout << "─";
+      }
+      cout << "┘";
+  }
+  cout << "\n";
+}
+
+string progress (const int &i, const int &j) {
+    return j < i ? "#" : " "; 
+}
+string waiting (const int &i, const int &j) {
+    wstring bar = L"▉▊▋▌▍▎▏▎▍▌▋▊▉";
+    return u8 (bar[(i + j) % bar.size()]);
+}
+
+class Bar {
+    public :
+        int width, height;
+        func fn;
+
+        Bar (int a, int b, func cls) {
+            width = a, height = b, fn = cls;
+        }
+};
+
+
+string mkup (const Bar &curr) {
+    string os;
+        os += "┌";
+        for (int j = 0; j < curr.width; j++) {
+            os +="─";
+        }
+        os +="┐";
+    
+
+    return os;
+}
+string mkbot (const Bar &curr) {
+    string os;
+      os +=  "└";
+      for (int j = 0; j < curr.width; j++) {
+          os += "─";
+      }
+      os += "┘";
+    return os;
+}
+
+string inner (const Bar &curr, int i) {
+    string os;
+
+    os += "│";
+    for (int j = 0; j < curr.width; j++) {
+        
+        os += curr.fn (i,j);
+    }
+
+    os += "│";
+    return os;
 }
 int main () {
 
     setlocale (LC_ALL, "en_US.UTF-8");
 
-    struct winsize w;
-    ioctl(0, TIOCGWINSZ, &w);
-    const int width = w.ws_col, height = w.ws_row;
+    wstring bar = L"▉▊▋▌▍▎▏▎▍▌▋▊▉";
 
-    // const wstring sw = L"─";
-    // const wstring ss = L"┌", es = L"┐\n";
-    // const wstring sl = L"│", nl = L"│\n";
-    // const wstring ns = L"└", ne = L"┘\n";
-
-    std::cout << "┌";
-    for (int i = 0; i < width - 2; i++) {
-        std::cout << "─";
-    }
-    std::cout << "┐\n";
-
-    for (int i = 0; i < height - 2; i++) {
-        std::cout << "│";
-
-        std::cout << std::setw (width+2);
+    Bar curr (5,1, waiting);
 
 
-        std::cout << "│\n";
-    }
+    cout << mkup (curr) << "\n";
 
+    cout << mkbot (curr) << "\n";
 
-
-    // std::cout << "exit";
+    //      vector<int> row {50,5};
+    //
+    //      for (int i = 0; i < 51; i++) {
+    //            cout << "\033c\033[36m";
+    //            header (row);
+    //
+    //            cout << "│";
+    //            for (int j = 0; j < row[0]; j++) {
+    //                if (j < i) {
+    //                    cout << "▉";
+    //                } else {
+    //                    cout << " ";
+    //                }
+    //            }
+    //            cout << "│";
+    //
+    //            cout << "│";
+    //            for (int j = 0; j < row[1]; j++) {
+    //                    cout << u8 (bar[(i + j) % bar.size()]);
+    //            }
+    //            cout << "│";
+    //            cout << "\n";
+    //
+    //            bottom (row);
+    //            std::this_thread::sleep_for(std::chrono::milliseconds(70));
+    //      }
+    //
 }
--- rain.cpp
+++ rain.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <string>
 
+#include <unistd.h>
 #include <sys/ioctl.h>
+#include <termios.h>
+
 
 #include <thread>
 #include <random>
@@ -9,15 +12,24 @@
 
 // using namespace std;
 
-bool running = true;
+struct termios t;
+struct winsize w;
 
-int getwidth () {
-  struct winsize w;
+void input_enter_off() {
+    tcgetattr(STDIN_FILENO, &t);
+    t.c_lflag &= ~ICANON;
+    tcsetattr(STDIN_FILENO, TCSANOW, &t);
+}
+void input_enter_on() {
+    tcgetattr(STDIN_FILENO, &t);
+    t.c_lflag |= ICANON;
+    tcsetattr(STDIN_FILENO, TCSANOW, &t);
+}
+void initdim () {
   ioctl(0, TIOCGWINSZ, &w);
-  return w.ws_col;
 }
 
-std::string convert (const wchar_t wc) {
+std::string u8 (const wchar_t wc) {
     char buffer[8] = {0};
     bool shifts = std::wctomb(nullptr, 0); // reset the conversion state
     const int ret = std::wctomb (buffer, wc);
@@ -26,102 +38,74 @@ std::string convert (const wchar_t wc) {
     return std::string (buffer);
 }
 
-void progress (const std::wstring &bar) {
+template<typename Func> static void load (Func fn);
 
-    int cycle = 0;
-    std::chrono::steady_clock::time_point now, roll = std::chrono::steady_clock::now();
-    std::chrono::duration<double> interval;
+namespace module {
+    bool process;
 
-    while (running) {
-        now = std::chrono::steady_clock::now();
-        interval = now - roll;
+    void rain() {
+      const int width = w.ws_col - 1;
+      const int nflip = 5;
+      const std::wstring ch = L"1234567890qwertyuiopasdfghjklzxcvbnm,./':<>;[]?!@#$%^&*()-=_+¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶ᐿᥠᘌᕳᕲᎬƂᥫȠͳᘝ૯ΩΨΧΦῃᨒ";
 
-        if (interval >= std::chrono::milliseconds(100)) {
-              std::cout << "\033c";
-              std::cout << "\033[1;1m";
-              std::cout << convert (bar[cycle]);
+      int x = 0;
+      bool line[width] = {0};
+      std::random_device rd;
+      std::mt19937 gen(rd());
+      std::uniform_int_distribution<> dist (0, ch.size() - 1), pos (0, width-1);
 
-              std::cout << " type input => " << std::flush;
-              // std::cout << "Waiting... " << "\n" << std::flush;
-              cycle = cycle == bar.size() - 1 ? 0 : cycle + 1;
-              roll = now;
-        }
-    }
-}
-
-void rain() {
-    const int width = getwidth() - 1;
-    const int nflip = 5;
-    const std::wstring ch = L"1234567890qwertyuiopasdfghjklzxcvbnm,./':<>;[]?!@#$%^&*()-=_+¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶ᐿᥠᘌᕳᕲᎬƂᥫȠͳᘝ૯ΩΨΧΦῃᨒ";
-
-    int x = 0;
-    bool line[width] = {0};
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dist (0, ch.size() - 1), pos (0, width-1);
-
-    while (running) {
+      while (process) {
         for (int i = 0; i < width; i += 2) {
-            if (line[i]) {
-              std::cout << convert (ch[dist(gen)]) << " ";
-            } else {
-              std::cout << "  ";
-            }
+          if (line[i]) {
+            std::cout << u8 (ch[dist(gen)]) << " ";
+          } else {
+            std::cout << "  ";
+          }
         }
 
         for (int i = 0; i != nflip; i++) {
-            x = pos(gen);
-            line[x] = !line[x];
+          x = pos(gen);
+          line[x] = !line[x];
         }
+
         std::cout << "\n";
         std::this_thread::sleep_for(std::chrono::milliseconds(70));
+      }
+    }
+};
+namespace prog {
+    void execute () {
+      load (module::rain);
     }
 }
 
-int main() {
+template<typename Func> static void load (Func fn) {
 
-    setlocale (LC_ALL, "en_US.UTF-8");
+    input_enter_off();
+    module::process = true;
+    std::thread t1 (fn);
 
-    const std::wstring bar = L"⠁⠂⠄⡀⢀⠠⠐⠈";
-    std::thread t1 (rain);
-    std::string input;
-
-    while (running) {
-        if (std::cin.get()) {
-            running = false;
+    while (module::process) {
+        if (getchar()) {
+            module::process = false;
         }
     }
 
     if (t1.joinable())
-      t1.join();
+        t1.join();
 
-        std::string rules =
-        "rules of derivation: \n\n"
-        "cst : cst   => 0\n"
-        "lin : x     => 1\n"
-        "add : a + b => a' + b'\n"
-        "min : a - b => a' - b'\n"
-        "mul : a . b => a.b' + a'.b\n"
-        "div : a / b => (a'. b − b'. a) / b²\n"
-        "pow : x^a   => a.x^(a - 1)\n"
-        "exp : e^x   => e^x\n"
-        "exp : a^x   => a^x . ln a\n"
-        "ln  : ln(x) => 1 / x\n"
-        "sin : sin x => cos x\n"
-        "cos : cos x => -sin x\n"
-        "tan : tan x => 1 / cos²x\n";
-    // wcout << ext;
+      input_enter_on();
+}
+
+
+int main() {
+
+    setlocale (LC_ALL, "en_US.UTF-8");
+    initdim();
+
+
 
     std::cout << "exit\n";
 
     return 0;
-}
-
-std::string wchartostring1 (wchar_t wch) { // deprecated
-  // #include <locale>
-  // #include <codecvt>
-  // std::wstring_convert<std::codecvt_utf8<wchar_t>> conv1;
-  // std::string u8 = conv1.to_bytes(ch[dist(gen)]);
-  // std::cout << u8 << " ";
-  return "";
 }
