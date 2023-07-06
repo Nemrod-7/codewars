24496960bd8b9afdea9763b44cae1405676ed3b6
Nemrod-7 <ocopey@gmail.com>
2023-07-06 12:30:39 +0200

    pdate

--- iface.cpp
+++ iface.cpp
@@ -8,10 +8,19 @@
 #include <thread>
 #include <chrono>
 
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+
 using namespace std;
 using func = std::function<std::string(int,int)>;
 
+struct termios t;
+struct winsize w;
 
+void initdim () {
+  ioctl(0, TIOCGWINSZ, &w);
+}
 std::string u8 (const wchar_t wc) {
     char buffer[8] = {0};
     bool shifts = std::wctomb(nullptr, 0); // reset the conversion state
@@ -43,7 +52,7 @@ void bottom (vector<int> row) {
 }
 
 string progress (const int &i, const int &j) {
-    return j < i ? "#" : " "; 
+    return j < i ? "#" : " ";
 }
 string waiting (const int &i, const int &j) {
     wstring bar = L"▉▊▋▌▍▎▏▎▍▌▋▊▉";
@@ -68,7 +77,7 @@ string mkup (const Bar &curr) {
             os +="─";
         }
         os +="┐";
-    
+
 
     return os;
 }
@@ -87,51 +96,42 @@ string inner (const Bar &curr, int i) {
 
     os += "│";
     for (int j = 0; j < curr.width; j++) {
-        
+
         os += curr.fn (i,j);
     }
 
     os += "│";
     return os;
 }
+
 int main () {
 
     setlocale (LC_ALL, "en_US.UTF-8");
+    initdim();
 
     wstring bar = L"▉▊▋▌▍▎▏▎▍▌▋▊▉";
+    int width = w.ws_col;
 
-    Bar curr (5,1, waiting);
+     vector<Bar> win ={Bar(5, 1, progress), Bar(20, 1, waiting)};
+     for (int i = 0; i < 51; i++) {
+           cout << "\033c\033[36m";
 
+           for (auto curr : win) {
+              cout << mkup (curr);
+           }
+           cout << "\n";
 
-    cout << mkup (curr) << "\n";
+           for (auto curr : win) {
+              cout << inner (curr, i);
+           }
+           cout << "\n";
 
-    cout << mkbot (curr) << "\n";
+           for (auto curr : win) {
+             cout << mkbot (curr);
+           }
+           cout << "\n";
+           std::this_thread::sleep_for(std::chrono::milliseconds(70));
+     }
 
-    //      vector<int> row {50,5};
-    //
-    //      for (int i = 0; i < 51; i++) {
-    //            cout << "\033c\033[36m";
-    //            header (row);
-    //
-    //            cout << "│";
-    //            for (int j = 0; j < row[0]; j++) {
-    //                if (j < i) {
-    //                    cout << "▉";
-    //                } else {
-    //                    cout << " ";
-    //                }
-    //            }
-    //            cout << "│";
-    //
-    //            cout << "│";
-    //            for (int j = 0; j < row[1]; j++) {
-    //                    cout << u8 (bar[(i + j) % bar.size()]);
-    //            }
-    //            cout << "│";
-    //            cout << "\n";
-    //
-    //            bottom (row);
-    //            std::this_thread::sleep_for(std::chrono::milliseconds(70));
-    //      }
-    //
+     cout << "exit";
 }
