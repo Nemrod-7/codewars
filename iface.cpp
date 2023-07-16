5045108bf312184a9617bf2c38ae600114fb3cb8
Nemrod-7 <ocopey@gmail.com>
2023-07-16 19:09:06 +0200

    update

--- iface.cpp
+++ iface.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 #include <iomanip>
-#include <vector>
 #include <string>
-
-#include <functional>
-
-#include <thread>
-#include <chrono>
 
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 
+#include <thread>
+#include <random>
+#include <chrono>
+
 using namespace std;
-using func = std::function<std::string(int,int)>;
 
 struct termios t;
 struct winsize w;
 
+void input_enter_off() {
+    tcgetattr(STDIN_FILENO, &t);
+    t.c_lflag &= ~ICANON;
+    tcsetattr(STDIN_FILENO, TCSANOW, &t);
+}
+void input_enter_on() {
+    tcgetattr(STDIN_FILENO, &t);
+    t.c_lflag |= ICANON;
+    tcsetattr(STDIN_FILENO, TCSANOW, &t);
+}
 void initdim () {
   ioctl(0, TIOCGWINSZ, &w);
 }
+
 std::string u8 (const wchar_t wc) {
     char buffer[8] = {0};
     bool shifts = std::wctomb(nullptr, 0); // reset the conversion state
@@ -30,106 +38,76 @@ std::string u8 (const wchar_t wc) {
     return std::string (buffer);
 }
 
-void bottom (vector<int> row) {
-  for (int width : row) { // make bottom
-      cout <<  "└";
-      for (int j = 0; j < width; j++) {
-          cout << "─";
-      }
-      cout << "┘";
-  }
-  cout << "\n";
-}
+const std::string divers =
+      "Constants :\n" "       e      pi\n\n" "Functions :\n" "     log      ln     rad     sqr\n" "     cos    acos     deg     abs\n"
+      "     sin    asin    sqrt        \n" "     tan    atan                \n\n" "Operators :\n" "+ - * % / ^ = =>\n\n"
+      "lambdas :\n" "fn 'name' 'parameters' => 'function' \n" "ex : fn avg x y => (x + y) / 2\n\n";
 
-string progress (const int &i, const int &j) {
-    return j < i ? "█" : " ";
-}
-string waiting (const int &i, const int &j) {
-    wstring bar = L"▉▊▋▌▍▎▏▎▍▌▋▊▉";
-    return u8 (bar[(i + j) % bar.size()]);
-}
+const std::string rules =
+      "rules of derivation: \n\n" "cst : cst   => 0\n" "lin : x     => 1\n" "add : a + b => a' + b'\n" "min : a - b => a' - b'\n"
+      "mul : a . b => a.b' + a'.b\n" "div : a / b => (a'. b − b'. a) / b²\n" "pow : x^a   => a.x^(a - 1)\n" "exp : e^x   => e^x\n"
+      "exp : a^x   => a^x . ln a\n" "ln  : ln(x) => 1 / x\n" "sin : sin x => cos x\n" "cos : cos x => -sin x\n" "tan : tan x => 1 / cos²x\n";
 
-class Bar {
-    public :
-        int width, height;
-        func fn;
+void apppend (std::vector<std::string> &history, const std::string &txt) {
+    std::istringstream iss (rules);
+    std::string buffer;
 
-        Bar (int a, int b, func cls) {
-            width = a, height = b, fn = cls;
-        }
-};
-
-string mkup (const Bar &curr) {
-    string os;
-    os += "┌";
-    for (int j = 0; j < curr.width; j++) {
-        os +="─";
+    while (getline (iss, buffer)) {
+        history.push_back(buffer);
     }
-    os +="┐";
-
-
-    return os;
-}
-string mkbot (const Bar &curr) {
-    string os;
-      os +=  "└";
-      for (int j = 0; j < curr.width; j++) {
-          os += "─";
-      }
-      os += "┘";
-    return os;
 }
 
-string inner (const Bar &curr, int i) {
-    string os;
-
-    os += "│";
-    for (int j = 0; j < curr.width; j++) {
-
-        os += curr.fn (i,j);
-    }
-
-    os += "│";
-    return os;
-}
-void animate () {
-  int width = w.ws_col;
-
-       vector<Bar> win ={Bar(width - 9, 1, progress), Bar(5, 1, waiting)};
-
-       for (int i = 0; i < width; i++) {
-             cout << "\033c\033[36m";
-
-             for (auto curr : win) {
-                cout << mkup (curr);
-             }
-             cout << "\n";
-
-             for (auto curr : win) {
-                cout << inner (curr, i);
-             }
-             cout << "\n";
-
-             for (auto curr : win) {
-               cout << mkbot (curr);
-             }
-             cout << "\n";
-             std::this_thread::sleep_for(std::chrono::milliseconds(70));
-       }
-}
-
-
-int main () {
+int main() {
 
     setlocale (LC_ALL, "en_US.UTF-8");
     initdim();
+    input_enter_off();
 
-    const std::string reset = "\033[0m", bright = "\033[1m", fainted = "\033[2m", italic = "\033[3m", underline = "\033[4m", blink = "\033[5m", reverse = "\033[7m";
-    wstring bar = L"▉▊▋▌▍▎▏▎▍▌▋▊▉▉";
-    int width = w.ws_col;
+    const std::vector<std::string> menu = {"File","Help","Base","Mode","clear","Time","Rain"};
+    int width = w.ws_col, height = w.ws_row;
 
-    std::string head = "┌─────────────────────────────────┐\n";
+    std::string head, bott;
+
+    head += "┌", bott += "└";
+    for (int i = 0; i < width - 2; i++) {
+        head += "─";
+        bott += "─";
+    }
+    head += "┐\n", bott += "┘\n";
 
 
-     cout << "exit";
+    std::string input = "x + 1 = 3";
+    std::cout << "\033c";
+
+    for (int j = 0; j < menu.size(); j++) {         // display menu bar
+        std::cout << " " << menu[j] << " ";
+    }
+
+    std::cout << std::string (4, '\n');
+    std::cout << head;
+    std::cout << "│ᐅ " << std::left << std::setw(width-4) << input << "│\n";
+    std::cout << bott;
+    const int hsize = 5;
+
+
+
+    std::vector<std::string> history;
+    int fst = history.size() - hsize;
+
+    std::cout << head;
+    for (int i = 0; i < hsize; i++) {
+        std::cout << "│";
+        std::cout << std::setw(width - 2);
+        if (i + fst >= 0) {
+            std::cout << history[i] ;
+        } else {
+            std::cout << " ";
+        }
+        std::cout << "│\n";
+    }
+    std::cout << bott;
+    // std::cout << bott;
+
+    std::cout << "\nexit\n";
+    return 0;
 }
