3cb58284d894c0a663bf8b9784dfa0c0e80a5e5f
Nemrod-7 <ocopey@gmail.com>
2023-11-24 19:27:18 +0100

    next

--- rust/polynomial.rs
+++ rust/polynomial.rs
@@ -1,33 +1,36 @@
 
-extern crate regex;
-use regex::Regex;
+#![allow(dead_code, unused)]
 
-fn tokenize (src: &str) -> Vec<String> {
-    let token = Regex::new("((^-| -)?[0-9]*[a-z](\\^[0-9]+)?)|(^-| -)?[0-9]+|[+-/*]").unwrap();
-    token.captures_iter(src).map(|x| x[0].to_string()).collect::<Vec<_>>()
-}
-fn getvar (src: &str) -> (i32,String,i32) {
+fn getvar (src: &str) -> (i64,String,i64) {
     let var = src.chars().filter(|x| x.is_alphabetic()).collect::<String>();
-    let exp = if let Some(x) = src.chars().position(|x| x == '^') { src[x+1..].parse::<i32>().unwrap() } else { 1 };
-    let mut mul = 1;
+    let exp = if let Some(x) = src.chars().position(|x| x == '^') { src[x+1..].parse::<i64>().unwrap() } else { 1 };
+    let mut mul = if src.find('-') == None { 1 } else { -1 };
 
     match src.chars().position(|x| x.is_alphabetic()) {
-        Some(x) => if x > 0 { mul = src[0..x].parse::<i32>().unwrap() } ,
-        None => mul = src.parse::<i32>().unwrap(),
+        Some(x) => if let Ok(num) = src[0..x].parse::<i64>() { mul = num },
+        None => mul = src.parse::<i64>().unwrap(),
     }
-    //print!("{}{}^{}\n", mul, var, exp);
+
     (mul, var, exp)
 }
-fn show (num: (i32,String,i32)) -> String {
+fn tokenize (src: &str) -> Vec<(i64,String,i64)> {
+    let src = src .chars().filter(|x| x != &' ').collect::<String>().replace("+", ",+").replace("-", ",-");    
+    let arg = src.split(',').collect::<Vec<_>>();
+
+    arg.iter().filter(|x| x != &&"").map(|x| getvar(x)).collect::<Vec<_>>()
+}
+fn show (num: &(i64,String,i64)) -> String {
     let mut os = String::new();
 
     if num.1 == "" {
-        os += &format!("{}\n", num.0);
-    } else {
-        if num.0 > 1 {
-            os += &format!("{}", num.0)
+        os += &format!("{}", num.0);
+    } else if num.0 != 0 {
+        match num.0 {
+            -1 => os += &format! ("-{}", num.1),
+            1  => os += &format! ("{}", num.1),
+            _  => os += &format! ("{}{}", num.0, num.1), 
         }
-        os += &format!("{}", num.1);
+
         if num.2 > 1 {
             os += &format!("^{}", num.2)
         } 
@@ -35,70 +38,88 @@ fn show (num: (i32,String,i32)) -> String {
 
     os
 }
-
-fn polynomial_product(sr1: &str, sr2: &str) -> String {
-    let arg1 = tokenize(sr1);
-    let arg2 = tokenize(sr2);
-    //print!("ex1 : {:?}\n", arg1);
-    //print!("ex2 : {:?}\n", arg2);
+fn polynomial_product(s1: &str, s2: &str) -> String {
+    let arg1 = tokenize(s1);
+    let arg2 = tokenize(s2); 
+    //print!("[{}] [{}]\n", s1, s2);
+    let mut i = 0;
     let mut res = String::new();
     let mut prod = Vec::new();
 
-    for arg in &arg1 {
-
-        if arg != "+" && arg != "-" {
-            let t1 = getvar(&arg);
-
-            for ar2 in &arg2 {
-                if ar2 != "+" && ar2 != "-" {
-                    let t2 = getvar(ar2);
-
-                    let x0 = t1.0 * t2.0;
-                    let mut x1 = t1.1.to_string();
-                    let mut x2 = t1.2;
-
-                    if t1.1 == t2.1 {
-                        x2 = t1.2 + t2.2;
-                    } else {
-                        x1 += &t2.1;
-                        x2 = std::cmp::max (t1.2,t2.2);
-                    }
-
-                    let nx = (x0, x1, x2);
-                    prod.push(nx);
-                }
+    for t1 in &arg1 {
+        for t2 in &arg2 {
+            let mut nx = (t1.0 * t2.0, t1.1.to_string(),std::cmp::max (t1.2,t2.2));
+            if t1.1 == t2.1 {
+                nx.2 = if t1.1 == "" { 1 } else { t1.2 + t2.2 };
+            } else {
+                nx.1 += &t2.1;
             }
-            // show(nu1);
+
+            prod.push(nx);
         }
     }
 
-    for t1 in prod {
-       res += &show(t1);
+    while i < prod.len() {
+        let mut j = i + 1;
+        while j < prod.len() {
+            if prod[i].1 == prod[j].1 && prod[i].2 == prod[j].2 {
+                prod[i].0 += prod[j].0;
+                prod[j] = (0, format!(" "), 0);
+          //      prod.remove(j);
+          //  } else {
+            }
+            j += 1;
+        }
+        i += 1;
     }
-    print!("{}\n", res);
-    /*
-    */
-    res
+
+    let mut prod = prod.iter().filter(|x| x.0 != 0).collect::<Vec<_>>();
+    prod.sort_by( 
+        |a,b| if a.2 != b.2 { b.2.cmp(&a.2) } else if a.1 != b.1 { b.1.cmp(&a.1) } else { b.0.cmp(&a.0) }
+    );
+
+    for i in 0..prod.len() {
+        //print!("{}[{}]{}\n", prod[i].0, prod[i].1, prod[i].2); 
+        if prod[i].0 > 0 && i > 0 {
+            res += "+";
+        }
+
+        res += &show(&prod[i]);
+    }
+
+    match res.len() {
+        0 => format!("0"),
+        _ => res,
+    }
 }
 
 fn main () {
 
-    polynomial_product("1", "-1"); // "-1"
-       polynomial_product("-1", "0"); // "0"
-       polynomial_product("1", "p^3"); // "p^3"
-       /*
-       polynomial_product("2", "4y - 4"); // "8y-8"
-       polynomial_product("u^2 + 2u+1", "u + 1");// "u^3+3u^2+3u+1"
+let a = "[34n^36+n^12+46n^23+49n^29+n^47-6n^44+n^14-n^3-n^5+n^11-6n^28-36n^19+44n^2+40n^4+3n^10-20n^48-3n^7-36n^45+18n^22-n^26-15n^16-n^40+n+2n^21+n^38+n^34+44n^24+n^43+n^33";
+let b = "[-45n^18+9n^47+49n^31-n^37-n^16+n^41+20n^5+20n^42-n^19-n^32-n^49+21n^7+n^20-36n^9-n^27-n^2+21n^25-45n^43+n^17+28n^15+19-3n^35+n^13-15n-45n^48-13n^44";
 
-       polynomial_product("x^2", "3x - 1"); // "3x^3-x^2"
-       polynomial_product("-4r^2 + 1", "-1"); // "4r^2 - 1"
-       polynomial_product("0", "2 - x") // "0"
-       polynomial_product("v^2 - 1+3v^3", "1+v^2"); // "3v^5+v^4+3v^3-1"
-       */
+    tests();
+
 
 }
 
+fn dotest(s1: &str, s2: &str, expected: &str) {
+    let actual = polynomial_product(s1, s2);
+    if actual != expected {
+        print!("expected : {} got : {}\n", expected, actual)
+    }
+}
+fn tests () {
+    dotest("x^2", "3x - 1", "3x^3-x^2");
 
+    dotest("v^2 - 1+3v^3", "1+v^2", "3v^5+v^4+3v^3-1");
+    dotest("u^2 + 2u+1", "u + 1", "u^3+3u^2+3u+1");
+    dotest("2", "4y - 4", "8y-8");
+    dotest("-4r^2 + 1", "-1", "4r^2-1");
+    dotest("1", "p^3", "p^3");
+    dotest("1", "-1", "-1");
+    dotest("0", "2 - x", "0");
+    dotest("-1", "0", "0");
 
-
+}
 
--- rust/test.rs
+++ rust/test.rs
@@ -1,32 +1,23 @@
+
 #![allow(dead_code, unused)]
 
-fn getvar (src: &str) -> (i32,String,i32) {
-    let sig = if src.find('-') == None { 1 } else { -1 };
-    let var = src.chars().filter(|x| x.is_alphabetic()).collect::<String>();
-    let exp = if let Some(x) = src.chars().position(|x| x == '^') { src[x+1..].parse::<i32>().unwrap() } else { 1 };
-    let mut mul = 1;
-
-    match src.chars().position(|x| x.is_alphabetic()) {
-        Some(x) => if x > 0 { mul = src[0..x].parse::<i32>().unwrap() } ,
-        None => mul = src.parse::<i32>().unwrap(),
-    }
-    //print!("{}{}^{}\n", mul, var, exp);
-    (sig * mul, var, exp)
-}
-fn tokenize (src: &str) {
-    let src = src .chars().filter(|x| x != &' ').collect::<String>().replace("+", ",+").replace("-", ",-");    
-    let arg = src.split(',').collect::<Vec<_>>();
-
-}
+use std::collections::LinkedList;
 
 fn main() {
 
-    let src = "v^2 - 1+3v^3";
+    let mut list = LinkedList::from([1,2,3,4,5,6,7,8,9]);
+    let mut iter = list.iter();
 
-    let src = src .chars().filter(|x| x != &' ').collect::<String>().replace("+", ",+").replace("-", ",-");    
-    let arg = src.split(',').collect::<Vec<_>>();
-    let arg = arg.iter().map(|x| getvar(x)).collect::<Vec<_>>();
+    if let Some(removed) = list.remove(1) {
+        println!("Removed element at index 1: {}", removed);
+    }
+    
+    while let Some(num) = iter.next() {
+        print!("{}", num);
+    }
+    print!("\n");
 
-    //print!("{:?}\n", arg);
+    /*
+    */
     //show(getvar(t1));
 }
