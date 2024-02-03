c391ffad5f327537c28a181ae102164e099c707e
Nemrod-7 <ocopey@gmail.com>
2024-02-03 19:09:04 +0100

    update

--- rust/trans/target/debug/build/proc-macro2-053ea2782b9cb01e/build_script_build-053ea2782b9cb01e.d
+++
@@ -1,5 +0,0 @@
-/home/wintermute/code/codewars/rust/trans/target/debug/build/proc-macro2-053ea2782b9cb01e/build_script_build-053ea2782b9cb01e: /home/wintermute/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.78/build.rs
-
-/home/wintermute/code/codewars/rust/trans/target/debug/build/proc-macro2-053ea2782b9cb01e/build_script_build-053ea2782b9cb01e.d: /home/wintermute/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.78/build.rs
-
-/home/wintermute/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.78/build.rs:
