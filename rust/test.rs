058660999b0bb83322ad1db21719d0827ef7b584
Nemrod-7 <ocopey@gmail.com>
2024-02-05 18:42:32 +0100

    update

--- rust/test.rs
+++ rust/test.rs
@@ -1,5 +1,5 @@
 #![allow(dead_code, unused)]
-
+use std::collections::HashMap;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Element { C, H, O, B, Br, Cl, F, Mg, N, P, S, }
@@ -13,34 +13,39 @@ struct Atom {
 }
 struct Molecule {
     id:String,
-    atoms:Vec<Atom>,
+    subs:Vec<Vec<Atom>>,
+    lock:bool,
 }
+impl Molecule {
+    fn tablex (&mut self) {
 
+    }
+    fn branch (&mut self, nxt: &[usize]) {
+
+    }
+    fn bond (&mut self, )
+}
 fn main () {
 
-    let src = [14,1,1];
+    let nxt = [14,1,1];
 
-    let mut index = 0;
-    let carbon = Atom{id: index, element:C, edge:vec![] };
+    let carbon = Atom{id: 0, element:C, edge:vec![] };
+    let hydrog = Atom{id: 0, element:H, edge:vec![] };
 
-    for i in 0..src.len() {
-        let mut branch:Vec<Atom> = (0..src[i]).map(|_| Atom {id:0, element:C, edge:vec![]}).collect();
+    let mut subs:Vec<Vec<Atom>> = Vec::new();
 
-        for j in 0..branch.len() {
-            branch[j].id = index;
-            index += 1;
-        }
+     for _ in 0..nxt.len() {
+         let mut atoms:Vec<Atom> = (0..=src[i]).map(|ix| Atom {id:ix, element:C, edge:vec![]}).collect();
 
-
-        for j in 0..branch.len() - 1 {
-            let a = j;
-            let b = j + 1;
-            let ida = branch[a].id;
-            let idb = branch[b].id;
-
-            branch[a].edge.push(idb);
-            branch[b].edge.push(ida);
-
-        }
-    }
+    //     for j in 0..atoms.len() - 1 {
+    //         let a = j;
+    //         let b = j + 1;
+    //         let ida = atoms[a].id;
+    //         let idb = atoms[b].id;
+    //
+    //         atoms[a].edge.push(idb);
+    //         atoms[b].edge.push(ida);
+    //
+    //     }
+     }
 }
