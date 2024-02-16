7eacb5ddb48a1ee780848769959c9e7ca80fb699
Nemrod-7 <ocopey@gmail.com>
2024-02-16 18:52:45 +0100

    udapte

--- test.cpp
+++ test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
-
 
 enum type_e {zero, equals, unequal};
 
@@ -16,7 +16,7 @@ struct result : public testing {
         return *this;
     }
 
-    template<typename T> 
+    template<typename T>
         result EqualTo(const T &x) {
             if (type == zero) type = equals;
 
@@ -24,17 +24,50 @@ struct result : public testing {
         }
 };
 
+// result Is () { return {zero}; }
+int smbase (const unsigned long int N) {
+    if (N % 10 == 1) {
+        // return N - 1;
+    }
+    for (int i = 2; i < N; i++) {
+        int num = N;
 
+        while (num % i == 1) {
+            num /= i;
+        }
 
+        if (num == 0) {
+            return i;
+        }
+    }
 
-result Is () { return {zero}; }
-
-
-
+    return -1;
+}
 int main () {
 
     //fn res = Is().Not().EqualTo(3);
-    result expect = Is().Not().EqualTo(3.5);
+    // result expect = Is().Not().EqualTo(3.5);
+
+    int number = 6587;
+    int mul = 10;
+    int temp = number % mul;
+    vector<vector<int>> base(10, vector<int> (1,1));
+
+    while (temp < number) {
+        temp = number % mul;
+
+        for (int i = 0; i < 10; i++) {
+            int num = base[i].back();
+            base[i].push_back(num * i);
+            cout << num << ' ';
+        }
+
+        cout << '\n';
+
+        mul *= 10;
+    }
+
+
 
 
 }
