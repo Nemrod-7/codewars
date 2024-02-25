2b5154190d7b4ff6ebf24b53952f5f4c8c13483c
Nemrod-7 <ocopey@gmail.com>
2024-02-25 19:10:33 +0100

    update

--- cockroach.cpp
+++ cockroach.cpp
@@ -4,79 +4,79 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
 bool is_inside (int x, int y, int width, int height) { return x >= 0 && y >= 0 && x < width && y < height; }
 
-std::array<int, 10> cockroaches(const std::vector<std::string> &room) {
-  array<int, 10> freq;
-  vector<pair<int,int>> hole;
-  vector<string> curr = room, next = curr;
+std::array<int, 10> cockroaches(const std::vector<std::string> &grid) {
+    const std::vector<std::pair<int,int>> direction = {{-1,0},{0,-1},{0,1},{1,0}};
+    const int width = grid[0].size(), height = grid.size();
+    std::array<int, 10> freq {0};
+    std::vector<std::pair<char,std::pair<int,int>>> roaches;
 
-    for (int i = 0; i < curr.size(); i++) {
-        for (int j = 0; j < curr[i].size(); j++) {
-            char ch = curr[i][j];
-            pair<int,int> nx = {j,i};
-
-            if (ch == 'U') {
-                // next[i][j] = ' ';
-                // next[i-1][j] = 'U';
-                nx = {j,i-1};
-            } else if (ch == 'D') {
-                // next[i][j] = ' ';
-                // next[i+1][j] = 'D';
-                nx = {j,i+1};
-            } else if (ch == 'R') {
-                // next[i][j] = ' ';
-                // next[i][j+1] = 'R';
-                nx = {j+1,i};
-
-            } else if (ch == 'L') {
-                // next[i][j] = ' ';
-                // next[i][j-1] = 'L';
-                nx = {j-1,i};
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            if (isalpha(grid[y][x])) {
+                roaches.push_back({grid[y][x], {x,y}});
             }
-
-            next[i][j] = ' ';
-            next[nx.second][nx.first] = ch;
         }
     }
-    curr = next;
 
-    for (int i = 0; i < room.size(); i++) {
-        for (int j = 0; j < room[i].size(); j++) {
-            cout << next[i][j];
+    for (auto [dir,pos] : roaches) {
+        bool search = true;
+
+        while (search) {
+            for (auto &[dx,dy] : direction) {
+                int nx = pos.first + dx, ny = pos.second + dy;
+
+                if (is_inside (nx,ny,width,height)) {
+
+                    if (isdigit (grid[ny][nx])) {
+                        freq[grid[ny][nx] - '0']++;
+                        search = false;
+                        break;
+                    } else if (grid[ny][nx] == '|') {
+                        if (dir == 'R') {dir = 'U'; break; }
+                        if (dir == 'L') {dir = 'D'; break; }
+                    } else if (grid[ny][nx] == '-') {
+                        if (dir == 'U') {dir = 'L'; break; }
+                        if (dir == 'D') {dir = 'R'; break; }
+                    }
+                }
+            }
+
+            switch (dir) {
+                case 'L': pos.first--; break;
+                case 'R': pos.first++; break;
+                case 'U': pos.second--; break;
+                case 'D': pos.second++; break;
+            }
         }
-        cout << "\n";
     }
 
     return freq;
 }
-
 int main () {
 
-      vector<string> room = {
-    "+----------------0---------------+",
-    "|                                |",
-    "|                                |",
-    "|          U        D            |",
-    "|     L                          |",
-    "|              R                 |",
-    "|           L                    |",
-    "|  U                             1",
-    "3        U    D                  |",
-    "|         L              R       |",
-    "|                                |",
-    "+----------------2---------------+"
-};
-// , {1,2,2,5,0,0,0,0,0,0});
+    std::vector<std::string> room = {
+        "+----------------0---------------+",
+        "|                                |",
+        "|                                |",
+        "|          U        D            |",
+        "|     L                          |",
+        "|              R                 |",
+        "|           L                    |",
+        "|  U                             1",
+        "3        U    D                  |",
+        "|         L              R       |",
+        "|                                |",
+        "+----------------2---------------+"
+    };
+    // , {1,2,2,5,0,0,0,0,0,0});
+    auto res = cockroaches(room);
 
-    // There are 0 or more cockroaches in the room
-    // Cockroaches firstly run in a straight line in the direction they are facing
-    // When they hit a wall they always turn LEFT and then they follow the wall until they can find a hole to crawl into!
-    // Cockroaches do not bump into each other
-    // There are no cockroaches outside the room
-
-    cockroaches(room);
+    for (int i = 0; i < 10; i++) {
+        if (res[i]) {
+            std::cout << i << " :: " << res[i] << "\n";
+        }
+    }
 
 }
