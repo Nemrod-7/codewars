e0e72228a82fd0240d9cd36bbb76838a7e96ec27
Nemrod-7 <ocopey@gmail.com>
2024-04-27 10:59:22 +0200

    update

--- next/dithering.cpp
+++ next/dithering.cpp
@@ -1,94 +1,52 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
+#include <queue>
 
 using namespace std;
 
-void recurse (int width, int height) {
-
-}
-
 int main () {
 
-  /*
-    2x2 : 1 3 4 2
-    1 3
-    4 2
-
-    4×4 : 1 9 3 11 13 5 15 7 4 12 2 10 16 8 14 6
-     1  9  3 11
-    13  5 15  7
-     4 12  2 10
-    16  8 14  6
-
-    8×8 :
-    vector<vector<int>> grid = {
-    { 1, 33,  9, 41,  3, 35, 11, 43},
-    {49, 17, 57, 25, 51, 19, 59, 27},
-    {13, 45,  5, 37, 15, 47,  7, 39},
-    {61, 29, 53, 21, 63, 31, 55, 23},
-    { 4, 36, 12, 44,  2, 34, 10, 42},
-    {52, 20, 60, 28, 50, 18, 58, 26},
-    {16, 48,  8, 40, 14, 46,  6, 38},
-    {64, 32, 56, 24, 62, 30, 54, 22} };
-    ref = {1,33,9,41,3,35,11,43, 49,17,57,25,51,19,59,27, 13,45,5,37,15,47,7,39, 61,29,53,21,63,31,55,23, 4,36,12,44,2,34,10,42, 52,20,60,28,50,18,58,26, 16,48,8,40,14,46,6,38, 64,32,56,24,62,30,54,22};
-
-    3x3 :
-    1 6 3
-    8 5 9
-    4 7 2
-
-    // 1  2  3  4  5  6  7  8  9  10 11 12 13 14 15
-    // 8,-6, 8, 2,-8,10,-8,-3, 8,-10, 8, 6,-8, 6,-8
-    // +     +     -     -     +      +     -     -
-    */
-
-    int size = 4, s2 = size * size;
+    int width = 8, height = 8;
     vector<int> ref = {1,9,3,11,13,5,15,7,4,12,2,10,16,8,14,6}; // 4x4
 
-    const vector<vector<int>> seq = {{0,2},{3,1}};
-    vector<vector<int>> mat (size, vector<int> (size));
+    vector<vector<int>> mat (width, vector<int> (height));
+    vector<vector<bool>> visit (width, vector<bool> (height));
 
-    for (int x = 0; x < 2; x++) {
-        for (int y = 0; y < 2; y++) {
-            int nx = x * size / 2, ny = y * size / 2;
-            mat[nx][ny] = seq[x][y] + 1;
-         }
+    queue<pair<int,int>> s1 ;
+    vector<pair<int,int>> order = {{0,0},{1,1},{0,1},{1,0}};
+    s1.push({0,0});
+    int index = 1;
+
+    int cycle = 0;
+
+    while (!s1.empty()) {
+        auto [sx, sy] = s1.front();
+        s1.pop();
+
+        cycle++;
+        if (cycle == 3) {
+            break;
+        }
+        cout << sx << " " << sy << "\n";
+
+        if (!visit[sx][sy]) {
+            visit[sx][sy] = true;
+
+            for (int i = 0; i < 4; i++) {
+              auto [dx,dy] = order[i];
+              int nx = dx * width / 2 + sx, ny = dy * height / 2 + sy;
+
+              mat[nx][ny] = index++;
+              // cout << nx / 2 << " " << ny / 2 << "\n";
+              s1.push({nx / 2, ny / 2});
+            }
+        }
     }
 
-    for (int x = 0; x < size; x++) {
-        for (int y = 0; y < size; y++) {
-            cout << mat[x][y] << ' ';
+    for (int x = 0; x < width; x++) {
+        for (int y = 0; y < height; y++) {
+            cout <<  mat[x][y] << ' ';
         }
         cout << endl;
     }
-
-
-}
-
-void arkive() {
-
-    // int sign = 1;
-    // for (int i = 0; i < size; i++) {
-    //     for (int j = 0; j < size; j++) {
-    //         // M(i, j) = bit_reverse(bit_interleave(bitwise_xor(i, j), i)) / n ^ 2;
-    //         // M(i, j) = bit_reverse(bit_interleave(i^j, i)) / s2;
-    //         cout << xor(i,j) << ' ';
-    //     }
-    //     cout << endl;
-    // }
-    // for (int i = 1; i < s2; i += size) {
-    //     for (int j = 0; j < size; j += 2) {
-    //       seq[i + j] = ((size * size) / 2) * sign;
-    //       // cout << i + j << " ";
-    //     }
-    //     sign *= -1;
-    // }
-    //
-    //
-    // for (int i = 1; i < ref.size(); i++) {
-    //     // cout << ref[i-0] - ref[i-1] << " " ;
-    //     cout << seq[i] << " ";
-    // }
 
 }
