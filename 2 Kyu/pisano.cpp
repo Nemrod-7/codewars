be0885eb4ea1fa1f92741bb3ab71f940d4ee5701
Nemrod-7 <ocopey@gmail.com>
2024-06-15 14:55:48 +0200

    update

--- 2 Kyu/pisano.cpp
+++ 2 Kyu/pisano.cpp
@@ -78,7 +78,7 @@ vector<pair<uint64_t,uint64_t>> factors (uint64_t N) { // returns all factors of
     if (N > 1) vs.push_back({N,1});
     return vs;
 }
-vector<uint64_t> divisors (const uint64_t N) { // returns all divisors of N
+vector<uint64_t> divisors (const uint64_t N) { // returns all sorted divisors of N
     vector<uint64_t> divisor = {1};
     vector<pair<uint64_t,uint64_t>> fac = factors(N);
 
@@ -98,23 +98,22 @@ vector<uint64_t> divisors (const uint64_t N) { // returns all divisors of N
     return divisor;
 }
 
-pair<int64_t,int64_t> fibonacci (int64_t n, int64_t mod) {
-  // Function calculate the N-th fibonacci number using fast doubling method
-  // returns [Fn, Fn+1]
+pair<uint64_t,uint64_t> fibonacci (uint64_t n, uint64_t mod) {
+  // Function calculate the N-th fibonacci number using fast doubling method => returns [Fn, Fn+1]
     if (n == 0) return {0,1};
-    auto [f0, f1] = fibonacci ((n / 2), mod);
-    int64_t c = 2 * f1 - f0;
+    auto [f0, f1] = fibonacci (n / 2, mod);
+    // As F(2n + 1) = F(n)^2 + F(n+1)^2, As F(2n) = F(n)[2F(n+1) – F(n)]
+    int64_t c = mulmod(2, f1, mod) - f0, d = addmod(f0 * f0, f1 * f1, mod);
 
     if (c < 0) c += mod;
-    c = (f0 * c) % mod; // As F(2n) = F(n)[2F(n+1) – F(n)]
+    c = mulmod(f0, c, mod);
 
-    int64_t d = (f0 * f0 + f1 * f1) % mod; // As F(2n + 1) = F(n)^2 + F(n+1)^2
-
-    return (n % 2 == 0) ? pair<int64_t,int64_t> {c, d} : pair<int64_t,int64_t> {d, c + d};
+    return (n % 2 == 0) ? pair<uint64_t,uint64_t> {c, d} : pair<uint64_t,uint64_t> {d, c + d};
 }
+
 uint64_t pisano_prime (uint64_t N) {
 
-    const pair<int64_t, int64_t> period = {0,1};
+    const pair<uint64_t, uint64_t> period = {0,1};
     uint64_t cycle = 2 * N + 2;
     vector<uint64_t> divisor, d1 = divisors(N - 1);
 
@@ -136,6 +135,7 @@ uint64_t pisano_prime (uint64_t N) {
     }
 
     for (auto &d : divisor) {
+        // cout << fibonacci(d, N) << "\n" ;
         if (fibonacci(d, N) == period) {
             cycle = min(d,cycle);
         }
@@ -153,13 +153,10 @@ uint64_t pisano_period (uint64_t N) { // OEIS A001175 pisano period of fib(i) mo
 
     if (fac.size() == 1 && fac[0].second == 1) { // if n is a prime number
         cycle = pisano_prime(N);
-
     } else {
         // if p is a prime number and k is interger power : pisano(p^k) == p^(k-1) * pisano (p)
         for (auto &[p,ex] : fac) {
-            int an = pisano_period(p) * powmod(p, ex - 1, N);
-
-            cycle = lcm(cycle, an) ;
+            cycle = lcm(cycle, pisano_period(p) * powmod(p, ex - 1, N)) ;
         }
     }
 
@@ -170,12 +167,61 @@ int main () {
 
     const vector<int> pi = {1, 1, 3, 8, 6, 20, 24, 16, 12, 24, 60, 10, 24, 28, 48, 40, 24, 36, 24, 18, 60, 16, 30, 48, 24, 100, 84, 72, 48, 14, 120, 30, 48, 40, 36, 80, 24, 76, 18, 56, 60, 40, 48, 88, 30, 120, 48, 32, 24, 112, 300, 72, 84, 108, 72, 20, 48, 72, 42, 58, 120, 60, 30, 48, 96, 140, 120, 136};
 
-    uint64_t N = 2438389198053, res = 10624179384;
+    int64_t N = 2474324059;
+    int64_t d = 2474324058;
 
-    uint64_t cycle = 2 * N + 2;
-    const pair<int64_t, int64_t> period = {0,1};
+    Assert::That(pisano_period(4256513951), Equals(851302790)); // primes_32_bits
+    // Assert::That(pisano_period(1840094077), Equals(3219724224703183324)); // primes_64_bits
 
-    Test();
+    // Test();
     cout << "\nexit\n";
     //Assert::That(pisano_period(2438389198053), Equals(10624179384));
 }
+
+/*
+uint64_t pisano_period2 (uint64_t N) { // OEIS A001175 pisano period of fib(i) mod n
+    if (N == 1) return 1;
+    if (N == 2) return 3;
+    if (N == 5) return 20;
+
+    const pair<int64_t, int64_t> period = {0,1};
+    uint64_t cycle = 1;
+    vector<pair<uint64_t,uint64_t>> fac = factors(N);
+
+    if (fac.size() == 1 && fac[0].second == 1) { // if n is a prime number
+        vector<uint64_t> divisor, d1 = divisors(N - 1);
+        cycle = 2 * N + 2;
+
+        if ((N - 1) % 10 == 0 || (N + 1) % 10 == 0) {
+            for (auto &d : d1) {
+                if (d % 2 == 0) {
+                    divisor.push_back(d);
+                }
+            }
+
+        } else {
+            vector<uint64_t> d2 = divisors(2 * N + 2);
+
+            for (auto &d : d2) {
+                if (find(d1.begin(), d1.end(), d) == d1.end()) {
+                    divisor.push_back(d);
+                }
+            }
+        }
+
+        for (auto &d : divisor) {
+
+            if (fibonacci(d, N) == period) {
+                cycle = min(d,cycle);
+            }
+        }
+    } else {
+        // if p is a prime number and k is interger power : pisano(p^k) == p^(k-1) * pisano (p)
+        for (auto &[p,ex] : fac) {
+            cycle = lcm(cycle, pisano_period(p) * powmod(p, ex - 1, N)) ;
+        }
+    }
+
+    return cycle;
+}
+*/
