e38e5026b6f3be5be12d5b2dd9fea0a7ece31abe
Nemrod-7 <ocopey@gmail.com>
2024-07-12 06:42:33 +0200

    update

--- rust/sieve.rs
+++ rust/sieve.rs
@@ -1,36 +1,55 @@
 #![allow(dead_code,unused)]
 
+use std::time::{Duration, Instant};
 
 fn main() {
 
+    let now = Instant::now();
     let n = 100;
 
-    let mut sieve = vec![false; n >> 1];
+    let mut sieve = vec![false; n];
     let mut primes = vec![2];
 
     (3..n)
         .step_by(2)
         .for_each(
-            |i| if sieve[i >> 1] == false {
+            |i| if sieve[i] == false {
                 primes.push(i);
 
                 (i*i..n)
                     .step_by(2 * i)
-                    .for_each(|j| sieve[j >> 1] = true)
+                    .for_each(|j| sieve[j] = true)
             });
 
-    // print!("{:?}\n", primes);
+
+    let seq = [ 4,6,10,12,16 ];
+
+    for i in (3..n).step_by(2) {
+        let mut flag = true;
+
+        if sieve[i] == false {
+            for j in seq {
+
+                if sieve[j] == true {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+
+        if flag == true {
+            for j in seq {
+
+            }
+        }
+    }
+
+    //print!("{:?}\n", primes);
 
     // let primes = (3..n)
     //     .step_by(2)
     //     .filter(|&x| sieve[x] == false)
     //     .collect::<Vec<_>>();
 
-    let re = 1;
-
-    for i in 1..8 {
-        let nu = re << i;
-        print!("{i} : {nu}\n");
-    }
-
+    println!("time elapsed : {} ms", now.elapsed().as_millis());
 }
