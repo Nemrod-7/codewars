d295bc95ae1bde72940e0ee83806152bbf4e5520
Nemrod-7 <ocopey@gmail.com>
2024-07-13 15:25:51 +0200

    update

--- rust/sieve.rs
+++ rust/sieve.rs
@@ -2,49 +2,80 @@
 
 use std::time::{Duration, Instant};
 
-fn main() {
-
-    let now = Instant::now();
-    let n = 100;
+fn sieve (n:usize) {
 
     let mut sieve = vec![false; n];
-    let mut primes = vec![2];
-
     (3..n)
         .step_by(2)
         .for_each(
             |i| if sieve[i] == false {
-                primes.push(i);
-
                 (i*i..n)
                     .step_by(2 * i)
                     .for_each(|j| sieve[j] = true)
             });
+}
 
+fn getsize (size:usize) { // human-readable size
 
-    let seq = [ 4,6,10,12,16 ];
+    let size = size as f64;
+    let sizes = [("TiB",1099511627776.0),("GiB",1073741824.0),("MiB",1048576.0),("KiB",1024.0),("B",1.0)];
 
-    for i in (3..n).step_by(2) {
-        let mut flag = true;
-
-        if sieve[i] == false {
-            for j in seq {
-
-                if sieve[j] == true {
-                    flag = false;
-                    break;
-                }
-            }
-        }
-
-        if flag == true {
-            for j in seq {
-
-            }
+    for curr in sizes {
+        if size >= curr.1 {
+            print!("{:.2} {}\n", size / curr.1, curr.0);
+            break;
         }
     }
+}
 
-    //print!("{:?}\n", primes);
+fn main() {
+
+    let now = Instant::now();
+    let limit = 1000;
+    let max = 1e9 as usize;
+
+    let seq = [0,4,6,10,12,16];
+    let seq = vec![16,12,10,6,4,0];
+    //let mut sieve = vec![true; max];
+
+    let mut searching = true;
+    let mut i = 3;
+
+    getsize(max >> 1);
+    /*
+       while searching && i < max {
+
+       if sieve[i] {
+       let mut sum:i32 = 0;
+
+       for &j in &seq {
+       let ix = (i as i32) - (j as i32);
+
+       if ix < 2 || !sieve[ix as usize] { 
+       sum = 0;
+       break;
+       } else {
+       sum += ix
+       }
+       }
+
+       if sum > 0 {
+       print!("{sum} = ");
+       for j in &seq {
+       print!("{} ", i - j);
+       }
+
+       print!("\n");
+       }
+
+       (i*i..max)
+       .step_by(2 * i)
+       .for_each(|j| sieve[j] = false)            
+       }
+
+       i += 2;
+       }
+       */
 
     // let primes = (3..n)
     //     .step_by(2)
