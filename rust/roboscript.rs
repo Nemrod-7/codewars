10a225c8ca68f0e9a0a57ee3c011fb93ec9f64cb
Nemrod-7 <ocopey@gmail.com>
2024-07-19 07:56:37 +0200

    update

---
+++ rust/roboscript.rs
@@ -0,0 +1,160 @@
+#![allow(dead_code,unused)]
+use std::iter::Peekable;
+
+fn tokenize<'a>( program : &'a str) -> Vec<String> {
+    let mut tokens : Vec<String> = vec![];
+    let mut iter = program.chars().peekable();
+
+    loop {
+        match iter.peek() {
+            Some(&c) => match c {
+                'a'..='z'|'A'..='Z' => {
+                    let mut tmp = String::new();
+                    while iter.peek().is_some() && iter.peek().unwrap().is_alphabetic() {
+                        tmp.push(iter.next().unwrap());
+                    }
+                    tokens.push(tmp);
+                },
+                '0'..='9' => {
+                    let mut tmp = String::new();
+                    while iter.peek().is_some() && iter.peek().unwrap().is_numeric() {
+                        tmp.push(iter.next().unwrap());
+                    }
+                    tokens.push(tmp);
+                },
+                ' ' => { iter.next(); },
+                _ => {
+                    tokens.push(iter.next().unwrap().to_string());
+                },
+            },
+            None => break
+        }
+    }
+
+    tokens
+}
+fn highlight(code :&str) -> String {
+
+    let mut code = code.chars().peekable();
+    let mut os = String::new();
+
+    while let Some(ch) = code.next() {
+        let mut com = format!("{ch}");
+
+        match ch {
+            '0'..='9' => {
+                while let Some(nxt) = code.peek() {
+                    match nxt {
+                        '0'..='9' => com.push(code.next().unwrap()),
+                        _ => break,
+                    }
+                }
+
+                os += &format!("<span style=\"color: orange\">{com}</span>");
+            }
+            _ => {
+                while code.peek() == Some(&ch) {
+                    com.push( code.next().unwrap()) ;
+                }
+
+                match ch {
+                    '(' => os += &format!("{com}"),
+                    ')' => os += &format!("{com}"),
+                    'F' => os += &format!("<span style=\"color: pink\">{com}</span>"),
+                    'L' => os += &format!("<span style=\"color: red\">{com}</span>"),
+                    'R' => os += &format!("<span style=\"color: green\">{com}</span>"),
+                    _ => unreachable!(),    
+                }
+os += "\n";
+            },
+        }
+    }
+
+    print!("{os}");
+    os
+}
+
+fn getnum (code : &mut Peekable<impl Iterator<Item = char>>) -> i32 {
+    let mut tmp = String::new(); 
+
+    while let Some(dig) = code.peek() {
+        match dig {
+            '0'..='9' => tmp.push(code.next().unwrap()),
+            _ => break,
+        }
+    }
+
+    match tmp.parse::<i32>() {
+        Ok(num) => { num },
+        Err(_) => { 1 },
+    }
+}
+fn execute (code: &str) -> String {
+
+    let compass = [(1,0),(0,1),(-1,0),(0,-1)];
+    let x = [1,0,-1,0];
+    let y = [0,1,0,-1];
+
+    let mut ix = 0;
+    let mut code = code.chars().peekable();
+    let mut hist = vec![(0,0)]; 
+
+    while let Some(curr) = code.next() {
+        match curr {
+            'F' => {
+                let coef = getnum(&mut code);
+                let last = hist[hist.len() - 1];
+
+                hist.push((coef * x[ix] + last.0, coef * y[ix] + last.1)); 
+                //hist.push((coef * compass[ix].0 + last.0, coef * compass[ix].1 + last.1)); 
+            }
+            'R' => { ix = if ix == 3 { 0 } else { ix + 1 }; },
+            'l' => { ix = if ix == 0 { 3 } else { ix - 1 }; },
+            _ => {},
+        }
+    }
+
+    construct(&hist);
+    format!("")
+}
+
+fn construct(path:  &Vec<(i32,i32)>) {
+
+    let mut minx = path.iter().min_by_key(|x| x.0).unwrap().0;
+    let mut maxx = path.iter().max_by_key(|x| x.0).unwrap().0;
+    let mut miny = path.iter().min_by_key(|x| x.1).unwrap().1;
+    let mut maxy = path.iter().max_by_key(|x| x.1).unwrap().1;
+
+    let width = minx.abs() + maxx.abs() + 1;
+    let height = miny.abs() + maxy.abs() + 1;
+
+    let mut grid = vec![vec!['.'; width as usize]; height as usize];
+
+    for it in path.iter() {
+        let x = it.0;
+        let y = it.1;
+
+        grid[y as usize][x as usize] = 'x';
+    }
+
+    print!("{} {} {} {}\n", minx, maxx, miny, maxy);
+    print!("width : {} height : {} \n", width, height);
+
+    for line in grid {
+
+        print!("{:?}\n", line);
+    }
+
+}
+
+fn main() {
+
+    let code = "FFFRFFFFRFFFRFFF";
+    execute(&code);
+
+
+
+
+    //print!("{:?}", curr);
+
+}
--- rust/sieve.rs
+++ rust/sieve.rs
@@ -15,8 +15,8 @@ fn sieve (n:usize) {
             });
 }
 
-fn getsize (size:usize) { // human-readable size
-
+fn showsize (size:usize) { // show size in human-readable format
+                           
     let size = size as f64;
     let sizes = [("TiB",1099511627776.0),("GiB",1073741824.0),("MiB",1048576.0),("KiB",1024.0),("B",1.0)];
 
@@ -28,37 +28,33 @@ fn getsize (size:usize) { // human-readable size
     }
 }
 
-fn main() {
+fn find_primes_sextuplet (sum_limit: u32) -> Vec<u32> {
 
-    let now = Instant::now();
-    let limit = 1000;
     const WHEEL: [usize;8] = [4,2,4,2,4,6,2,6];
-    // 29 700 000
     let max = 1e8 as usize;
     let mut i = 7;
     let mut u = 0;
-    let mut sieve = vec![0u32; (max >> 5) + 1];
-
     let mut seq = Vec::new();
-    //getsize(max >> 6);
-    // (i..max).step_by(2).for_each(|i| {})
-    //print!("2 3 5 ");
+    let mut sieve = vec![0u32; (max >> 5) + 1];
 
     while i < max {
         if (sieve[i >> 5] >> (i &31)&1) == 0 {
             let mut j = i * i;
-            if u < 6 { seq.push(i) }
+            let mut v = u;
+            if u < 6 { seq.push(i as u32) }
 
-           // print!("{} ", i);
             while j < max {
                 sieve[j >> 5] |= 1 << (j &31);
-                j += 2 * i;
+                j += i * WHEEL[v];
+                v = if v == 7 { 0 } else { v + 1 };
             }
+
+
         }
 
-        if seq.len() == 6 {
-            let sum:usize = seq.iter().sum();
-            print!("seq : {:?} {}\n", seq, sum);
+        if seq.len() == 6 && seq.iter().sum::<u32>() > sum_limit {
+            print!("seq : {:?} \n", seq);
+            break;
         }
 
         if u == 5 && seq.len() > 0 {
@@ -69,7 +65,27 @@ fn main() {
         u = if u == 7 { 0 } else { u + 1 };
     }
 
+    seq
+}
 
+fn dotest(n: u32, expected: &[u32]) {
+    let actual = find_primes_sextuplet(n);
+    assert!(actual == expected, "With n = {n}\nExpected {expected:?} but got {actual:?}")
+}
+fn fixed_tests() {
+    dotest(70, &[7, 11, 13, 17, 19, 23]);
+    dotest(600, &[97, 101, 103, 107, 109, 113]);
+    dotest(2000000, &[1091257, 1091261, 1091263, 1091267, 1091269, 1091273]);
+}
+
+fn main() {
+
+    let now = Instant::now();
+    let limit = 1000;
+    const WHEEL: [usize;8] = [4,2,4,2,4,6,2,6];
+    // 29 700 000
+
+    find_primes_sextuplet (1000);
     // let primes = (3..n)
     //     .step_by(2)
     //     .filter(|&x| sieve[x] == false)
