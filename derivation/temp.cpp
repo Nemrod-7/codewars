cbde5fd9d32b5588ad5cab068fac7590db989243
Nemrod-7 <ocopey@gmail.com>
2024-09-14 18:27:19 +0200

    update

--- derivation/temp.cpp
+++ derivation/temp.cpp
@@ -4,15 +4,31 @@
 
 using namespace std;
 
+struct node {
+
+    //node(string src) : x(src) {}
+    //node(int v) : c(v) {}
+};
+
 int main () {
 
-		string expr = "cot(39.6^11.5*42.3/x+93.7^73.3)";
-		complex<double> x (-4.87,-7.25);
+//    string expr = "cot(39.6^11.5*42.3/x+93.7^73.3)";
+//    complex<double> x (-4.87,-7.25);
+//
+//    complex<double> a(39.6), b(11.5), c(42.3), d(93.7), e(73.3);
+//    complex<double> rz = pow(a,b) * c / x + pow(d,e);
+//    //cout << rz;
+//
+//    const complex<double> zero(0.0,0.0), one(1.0,0.0);
+//
+//    cout << cos(zero) << "\n";
+//    cout << sin(zero) << "\n";
+//    cout << tan(zero) << "\n";
+//
+//    cout << cos(one) << "\n";
+//    cout << sin(one) << "\n";
+//    cout << tan(one) << "\n";
 
-		complex<double> a(39.6), b(11.5), c(42.3), d(93.7), e(73.3);
-		complex<double> rz = pow(a,b) * c / x + pow(d,e);
-
-		cout << cos(rz);
-
+    //curr.x = "cos";
 
 }
