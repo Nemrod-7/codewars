a8b8b9d16dfc55b5bf2472f380e2fc2e050dcd14
Nemrod-7 <ocopey@gmail.com>
2024-09-27 19:06:52 +0200

    update

--- alphawar.c
+++ alphawar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -27,14 +28,14 @@ const char *count (const char *src) {
 
     for (int i = 0; src[i] != '\0'; i++) {
         switch(src[i]) {
-          case 's': lfs += 1; break;
-          case 'z': rfs += 1; break;
-          case 'b': lfs += 2; break;
-          case 'd': rfs += 2; break;
-          case 'p': lfs += 3; break;
-          case 'q': rfs += 3; break;
-          case 'w': lfs += 4; break;
-          case 'm': rfs += 4; break;
+            case 's': lfs += 1; break;
+            case 'z': rfs += 1; break;
+            case 'b': lfs += 2; break;
+            case 'd': rfs += 2; break;
+            case 'p': lfs += 3; break;
+            case 'q': rfs += 3; break;
+            case 'w': lfs += 4; break;
+            case 'm': rfs += 4; break;
         }
     }
 
@@ -49,63 +50,96 @@ const char *count (const char *src) {
 
 void reinforce(char *field, int nforce, char *force[nforce]) {
 
-		const int size = strlen(force[0]);
+    const int size = strlen(force[0]);
 
     for (int i = 0; i < nforce; i++){
-         for (int j = 0; j < size; j++) {
-             if (force[i][j] != '_' && field[j] == '_') {
-                 field[j] = force[i][j];
-                 force[i][j] = '_';
-             }
-         }
+        for (int j = 0; j < size; j++) {
+            if (force[i][j] != '_' && field[j] == '_') {
+                field[j] = force[i][j];
+                force[i][j] = '_';
+            }
+        }
     }
 
 }
 const char *alphabet_war(const char **force, const char **attak, int nforce, int nattak) {
 
-		const int size = strlen(force[0]);
+    const int size = strlen(force[0]);
 
-		char *field = malloc(size * sizeof(char));
-		memset (field,'_',size);
+    char *field = malloc(size * sizeof(char));
+    memset (field,'_',size);
 
-		char *forc2[nforce];
+    char *forc2[nforce];
 
-		for (int i = 0; i < nforce; i++){
-				forc2[i] = malloc(size * sizeof(char));
-				memcpy(forc2[i], force[i], size * sizeof(char));
-		}
+    for (int i = 0; i < nforce; i++){
+        forc2[i] = malloc(size * sizeof(char));
+        memcpy(forc2[i], force[i], size * sizeof(char));
+    }
 
 
-		for (int i = 0; i < nattak; i++) {
-				reinforce(field, nforce, forc2);
-				field = strike (field, attak[i]);
-		}
+    for (int i = 0; i < nattak; i++) {
+        reinforce(field, nforce, forc2);
+        field = strike (field, attak[i]);
+    }
 
-		reinforce(field, nforce, forc2);
+    reinforce(field, nforce, forc2);
 
-		printf("%s\n", field);
-		return field;
+    printf("%s\n", field);
+    return field;
 }
 
 int main () {
 
-		const int nforce = 10, nattak = 9;
-		const char *force[10] =
-		{"g964xxxxxxxx", "myjinxin2015", "steffenvogel", "smile67xxxxx", "giacomosorbi",
-				"freywarxxxxx", "bkaesxxxxxxx", "vadimbxxxxxx", "zozofouchtra", "colbydauphxx" };
-		const char *attak[10] =
-		{"* *** ** ***", 
-				" ** * * * **",
-				" * *** * ***",
-				" **  * * ** ",
-				"* ** *   ***",
-				"***         ",
-				"**          ",
-				"*           ",
-				"*           " };
+    const int nforce = 10, nattak = 9;
+    const char *force[10] =
+    {"g964xxxxxxxx", "myjinxin2015", "steffenvogel", "smile67xxxxx", "giacomosorbi",
+        "freywarxxxxx", "bkaesxxxxxxx", "vadimbxxxxxx", "zozofouchtra", "colbydauphxx" };
+    const char *attak[10] =
+    {"* *** ** ***", " ** * * * **", " * *** * ***", " **  * * ** ", "* ** *   ***",
+        "***         ", "**          ", "*           ", "*           " };
 
-		alphabet_war(force, attak, 10, 9); /* => codewarsxxxx */
+    //alphabet_war(force, attak, 10, 9); /* => codewarsxxxx */
+
+    char *field = strdup("abcde[fg]hi#jkl[m]nop");
 
 
-		printf("\nexit\n");
+    for (int i = 0; field[i]!= '\0'; i++) {
+        if (field[i] == '#') {
+            bool shelter = false;
+
+            for (int j = 0; field[j] != '\0'; j++) {
+                if (field[j] == '[') shelter = true;
+                if (shelter == false) field[j] = '_';
+                if (field[j] == ']') shelter = false;
+            }
+
+            int j = i;
+            while (j >= 0 ) {
+                if (field[j] == '[' || field[j] == ']') {
+                    field[j] = '_';
+                    break;
+                }
+                j--;
+            }
+            j = i;
+
+            while (field[j] != '\0') {
+                if (field[j] == '[' || field[j] == ']') {
+                    field[j] = '_';
+                    break;
+                }
+                j++;
+            }
+
+
+        }
+    }
+
+    for (int i = 0; field[i]!= '\0'; i++) {
+
+    }
+
+    printf("%s\n", field);
+
+    printf("\nexit\n");
 }
