c776c6a933a38e7ef0c870e50260bfe14b2bb8a4
Nemrod-7 <ocopey@gmail.com>
2024-10-18 18:05:58 +0200

    update

--- alphawar.c
+++ alphawar.c
@@ -53,103 +53,79 @@ char *trench_assault(const char *src) {
     int prv, cnt = 0, upper = 0, lower = 0;
     _Bool trench = 0;
 
-    //showpoint(src);
+    showpoint(src);
     for (int i = 0; i < size; i++) {
         int gnd = count(src[i]), sub = count(src[size+i+1]);
 
-        if (trench == 0) {
-            lower = 0;
-        }
 
         if (src[size+i+1] == '|') {
-            trench ^= 1;
-        }
+						trench ^= 1;
+						lower = 0;
+				}
 
-        if (trench == 1 && src[size+i] == '|') {
-            sub *= 2;
-        }
+				if (trench == 1 && src[size+i] == '|') {
+						sub *= 2;
+				}
 
-        if (gnd == 0) {
-            lower += upper;
-            upper = 0;
-        }
+				if (gnd == 0) {
+						lower += upper;
+						upper = 0;
+				}
 
+				prv = cnt;
+				upper += gnd, lower += sub;
+				cnt = upper + lower;
 
-        prv = cnt;
-        upper += gnd, lower += sub;
-        cnt = upper + lower;
+				if (cnt == 0) {
+						cnt = prv;
+				}
 
-        if (cnt == 0) {
-            cnt = prv;
-        }
+				printf("%2i ", cnt);
+				if (src[size+i+1] == '|') {
+						*ptr++ = '|';
+				} else if (cnt < 0) {
+						*ptr++ = 'L';
+				} else if (cnt > 0){
+						*ptr++ = 'R';
+				}
+		}
 
-        if (src[size+i+1] == '|') {
-            *ptr++ = '|';
-        } else if (cnt < 0) {
-            *ptr++ = 'L';
-        } else if (cnt > 0){
-            *ptr++ = 'R';
-        }
-    }
-
-    *(ptr) = '\0';
-    return res;
+		*(ptr) = '\0';
+		return res;
 }
 
 void do_test (const char *source, const char *expected) {
-    const char *actual = trench_assault(source);
+		const char *actual = trench_assault(source);
 
-    if (strcmp(actual, expected) != 0) {
-        show(source);
-        printf("error: expected [%s] actual [%s]\n", expected, actual);
-    }
+		if (strcmp(actual, expected) != 0) {
+				show(source);
+				printf("error:\n expected [%s]\n actual   [%s]\n", expected, actual);
+		}
 }
 void Tests() {
 
-    do_test("pwss     s\n" "----|qdd|-", "LLLL|LLR|L");
-    do_test("psbpww\n" "------", "LLLLLL");
-    do_test("dzm    qm\n" "---|dq|--", "RRR|RR|RR");
-    do_test("sbbb  \n" "----|m", "LLLL|R");
-    do_test("pbbzmq\n" "------", "LLLLLR");
+		do_test("pwss     s\n" "----|qdd|-", "LLLL|LLR|L");
+		do_test("psbpww\n" "------", "LLLLLL");
+		do_test("dzm    qm\n" "---|dq|--", "RRR|RR|RR");
+		do_test("sbbb  \n" "----|m", "LLLL|R");
+		do_test("pbbzmq\n" "------", "LLLLLR");
 
+		do_test("     s\n" "mmmm|-", "RRRR|L");
+		do_test("m     \n" "-|pzzz", "R|LLLR");
+		do_test("  zb\n" "m|--", "R|RL");
 
-    do_test("  zb\n" "m|--", "R|RL");
-    do_test("     s\n" "mmmm|-", "RRRR|L");
-    do_test("m     \n" "-|pzzz", "R|LLLR");
-    do_test("  zb\n" "m|--", "R|RL");
+		do_test ("       ww      mmqpsw      \n" "pszwbw|--|swzd|------|qpbwb", "LLLLLL|LL|LLLL|RRRRRR|RRRLL");
+		do_test("     mdp\n" "qqqs|---", "RRRR|RRR");
 }
 
 int main () {
 
-    // int hash[20] = {[0 ... 19] = 2};
-      "pwss     s"
-      "----|qdd|-",
+		// int hash[20] = {[0 ... 19] = 2};
 
-      "psbpww"
-      "------",
+		//Tests();
 
-      "dzm    qm"
-      "---|dq|--",
-
-      "sbbb  "
-      "----|m",
-
-      "pbbzmq"
-      "------",
-
-      "  zb"
-      "m|--",
-
-      "     s"
-      "mmmm|-",
-
-      "m     "
-      "-|pzzz",
-
-      "  zb"
-      "m|--",
-
-    // Tests();
-
-    printf("\nexit\n");
+		//do_test( "m       \n"       "-|mpbpwb", "R|RRRRLL"); 
+		do_test( "    zqqp    \n" "ppw|----|ppm", "LLL|RRRR|LLL");
+ 
+		printf("\nexit\n");
 }
