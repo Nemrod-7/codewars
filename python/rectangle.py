0f7a434e429c8b156286b74cb2cdef3328d25a80
Nemrod-7 <ocopey@gmail.com>
2025-02-27 16:16:02 +0100

    270225

--- python/rectangle.py
+++ python/rectangle.py
@@ -17,21 +17,30 @@ def calculate (rect) :
 
     for i in range(0,len(sweep)) :
         [x, record] = sweep[i]
-        bounds = []
-        inter = []
 
         if line :
+            vertical, interval = [], []
             x0 = sweep[i - 1][0]
             miny, maxy = 999, -999
 
             for idx in line :
-                # [x1,y1,x2,y2] = rect[idx]
-                miny = min(rect[idx][1], miny)
-                maxy = max(rect[idx][2], maxy)
+                vertical.append([rect[idx][1], rect[idx][3]])
+            
+            vertical.sort();
+            # print(vertical)
+            for y in vertical :
+                if len(interval) == 0 :
+                    interval.append(y)
+                else :
+                    last = len(interval) - 1
 
-            inter.append([miny,maxy])
+                    if interval[last][1] < y[0] :
+                        interval.append(y)
+                    else :
+                        interval[last][1] = max(interval[last][1], y[1])
 
-            for [y0,y1] in inter :
+            # print(interval)
+            for [y0,y1] in interval :
                 area += (x - x0) * (y1 - y0)
 
         for idx in record :
@@ -44,4 +53,5 @@ def calculate (rect) :
 
 print('\n\n\n\n\n\n')
 rect = [[3,3,8,5], [6,3,8,9], [11,6,14,12]]
-calculate(rect)
+actual = calculate(rect)
+print(actual)
