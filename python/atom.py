c501af6af7c4a97cca088b7d73846a88ac4cb85d
Nemrod-7 <ocopey@gmail.com>
2025-03-29 10:15:07 +0100

    update

--- python/atom.py
+++ python/atom.py
@@ -12,10 +12,21 @@ class Atom() :
         self.id = id
         self.element = elt
         self.edge = adj
+        self.str = ''
 
-    def show(self) :
-        at = self.element + str(self.id)
-        return [at, [ix.id for ix in self.edge]]
+    def __hash__(self):      return self.id
+    def __eq__(self, other): return self.id == other.id
+
+    def __str__(self) :
+        prefix = 'Atom(' + self.element + '.' + str(self.id)  
+        edge =  ['H' if bond.element == 'H' else bond.element + str(bond.id) for bond in self.edge]
+        
+        if len(edge) > 0 : prefix += ': '
+        
+        return prefix + ','.join(edge) + ')'
+
+    def valid_valence(self) :
+        return len(self.edge) < table[self.element][0]
 
 class Molecule () :
     def __init__ (self, name='') :
@@ -24,38 +35,33 @@ class Molecule () :
         self.molecular_weight = 0.0
 
         self.lock = False
+        self.atoms = []
         self.arms = [[0]]
-        self.atoms = [Atom(0,'C',[])]
 
-    def getcarbons(self, nb) :
-        return [ i for i in range(0, len(self.arms[nb])) if self.arms[nb][i].element == 'C']
+    # def getcarbons(self, nb) : return [ i for i in range(0, len(self.arms[nb])) if self.arms[nb][i].element == 'C']
 
     def bond(self, i1, i2) :
         a1, a2 = self.atoms[i1], self.atoms[i2]
-        # self.atoms[i1].edge.append(a2)
-        # self.atoms[i2].edge.append(a1)
-        a1.edge.append(a2.id)
-        a2.edge.append(a1.id)
+        self.atoms[i1].edge.append(a2)
+        self.atoms[i2].edge.append(a1)
+        # a1.edge.append(a2.id)
+        # a2.edge.append(a1.id)
         
 
     def brancher(self, *arg) :
         # Adds new "branches" to the current molecule.
         # Can take any number of arguments (positive integers).
         # Each argument gives the number of carbons of the new branch.
-        if self.lock == True : raise 'error'
+        if self.lock == True : raise Exception( 'UnlockedMolecule')
         
         for nc in arg :
             branch = [0] + [len(self.atoms) + i for i in range(0, nc)]
-            self.atoms += [Atom(len(self.atoms) + i, 'C',[]) for i in range(0,nc)]
+            self.atoms += [Atom((len(self.atoms) + i + 1), 'C',[]) for i in range(0,nc)]
             self.arms.append(branch)
 
             for i in range(2, nc + 1) : 
                 self.bond(branch[i-1], branch[i-0])
         
-        for i in range(1,len(self.atoms)) :
-            atom = self.atoms[i]
-            # print(atom.element, atom.id, len(atom.edge))
-
         return self
 
     def bounder(self,*arg) :
@@ -64,14 +70,15 @@ class Molecule () :
         #     c1 & b1: carbon and branch positions of the first atom
         #     c2 & b2: carbon and branch positions of the second atom
         # All positions are 1-indexed, meaning (1,1,5,3) will bound the first carbon of the first branch with the fifth of the third branch.
-        if self.lock == True : raise 'error'
+        if self.lock == True : raise Exception('LockedMolecule')
 
         for (c1,b1,c2,b2) in arg :
-            # i1 = [ix for ix in self.arms[b1] if self.atoms[ix].element == 'C']
-            # i2 = [ix for ix in self.arms[b2] if self.atoms[ix].element == 'C']
-            # self.bond(i1[c1], i2[c2])
-
             i1, i2 = self.arms[b1][c1], self.arms[b2][c2]
+            a1, a2 = self.atoms[i1], self.atoms[i2]
+
+            if i1 == i2 : raise Exception('Self bonding.')
+            if not a1.valid_valence() or not a2.valid_valence() : raise Exception('Exceding valence')
+
             self.bond(i1, i2)
 
         return self
@@ -80,23 +87,33 @@ class Molecule () :
         # Mutates the carbon nc in the branch nb to the chemical element elt(given as a string).
         # Don't forget that carbons and branches are 1-indexed.
         # This is mutation: the id number of the Atom instance stays the same. See the Atom class specs about that.
+        if self.lock == True : raise Exception( 'LockedMolecule')
 
         for (nc,nb,elt) in arg :
-            # carbon = [ix for ix in self.arms[nb] if self.atoms[ix].element == 'C']
-            # if nc < len(carbon) : self.atoms[carbon[nc]].element = elt
+            [valence, weight] = table[elt]
+            a1 = self.arms[nb][nc]
 
-            self.atoms[self.arms[nb][nc]].element = elt
+            if len(self.atoms[a1].edge) > valence : 
+                raise Exception('Invalid valence')
+
+            self.atoms[a1].element = elt
 
         return self
 
     def add(self, *arg) :
         # Adds a new Atom of kind elt (string) on the carbon nc in the branch nb.
         # Atoms added this way are not considered as being part of the branch they are bounded to and aren't considered a new branch of the molecule.
+        if self.lock == True : raise 'LockedMolecule'
 
         for (nc,nb,elt) in arg :
-            self.atoms.append( Atom(len(self.atoms), elt, []))
-            self.bond(i1, self.atoms[-1].id)
+            a1 = Atom(len(self.atoms), elt, [])
+            a2 = self.atoms[self.arms[nb][nc]]
 
+            if not a1.valid_valence() or not a2.valid_valence() : 
+                raise Exception('Invalid valence')
+
+            self.atoms.append(a1)
+            self.bond(i1, self.atoms[-1].id)
 
         return self
 
@@ -105,24 +122,22 @@ class Molecule () :
         return self
 
     def closer(self) : # add missing missing hydrogen and lock molecule
+        if self.lock == True : raise Exception('LockedMolecule')
         self.lock = True
         hist = {}
+        
+        for atom in self.atoms :
+            while atom.valid_valence() :
+                hydr = Atom(len(self.atoms) + 1, 'H', [atom])
 
-        for ix in range (1, len(self.atoms)) :
-            atom = self.atoms[ix]
+                atom.edge.append(hydr)
+                self.atoms.append(hydr)
 
-            while len(atom.edge) < table[atom.element][0] :
-                edge = Atom(len(self.atoms), 'H', [atom.id])
-                atom.edge.append(len(self.atoms))
-
-                self.atoms.append(edge)
-
-        for ix in range (1, len(self.atoms)) :
-            atom = self.atoms[ix]
+        for atom in self.atoms :
             hist[atom.element] = 1 if atom.element not in hist else hist[atom.element] + 1
 
         arr = [(key,hist[key]) for key in hist]
-        arr.sort()
+        arr.sort(key = lambda x: (len(x[0]), x[0]))
 
         for [elt, nb] in arr :
             self.formula += elt + (str(nb) if nb > 1 else '')
@@ -132,48 +147,40 @@ class Molecule () :
 
     def unlock (self) : 
         # makes the molecule mutable again and remove hydrogen atoms
+        if self.lock == False: raise Exception('UnlockedMolecule')
         self.lock = False
-        atoms = [ atom for atom in self.atoms if atom.element != 'H' ]
+        self.atoms = [ atom for atom in self.atoms if atom.element != 'H' ]
+
+        for atom in self.atoms : atom.edge = [nxt for nxt in atom.edge if nxt.element != 'H']
 
         return self
 
-    def show(self) :
-        for i in range(1, len(self.arms)) :
-            arm = self.arms[i]
-            for atom in arm : atom.show()
+
+class display :
+    def branches(molecule) :
+        print(molecule.name, molecule.formula, molecule.molecular_weight)
+        m = molecule
+        for i in range(1, len(m.arms)) :
+            for j in range(1, len(m.arms[i])) :
+                atom = m.atoms[m.arms[i][j]]
+
+                print(atom)
             print()
 
-
-def display(molecule) :
-    print(molecule.name, molecule.formula, molecule.molecular_weight)
-
-    for i in range(1, len(molecule.arms)) :
-        for j in range(1, len(molecule.arms[i])) :
-            atom = molecule.atoms[molecule.arms[i][j]]
-            curr = atom.element + '.' + str(atom.id)
-            edge = ','.join([edge.element + (str(edge.id) if edge.element != 'H' else '') for edge in atom.edge])
-            print(curr,':', edge)
-        print()
+    def atoms(molecule) :
+        for atom in molecule.atoms :
+            print(atom)
 
 
 # for elt in table : print(elt, table[elt])
-formula = 'C10H16N2O3S'
+# formula = 'C10H16N2O3S'
+# biotin = Molecule("biotin")
+# biotin.brancher(14,1,1)
+# biotin.bounder((2,1,1,2), (2,1,1,2), (10,1,1,3), (10,1,1,3), (8,1,12,1), (7,1,14,1))
+# biotin.mutate( (1,1,'O'),  (1,2,'O'), (1,3,'O'), (11,1,'N'), (9,1,'N'), (14,1,'S'))
+# biotin.closer()
 
-biotin = Molecule("biotin")
-biotin.brancher(14,1,1)
-biotin.bounder((2,1,1,2), (2,1,1,2), (10,1,1,3), (10,1,1,3), (8,1,12,1), (7,1,14,1))
-biotin.mutate( (1,1,'O'),  (1,2,'O'), (1,3,'O'), (11,1,'N'), (9,1,'N'), (14,1,'S'))
-biotin.closer()
 
-# display(biotin)
-m = biotin
 
-for atom in m.atoms :
-    curr = atom.element + '.' + str(atom.id)
-    edge = ','.join([m.atoms[edge].element + (str(m.atoms[edge].id) if m.atoms[edge].element != 'H' else '') for edge in atom.edge])
-
-    if atom.element != 'H' : print(curr,':', edge)
-
-        
 
 print("end")
--- python/molecules.py
+++ python/molecules.py
@@ -21,6 +21,40 @@ CH3-CH2-CH2-CH2-CH2-CH2-CH2-CH3
 # methane = Molecule("methane").brancher(1).closer()
 # octane = Molecule("octane").brancher(8).closer()
 
+def test_methane() :
+    methane = Molecule("methane").brancher(1)
+    try:    atoms = methane.atoms
+    except: test.expect(False, "Couldn't access to the list of atoms")
+        
+    test.assert_equals(str(atoms[0]), 'Atom(C.1)')
+
+    methane.closer()
+    atoms = methane.atoms;
+    # for atom in atoms : print(atom)
+
+    test.assert_equals(len(atoms), 5, "Wrong number of atoms")
+
+    for x,elt in enumerate("CHHHH"):
+        test.assert_equals(atoms[x].element, elt, "Wrong atom at the index {} in self.atoms".format(x))
+        test.assert_equals(atoms[x].id, x+1, "Wrong id value {} in self.atoms".format(x+1))
+
+
+    test.assert_equals(str(atoms[0]), "Atom(C.1: H,H,H,H)")
+    test.assert_equals(str(atoms[-1]), "Atom(H.5: C1)")
+
+
+def extractNoneHToStr(m): return [str(at) for at in m.atoms if at.element != "H"]
+
+def testThisMolecule(m, formula, mm, strNoH):
+    test.assert_equals(m.formula, formula, "Testing raw formula")
+    test.assert_equals(m.molecular_weight, mm, "Testing molecular weight")
+    test.assert_equals(extractNoneHToStr(m), strNoH, "Checking bonds (for non-hydrogens)")
+
+
+
+
+
+
 range1 = [
 
 (""" cyclohexane:
@@ -89,6 +123,8 @@ CH    CH
 "C6H6",
 78,
 ['Atom(C.1: C2,C2,C6,H)', 'Atom(C.2: C1,C1,C3,H)', 'Atom(C.3: C2,C4,C4,H)', 'Atom(C.4: C3,C3,C5,H)', 'Atom(C.5: C4,C6,C6,H)', 'Atom(C.6: C1,C5,C5,H)']),
+
+
 ]
 
 
@@ -126,6 +162,7 @@ CH3
 ['Atom(C.1: C2,H,H,H)', 'Atom(C.2: C1,C5,Mg3,H)', 'Atom(Mg.3: C2,Br4)', 'Atom(Br.4: Mg3)', 'Atom(C.5: C2,H,H,H)'])
 ]
 
+
 def carbohydrates () :
     print( "Create carbohydrates and bound them correctly (id tracking, raw formula and molecular weight tested)")
     for occ in range1 :
@@ -147,6 +184,62 @@ def mutations () :
         test.assert_equals(m.molecular_weight, mm, "Testing molecular weight")
         # test.assert_equals(extractNoneHToStr(m), strNoH, "Checking bonds (for non-hydrogens)")
 
+config = [
+("No self-bonding",
+[6],
+[(1,1,1,1)]),
+
+("Should fail when exceeding the valence number adding new alkyls to the same atom",
+[3,1,1,1],
+[(2,1,1,2), (2,1,1,3), (2,1,1,4)]),
+
+("Should fail when exceeding the valence number with multiple bonds",
+[4],
+[(2,1,3,1), (2,1,3,1), (2,1,3,1)]),
+]
+
+
+# for descript,branch,bonds in config:
+#     try :
+#         m = Molecule("").brancher(*branch).bounder(*bonds).closer()
+#         func = lambda: Molecule("").brancher(*branch).bounder(*bonds).closer()
+#     except :
+#         print('got error')
+
+
+config = [
+("Should fail when mutating a carbon with three atoms already linked to an oxygen",
+[3,1],
+[(2,1,1,2)],
+("mutate", (2,1,'O'))),
+
+("Should fail when mutating a carbon with two double bonds to nitrogen",
+[3],
+[(1,1,2,1), (3,1,2,1)],
+("mutate", (2,1,'N'))),
+
+("Should fail when adding a new hydrogen to a carbon with already 4 bonds",
+[3],
+[(1,1,2,1), (3,1,2,1)],
+("add", (2,1,'H'))),
+
+
+("Should fail when mutating an atom and then adding too much atoms on it",
+[3],
+[(1,1,2,1)],
+("mutadd", [(2,1,'N'), (2,1,'O')]) ),
+
+]
+
+
+# for descript,branch,bonds,(mutadd,inp) in config:
+#     m = Molecule("").brancher(*branch).bounder(*bonds)
+#
+#     if mutadd == "mutate":   func = lambda: m.mutate(inp).closer()
+#     elif mutadd == "add":    func = lambda: m.add(inp).closer()
+#     elif mutadd == "mutadd": func = lambda: m.mutate(inp[0]).add(inp[1]).closer()
+#     else:  
+#         pass
 
 
 
