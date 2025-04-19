101800f8158d62c56d17b2d120708d4f7e1851c2
Nemrod-7 <ocopey@gmail.com>
2025-04-19 10:59:16 +0200

    update

--- python/morse.py
+++ python/morse.py
@@ -24,45 +24,36 @@ def decode_bits2 (bin) :
 
     return morse
 
-def mkunit2 (token) :
+def mkunit (token) :
     ones, zero = {}, {}
 
     for cell in token :
         x = len(cell)
 
-        match cell[0] :
-            case '0' : zero[x] = zero[x] + 1 if x in zero else 1
-            case '1' : ones[x] = ones[x] + 1 if x in ones else 1
-        # if cell[0] == '0' : 
-        # if cell[0] == '1' : 
+        if cell[0] == '0' : zero[x] = zero[x] + 1 if x in zero else 1
+        if cell[0] == '1' : ones[x] = ones[x] + 1 if x in ones else 1
 
-    zero, ones = [[x,zero[x]] for x in zero], [[x,ones[x]] for x in ones]
+    zero, ones = np.array([[x,zero[x]] for x in zero]).astype(float), np.array([[x,ones[x]] for x in ones]).astype(float)
 
     # zero.sort(key = lambda x: x[0])
     # ones.sort(key = lambda x: x[0])
+    # print(zero)
+    # print(ones)
+    # c0, c1 = kmeans(whiten(zero), 3)[0], kmeans(whiten(ones), 2)[0]
+    # r0, r1 = np.sort(c0[:,0] * np.std(zero, 0)[0]), np.sort(c1[:,0] * np.std(ones, 0)[0])
+    c0, c1 = kmeans(zero, 3)[0], kmeans(ones, 2)[0]
+    r0, r1 = np.sort(c0[:,0]), np.sort(c1[:,0])
+    # print(c0 * np.std(zero, 0))
+    # print(c1 * np.std(ones, 0))
 
-    print(zero)
-    print(ones)
-    c0, c1 = kmeans(whiten(zero), 3)[0], kmeans(whiten(ones), 2)[0]
-    r0, r1 = np.sort(c0[:,0] * np.std(zero)), np.sort(c1[:,0] * np.std(ones))
-
-    u0 = whiten(zero) * np.std(zero)
-    
-
-    print(u0)
-
-    u1 = np.mean([r0[0], r1[0]])
+    u1 = np.min([r0[0], r1[0]])
     u2 = np.mean([r0[1], r1[1]])
-    u3 = np.mean([r0[2], r1[1]])
+    u3 = np.max([r0[2], r1[1]])
 
-    # u1 = min([r0[0], r1[0]])
-    # u2 = np.mean([r0[1], r1[0]])
-    # u3 = max([r0[2], r1[1]])
-
-    # plt.scatter(zero[:, 0], zero[:, 1])
-    # plt.scatter(ones[:, 0], ones[:, 1])
-    # plt.scatter([u1,u2,u3], [1,1,1])
-    # plt.show()
+    plt.scatter(zero[:, 0], zero[:, 1])
+    plt.scatter(ones[:, 0], ones[:, 1])
+    plt.scatter([u1,u2,u3], [1,1,1])
+    plt.show()
 
     # print(u1, u2, u3)
     # return [round(u1), round(u2), round(u3)]
@@ -76,7 +67,7 @@ def type (length, units) :
 def decode_bits (bin) :
     morse = ''
     token = bin.strip('0').replace("01", "0 1").replace("10", "1 0").split()
-    rate = mkunit2(token)
+    rate = mkunit(token)
 
     print(rate)
 
@@ -91,9 +82,16 @@ def decode_bits (bin) :
     print(morse)
     return morse
 
-def decode_morse (morse):
+def decode_morse (morse) :
     code = morse.strip().split(' ')
-    return ''.join( [' ' if sign == '' else MORSE_CODE[sign] for sign in code]).replace('  ',  ' ')
+    msg = ''
+
+    for glyph in code :
+        msg += MORSE_CODE[glyph] if glyph in MORSE_CODE else '_'
+
+    print(msg)
+    return msg
+    # return ''.join( [' ' if sign == '' else MORSE_CODE[sign] for sign in code]).replace('  ',  ' ')
 
 class test :
     def assert_equals(actual, expect) :
@@ -105,12 +103,12 @@ class test :
 msg = '0000000011011010011100000110000001111110100111110011111100000000000111011111111011111011111000000101100011111100000111110011101100000100000'
 
 msg = "00000000000111111100000011010001110111000000001110000000000000000001111111011111100001101111100000111100111100011111100000001011100000011111110010001111100110000011111100101111100000000000000111111100001111010110000011000111110010000011111110001111110011111110000010001111110001111111100000001111111101110000000000000010110000111111110111100000111110111110011111110000000011111001011011111000000000000111011111011111011111000000010001001111100000111110111111110000001110011111100011111010000001100001001000000000000000000111111110011111011111100000010001001000011111000000100000000101111101000000000000011111100000011110100001001100000000001110000000000000001101111101111000100000100001111111110000000001111110011111100011101100000111111000011011111000111111000000000000000001111110000100110000011111101111111011111111100000001111110001111100001000000000000000000000000000000000000000000000000000000000000" # "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"
+
 # ones = [it for it in bin.split('0') if it != '']
 # zero = [it for it in bin.split('1') if it != '']
 # token = msg.strip('0').replace("01", "0 1").replace("10", "1 0").split()
 
-morse = decode_bits(msg)
-# text = decode_morse(morse)
+text = decode_morse(decode_bits("00000000000111111100000011010001110111000000001110000000000000000001111111011111100001101111100000111100111100011111100000001011100000011111110010001111100110000011111100101111100000000000000111111100001111010110000011000111110010000011111110001111110011111110000010001111110001111111100000001111111101110000000000000010110000111111110111100000111110111110011111110000000011111001011011111000000000000111011111011111011111000000010001001111100000111110111111110000001110011111100011111010000001100001001000000000000000000111111110011111011111100000010001001000011111000000100000000101111101000000000000011111100000011110100001001100000000001110000000000000001101111101111000100000100001111111110000000001111110011111100011101100000111111000011011111000111111000000000000000001111110000100110000011111101111111011111111100000001111110001111100001000000000000000000000000000000000000000000000000000000000000"))
 # print(text)
 
 
--- python/test.py
+++ python/test.py
@@ -1,3 +1,22 @@
 import numpy as np
+from scipy.cluster.vq import whiten,kmeans
+import matplotlib.pyplot as plt
 
-origin = [[6, 7], [1, 29], [3, 14], [8, 3], [18, 2], [4, 10], [5, 10], [2, 16], [7, 4], [14, 2], [12, 1], [13, 1], [10, 1], [15, 1], [9, 1], [17, 1]]
+points = np.array( [[6, 7], [1, 29], [3, 14], [8, 3], [18, 2], [4, 10], [5, 10], [2, 16], [7, 4], [14, 2], [12, 1], [13, 1], [10, 1], [15, 1], [9, 1], [17, 1]], dtype=float)
+
+# data = sorted(points, key = lambda x: x[0])
+# seed1 = points[np.argmax([points[:,0]])]
+# seed2 = points[np.argmin([points[:,0]])]
+
+norm = np.std(points, 0)
+whit = whiten(points)
+
+ctoid = kmeans(points, 3)[0]
+stand = np.sort(ctoid * norm)
+
+print(points)
+print(ctoid)
+
+# plt.scatter(points[:, 0], points[:, 1])
+# plt.scatter(ctoid[:, 0], ctoid[:, 1])
+# plt.show()
