39f648a3efdf29515b821ad015460b087c22b0bf
Nemrod-7 <ocopey@gmail.com>
2025-08-12 10:27:32 +0200

    update

--- python/sha1.py
+++ python/sha1.py
@@ -1,11 +1,8 @@
 mod = 4294967296
+print('\n\n\n')
 
-def rotate(val, n) :
-    return (val << n) ^ (val >> (32-n));
-
-def printex (txt) :
-    hash = [format(ha, '08X') for ha in txt]
-    return hash
+def hex (txt) : return [format(ha, '08X') for ha in txt]
+def rotl(n, d): return ((n << d) | (n >> (32 - d))) & 0xffffffff
 
 class SHA1(object) :
     def preprocess(txt) :
@@ -24,19 +21,19 @@ class SHA1(object) :
     def digest(self) :
         msg = SHA1.preprocess(self.msg)
 
-        w = [0] * 90
+        w = [0] * 80
         for i in range(16) :
             w[i] = msg[i * 4 + 0] << 24 | msg[i * 4 + 1] << 16 | msg[i * 4 + 2] <<  8 | msg[i * 4 + 3]
 
         for i in range(16,80) :
-            w[i] = rotate(w[i-3] ^ w[i-8] ^ w[i-14] ^ w[i-16], 1);
+            w[i] = rotl(w[i-3] ^ w[i-8] ^ w[i-14] ^ w[i-16], 1);
 
         hash = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0]
         a,b,c,d,e = hash
-#
+
         for i in range(80) :
             if i < 20 :
-                f,k = (b & c) | ((~b) & d), 0x5A827999
+                f,k = (b & c) | (~b & d), 0x5A827999
             elif i < 40 :
                 f,k = b ^ c ^ d, 0x6ED9EBA1
             elif i < 60 :
@@ -44,20 +41,29 @@ class SHA1(object) :
             else :
                 f,k = b ^ c ^ d, 0xCA62C1D6
 
-            e = d;
-            d = c;
-            c = rotate(b,30);
-            b = a;
-            a = (e + f + k + rotate(a,5) + w[i]) % mod
+            s1 = (e + f) % mod
+            s2 = (s1 + rotl(a,5)) % mod
+            s3 = (s2 + w[i]) % mod
+            s4 = (s3 + k) % mod
 
-        digest = [a,b,c,d,e]
-        hash = [(digest[i] ) % mod for i in range(5)]
+            e,d,c,b,a = d % mod, c % mod, rotl(b,30) % mod, a % mod, s4 % mod
 
-        print(printex(hash))
-        
-        return hash
+            digest = [a,b,c,d,e]
+
+        return ''.join([format(digest[i] + hash[i], 'x') for i in range(5)])
+
+
+
+def binary(num) :
+    for i in range(32) :
+        bit = num >> i &1
+        print(bit,end='')
+    print()
 
 sha = SHA1()
 sha.update(b'abc')
 actual = sha.digest()
 expect = b'a9993e364706816aba3e25717850c26c9cd0d89d'
+
+if actual != expect :
+    print('actual : ', actual,  '\nexpect :', expect)
--- python/test.py
+++ python/test.py
@@ -1,11 +1,24 @@
-txt = b'this'
+txt = b'abc'
 
-txt = [ (it) for it in txt ]
+def rotl(n, d): return ((n << d) | (n >> (32 - d))) & 0xffffffff
 
-txt.append(0x01)
+msg = bytearray(txt)
+end = (448 - ((len(txt) * 8 + 8) - 64)) // 8 - 1
 
-print(txt)
+msg.append(0x80)
+msg += bytes(end)
+msg.append(len(txt) * 8)
 
+w = [0] * 80
 
-for it in txt :
-    print(it)
+for i in range(16) :
+    w[i] = msg[i * 4 + 0] << 24 | msg[i * 4 + 1] << 16 | msg[i * 4 + 2] <<  8 | msg[i * 4 + 3]
+
+for i in range(16,80) :
+    w[i] = rotl(w[i-3] ^ w[i-8] ^ w[i-14] ^ w[i-16], 1)
+
+hash = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0]
+
+hash = ''.join([format(x,'x') for x in hash])
+
+print(bytes(hash, 'utf-8'))
