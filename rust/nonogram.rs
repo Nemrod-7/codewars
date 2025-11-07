bb645ddfc39f164df354b5b98f648837fad98353
Nemrod-7 <ocopey@gmail.com>
2025-11-07 08:15:11 +0100

    up

---
+++ rust/nonogram.rs
@@ -0,0 +1,128 @@
+#![allow(unused)]
+
+type Clues = &'static [&'static [u8]];
+
+struct Combs {
+    bits: Vec<u32>,
+}
+
+impl Combs {
+    fn new(size:usize) -> Combs {
+        return Combs {
+            bits: (0..size+1).map( |x| !(!0 << x) ).collect()
+        }
+    }
+
+    fn place (&self, block: &[u8], total: u8) -> Vec<u32> {
+        if block.len() == 0 { return vec![0]; }
+        if block[0] > total { return vec![]; }
+
+        let start = total - block[0];
+        let mut res:Vec<u32> = Vec::new();
+
+        if block.len() == 1 {
+            for i in 0..(start + 1) {
+                res.push(self.bits[block[0] as usize] << i);
+            }
+        } else {
+            for i in 0..start {
+                for sol in self.place( &block[1..], start - i - 1) {
+                    res.push( (self.bits[block[0] as usize] << i) | (sol << (block[0] + i + 1 )) );
+                }
+            }
+        }
+
+        res
+    }
+}
+
+fn resize(combs: &Vec<u32>, x:u32, cell:u8) -> Vec<u32> {
+    combs.iter().filter(|curr| ((*curr >> x)&1) == cell as u32 ).map(|x| *x).collect::<Vec<_>>()
+}
+
+fn reduce(comb: &Vec<u32>, x:u32) -> u8 {
+    let mut ht = 0;
+
+    for curr in comb {
+        ht |= 1 << ((curr >> x)&1);
+        if ht == 3 { return 2; }
+    }
+
+    if ht == 1 { return 0; }
+    return 1;
+}
+
+pub fn solve ((top, left): (Clues, Clues), width: usize, height: usize) -> Vec<Vec<u8>> {
+    let combs = Combs::new(15);
+    let mut running = true ;
+    let mut grid = vec![vec![2u8;width];height];
+    let mut west:Vec<Vec<u32>> = left.iter().map(|x| combs.place(x, width as u8)).collect();
+    let mut north:Vec<Vec<u32>> = top.iter().map(|x| combs.place(x, height as u8)).collect();
+
+    let mut idy:Vec<(usize,usize)> = (0..west.len()).map(|x| ( west[x].len(), x) ).collect();
+    let mut idx:Vec<(usize,usize)> = (0..north.len()).map(|x| ( north[x].len(), x) ).collect();
+
+    while running {
+        running = false;
+        idy.sort(); idx.sort();
+
+        for &(_,y) in &idy {
+            for &(_,x) in &idx {
+                if grid[y][x] == 2 {
+                    let a = reduce(&north[x], y as u32);
+                    let b = reduce(&west[y], x as u32);
+
+                    if a != 2 { grid[y][x] = a; }
+                    if b != 2 { grid[y][x] = b; }
+                } else {
+                    north[x] = resize(&north[x], y as u32, grid[y][x]);
+                    west[y] = resize(&west[y], x as u32, grid[y][x]);
+                }
+
+                if grid[y][x] == 2 { running = true; }
+            }
+        }
+    }
+
+    display(&grid);
+    grid
+}
+
+fn display(grid:&Vec<Vec<u8>>) {
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            match grid[y][x] {
+                0 => print!("  "),
+                1 => print!("x "),
+                2 => print!(". "),
+                _ => print!("  "),
+            }
+        }
+        print!("\n");
+    }
+
+}
+fn show(num:u32, total:u8) {
+    for i in 0..total {
+        let bit = (num >> i) &1;
+        print!("{bit}");
+    }
+
+    print!("\n");
+}
+
+fn main () {
+
+    type Clues = &'static [&'static [u8]];
+    type Solution = &'static [&'static [u8]];
+
+    const CLUES: (Clues, Clues) = (
+        &[&[3], &[4], &[2, 2, 2], &[2, 4, 2], &[6], &[3]],
+        &[ &[4], &[6], &[2, 2], &[2, 2], &[2], &[2], &[2], &[2], &[], &[2], &[2], ],
+    );
+
+    let width = 6;
+    let height = 11;
+
+    let res = solve(CLUES, width, height);
+}
--- rust/test.rs
+++ rust/test.rs
@@ -1,119 +1,54 @@
+#![allow(unused)]
 
-pub struct Rnd {
-    Seed : u32,
+static mut BITS:[u64;35] = [0u64; 35];
+static mut DP:Vec<Vec<u64>> = Vec::new();
+
+fn place (block: &[u8], total: u8) -> Vec<u64> {
+    if block.len() == 0 { return vec![0]; }
+    if block[0] > total { return vec![]; }
+
+    let dig = block[0] as usize; 
+    let start = total - block[0];
+    let mut res:Vec<u64> = Vec::new();
+
+    unsafe {
+        if BITS[dig] == 0 { BITS[dig] = !(!0 << dig); } 
+
+        while DP.len() < (start + 1) as usize {
+            DP.push(vec![]);
+        }
+
+        if block.len() == 1 { 
+            if DP[dig].len() == 0 {
+                for i in 0..(start + 1) {
+                    DP[dig].push(BITS[dig] << i);
+                }
+            }
+            
+            return DP[dig].clone();
+        } else {
+            for i in 0..start {
+                for sol in place( &block[1..], start - i - 1) {
+                    res.push( (BITS[dig] << i) | (sol << (dig as u8 + i + 1 )) );
+                }
+            }
+        }
+    }
+
+    res
 }
 
-impl Rnd {
-    const OFFSET : u32 = 0x5414d5f4;
-
-    #[inline]
-    pub fn new(seed: u32) -> Self {
-        Self {
-            Seed: seed ^ Rnd::OFFSET,
-        }
-    }
-
-    #[inline]
-    pub fn new_randomized() -> Self {
-        Self {
-            Seed: Rnd::OFFSET.wrapping_mul(Rnd::generate_time_seed())
-        }
-    }
-
-    fn generate_time_seed() -> u32 {
-        use std::time::{SystemTime, UNIX_EPOCH};
-        let now = SystemTime::now();
-        let since_epoch = now.duration_since(UNIX_EPOCH);
-        match since_epoch {
-            Ok(d) => {
-                let ms: u128 = d.as_micros();
-                let a = (ms >> 96) as u32;
-                let b = ((ms >> 64) & 0xffffffff) as u32;
-                let c = ((ms >> 32) & 0xffffffff) as u32;
-                let d = ms as u32;                
-                a ^ b ^ c ^ d
-            } 
-            _ => { Rnd::OFFSET }
-        }
-    }
-
-    #[inline]
-    pub fn next_u32(&mut self) -> u32 {
-        let mut random : u32 = self.Seed;
-        random ^= random << 13;
-        random ^= random >> 17;
-        random ^= random << 5;
-        self.Seed ^= random;
-        random
-    }
-
-    #[inline]
-    pub fn next_u64(&mut self) -> u64 {
-        let a : u64 = self.next_u32() as u64;
-        let b : u64 = self.next_u32() as u64;
-        a | (b << 32)
-    }
-
-    #[inline]
-    pub fn next_i64(&mut self) -> i64 {
-        self.next_u64() as i64
-    }
-
-    #[inline]
-    pub fn next_i32(&mut self) -> i32 {
-        self.next_u32() as i32
-    }
-
-    #[inline]
-    pub fn next_u64_range(&mut self, min: u64, max: u64) -> u64 {
-        if max > min {
-            let range = max - min;
-            min + (self.next_u64() % range)
-        } else {
-            min
-        }
-    }
-
-    #[inline]
-    pub fn next_u32_range(&mut self, min: u32, max: u32) -> u32 {
-        if max > min {
-            let range = max - min;
-            min + (self.next_u32() % range)
-        } else {
-            min
-        }
-    }
-
-    #[inline]
-    pub fn next_i64_range(&mut self, min: i64, max: i64) -> i64 {
-        if max > min {
-            let range = (max - min) as u64;
-            let r = self.next_u64() % range;
-            (r as i64) + min
-        }
-        else
-        {
-            min
-        }
-    }
-
-    #[inline]
-    pub fn next_i32_range(&mut self, min: i32, max: i32) -> i32 {
-        if max > min {
-            let range = (max - min) as u32;
-            let r = self.next_u32() % range;
-            (r as i32) + min
-        } else {
-            min
-        }
-    }
-}
 
 fn main() {
-    let mut rand: Rnd = Rnd::new_randomized();
-    for _i in 0..43 {
 
-        let nr: u64 = rand.next_u64();
-        println!("{}", nr);
+    let comb = place(&[3, 2],10);
+
+    unsafe {
+
+        for i in 0..10 {
+            print!("{} ", BITS[i] );
+        }
+
     }
+
 }
