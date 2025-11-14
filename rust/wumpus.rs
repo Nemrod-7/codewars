6fb96af33bd43ebd4311f7ad5ea76c22bde5ce3b
Nemrod-7 <ocopey@gmail.com>
2025-11-14 17:21:20 +0100

    update

--- rust/wumpus.rs
+++ rust/wumpus.rs
@@ -11,20 +11,13 @@ fn mk_grid(src: &Vec<String>) -> Vec<Vec<String>> {
 
     for i in 0..height {
         for j in 0..width {
-            let y = (i * 4) / height;
-            let x = (j * 4) / width;
+            let (x,y) = ((j * 4) / width, (i * 4) / height) ;
 
-            match src[i][j] {
-                'W' => (),
-                'A' => (),
-                'P' => (),
-                'G' => (),
-                'w' => (),
-                's' => (),
-                _ => continue,
+            if !"WAPGws".contains(src[i][j]) {
+                continue;
             }
 
-            if !grid[y][x].find(src[i][j]).is_some() {
+            if !grid[y][x].contains(src[i][j]) {
                 grid[y][x] += &format!("{}",src[i][j] ) ;
             }
         }
@@ -36,19 +29,69 @@ fn mk_grid(src: &Vec<String>) -> Vec<Vec<String>> {
 
 
 fn next_dir(x:usize, y:usize) -> Vec<(usize,usize)> {
+    [(0,1),(0,-1),(1,0),(-1,0)] 
+        .iter() 
+        .map(|(dx,dy)| (dx + x as i8, dy + y as i8) ) 
+        .filter(|&(nx,ny)| is_inside(nx, ny)) 
+        .map(|(nx,ny)| (nx as usize, ny as usize))
+        .collect::<Vec<_>>()
+}
+fn solve (src: &Vec<String>) -> bool {
+
     let direction = [(0,1),(0,-1),(1,0),(-1,0)];
-    let mut res = vec![];
+    let grid = mk_grid(&src);
+    let mut exit = (0,0);
+    let mut dmz = [[0; 4]; 4];
 
-    for (dx,dy) in direction.iter() {
-        let (nx,ny) = (dx + x as i8, dy + y as i8);
+    (0..4).for_each(|y| 
+        (0..4).for_each(|x| {
+            if grid[y][x].contains(|ch| ch == 'G') {
+                exit = (x as usize,y as usize);
+            }
+            if grid[y][x].contains(|ch| ch == 's' || ch == 'w') {
+                next_dir(x,y).iter().for_each(|&(nx,ny)| dmz[ny][nx] += 1)
+            }
+        }
+        ));
 
-        if is_inside(nx,ny) {
-            res.push((nx as usize, ny as  usize));
+    (0..4).for_each(|y| 
+        (0..4).for_each(|x|
+            if grid[y][x] == "" {
+                next_dir(x,y).iter().for_each(|&(nx,ny)| dmz[ny][nx] = 0)
+            }
+        ));
+
+
+    let mut visit = [[0; 4]; 4];
+    let mut queue = vec![(0,0)];
+
+    while let Some((x,y)) = queue.pop() {
+
+        for (dx,dy) in direction.iter() {
+            let (nx,ny) = (x + dx, y + dy);
+
+            if is_inside(nx,ny) {
+                let (nx,ny) = (nx as usize, ny as usize);
+
+                if visit[ny][nx] == 0 && dmz[ny][nx] == 0 {
+                    visit[ny][nx] = 1;
+                    queue.push( (nx as i8, ny as i8) );
+                }
+            }
+
         }
     }
-    res
-}
 
+    for i in 0..4 {
+        for j in 0..4 {
+            print!("{:?} ", visit[i][j]);
+        }
+        print!("\n");
+    }
+
+    // print!("{} {}\n", exit.0, exit.1);
+    if visit[exit.1][exit.0] == 1 { return true } else { return false }
+}
 fn main () {
 
     let src = [
@@ -80,52 +123,7 @@ fn main () {
 
     let src = src.iter().map(|x| format!("{x}")).collect::<Vec<String>>();
 
-    let grid = mk_grid(&src);
-    let mut pts = [[0; 4]; 4];
-
-    (0..4).for_each(|y| 
-        (0..4).for_each(|x|
-        if grid[y][x].contains(|ch| ch == 's' || ch == 'w') {
-            for (nx,ny) in next_dir(x,y) {
-                pts[ny][nx] += 1; 
-            }
-        }
-    ));
-
-    (0..4).for_each(|y| 
-        (0..4).for_each(|x|
-        if grid[y][x] == "" {
-            for (nx,ny) in next_dir(x,y) {
-                pts[ny][nx] = 0; 
-            }
-        }
-    ));
-
-    for i in 0..4 {
-        for j in 0..4 {
-            print!("{:?} ", pts[i][j]);
-        }
-        print!("\n");
-    }
-
-    let mut visit = [[false; 4]; 4];
-    let mut queue = vec![(0,0)];
-
-    while let Some((x,y)) = queue.pop() {
-
-        for (dx,dy) in direction.iter() {
-            let (nx,ny) = (x + dx, y + dy);
-
-            if is_inside(nx,ny) {
-                let (nx,ny) = (nx as usize, ny as usize);
-
-                if !visit[ny][nx] {
-                    visit[ny][nx] = true;
-                }
-
-            }
-        }
-    }
-
+    let res = solve(&src);
+    print!("{res}\n");
 
 }
